9102fea4929e29d431487293eb39d745656a05a4
Pew CodesLoves <sodapew17@gmail.com>
2016-07-01 09:46:05 +0700

    update layouts product

---
+++ frontend/controllers/ProductsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use yii\base\InvalidParamException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use frontend\models\PasswordResetRequestForm;
+use frontend\models\ResetPasswordForm;
+use frontend\models\SignupForm;
+use frontend\models\ContactForm;
+
+/**
+ * Products controller
+ */
+class ProductsController extends MasterController {
+
+    public $enableCsrfValidation = false;
+
+    /**
+     * Displays homepage.
+     *
+     * @return mixed
+     */
+    public function actionIndex() {
+        //return Yii::$app->getResponse()->redirect('register/login');
+        $this->title = 'Cost.fit | Products';
+        $this->subTitle = 'Products';
+        return $this->render('products');
+    }
+
+}
