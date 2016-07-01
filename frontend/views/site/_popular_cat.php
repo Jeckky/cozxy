203e585db70e85d2455106a427ae25b9faaa35bc
Pew CodesLoves <sodapew17@gmail.com>
2016-07-01 10:10:12 +0700

    update layouts index and  products
    update layouts index and  products

--- frontend/views/site/_popular_cat.php
+++ frontend/views/site/_popular_cat.php
@@ -7,12 +7,12 @@
                     <span class="sale">Sale</span>
                 </div>
                 <div class="price-label">715,00 $</div>-->
-        <a href="#">
+        <a href="<?php echo Yii::$app->homeUrl; ?>products?category=<?= $model->categoryId; ?>">
             <img src="<?php echo Yii::$app->homeUrl . $model->image; ?>" alt="1"/>
             <span class="tile-overlay"></span>
         </a>
         <div class="footer">
-            <a href="#"><?= $model->title; ?></a>
+            <a href="<?php echo Yii::$app->homeUrl; ?>products?category=<?= $model->categoryId; ?>"><?= $model->title; ?></a>
             <!--<span>by David Banks</span>-->
             <!--<button class="btn btn-primary">Add to Cart</button>-->
         </div>
--- frontend/views/site/_save_cat.php
+++ frontend/views/site/_save_cat.php
@@ -2,7 +2,7 @@
 
 <!--Category-->
 <div class="category col-lg-2 col-md-2 col-sm-4 col-xs-6">
-    <a href="#">
+    <a href="<?php echo Yii::$app->homeUrl; ?>products??category=<?= $model->categoryId; ?>">
         <img src="<?php echo Yii::$app->homeUrl . $model->image; ?>" alt="1"/>
         <p><?= $model->title; ?></p>
     </a>
