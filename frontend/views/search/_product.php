3f2f6c10337e9e5f19f58edffa00dc14bff1788c
Pew CodesLoves <sodapew17@gmail.com>
2016-07-05 09:36:20 +0700

    update

--- frontend/views/search/_product.php
+++ frontend/views/search/_product.php
@@ -5,10 +5,12 @@ $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
 <!--Tile-->
 <div id="products-category-search" class="col-lg-4 col-md-6 col-sm-12">
     <div class="tile">
-        <div class="badges">
-            <span class="sale">Sale</span>
+        <div class="badges-price">
+            <div class="badges">
+                <span class="sale">Sale</span>
+            </div>
+            <div class="price-label"><?= isset($model->productOnePrice) ? $model->productOnePrice->price : "Not Set"; ?> ฿</div>
         </div>
-        <div class="price-label"><?= isset($model->productOnePrice) ? $model->productOnePrice->price : "Not Set"; ?> ฿</div>
         <a href="<?php echo Yii::$app->homeUrl; ?>products?productId=<?= $model->productId ?>">
             <?php if (isset($model->productImages[0]->image) && !empty($model->productImages[0]->image)): ?>
                 <img src="<?php echo Yii::$app->homeUrl . $model->productImages[0]->image; ?>" alt="1"/>
