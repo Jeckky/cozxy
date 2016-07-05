45c53ccadfc202f2fa532607d01e5d4e9df8a68c
Pew CodesLoves <sodapew17@gmail.com>
2016-07-05 16:23:51 +0700

    update

--- frontend/views/products/product_catalog_item.php
+++ frontend/views/products/product_catalog_item.php
@@ -68,7 +68,7 @@ $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
         foreach ($model->productPrices as $pp) {
             ?>
             <div  class="col-lg-2 col-md-2 col-sm-12 " style="float: left; padding-right: 0px; padding-left: 0px;">
-                <table id="pp<?= $pp->quantity ?>" class="col-lg-12 col-md-12 text-center <?= ($i == 0) ? " priceActive" : " " ?>" style="font-size: 14px; border: 1px #f5f5f5 solid;">
+                <table id="pp<?= $pp->quantity ?>" class="col-lg-12 col-md-12 text-center <?= ($i == 0) ? " priceActive" : " " ?>" style="font-size: 14px; border: 1px #f5f5f5 solid; color: #000;">
 
                     <thead style="border-bottom: 1px #f5f5f5 solid;">
                         <tr>
