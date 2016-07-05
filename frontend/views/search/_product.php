7c9240f95b1df9953e7215e6e8c7b4ed359f25b2
Pew CodesLoves <sodapew17@gmail.com>
2016-07-05 10:20:19 +0700

    update styles
    1. search category

--- frontend/views/search/_product.php
+++ frontend/views/search/_product.php
@@ -5,13 +5,13 @@ $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
 <!--Tile-->
 <div id="products-category-search" class="col-lg-4 col-md-6 col-sm-12">
     <div class="tile">
-        <div class="badges-price">
+        <div class="search-category-badges-price">
             <div class="badges">
                 <span class="sale">Sale</span>
             </div>
             <div class="price-label"><?= isset($model->productOnePrice) ? $model->productOnePrice->price : "Not Set"; ?> ฿</div>
         </div>
-        <a href="<?php echo Yii::$app->homeUrl; ?>products?productId=<?= $model->productId ?>">
+        <a href="<?php echo Yii::$app->homeUrl; ?>products?productId=<?= $model->productId ?>" style="min-height: 210px; max-height: 210px;">
             <?php if (isset($model->productImages[0]->image) && !empty($model->productImages[0]->image)): ?>
                 <img src="<?php echo Yii::$app->homeUrl . $model->productImages[0]->image; ?>" alt="1"/>
             <?php else: ?>
@@ -20,7 +20,7 @@ $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
             <span class="tile-overlay"></span>
         </a>
         <div class="footer search-category-footer">
-            <a href="<?php echo Yii::$app->homeUrl; ?>products?productId=<?= $model->productId ?>" style="word-wrap: break-word;"><?= $model->title; ?></a>
+            <a href="<?php echo Yii::$app->homeUrl; ?>products?productId=<?= $model->productId ?>"><?= $model->title; ?></a>
             <span><?//= $model->shortDescription; ?></span>
             <a href="<?php echo Yii::$app->homeUrl; ?>cart"><button class="btn btn-primary">Add to Cart</button></a>
         </div>
