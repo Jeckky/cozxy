c107bdffea7cef18019320c46c92839f02822f08
Pew CodesLoves <sodapew17@gmail.com>
2016-07-06 15:54:41 +0700

    update

--- frontend/views/checkout/address_new.php
+++ frontend/views/checkout/address_new.php
@@ -8,9 +8,8 @@ $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@app/themes/costfit/
 $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
 ?>
 
-<div class="col-md-12"> 
+<div class="col-md-12">
     <div class="form-group">
-        <label class="sr-only" for="order-notes">Order notes</label>
-        <textarea class="form-control" name="order-notes" id="order-notes" rows="4" placeholder="Order notes"></textarea>
+        <textarea class="form-control" name="order-notes" id="order-notes" rows="4" placeholder="New Address"></textarea>
     </div>
 </div>
