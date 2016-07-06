55b770553994c163c8097660f8c1eb5a8913e8db
Pew CodesLoves <sodapew17@gmail.com>
2016-07-06 11:56:40 +0700

    update layouts
    1.  register/thank
    2.  register/forgot?

--- frontend/views/register/register_forgot.php
+++ frontend/views/register/register_forgot.php
@@ -11,23 +11,33 @@ $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
 <!--Breadcrumbs-->
 <ol class="breadcrumb">
     <li><a href="<?php echo Yii::$app->homeUrl; ?>"><?= isset($this->context->subTitle) ? $this->context->subTitle : "sub Title" ?></a></li>
-    <li><a href="<?php echo Yii::$app->homeUrl . Yii::$app->controller->id; ?>"><?= isset($this->context->subSubTitle) ? $this->context->subSubTitle : "sub sub Title" ?></a></li>
+    <li>
+        <a href="<?php echo Yii::$app->homeUrl . Yii::$app->controller->id; ?>">
+            <?= isset($this->context->subSubTitle) ? $this->context->subSubTitle : "sub sub Title" ?>
+        </a>
+    </li>
 </ol><!--Breadcrumbs Close-->
 
-<!--Support-->
-<section class="support">
-    <div class="container">
-        <div class="row">
+<!--Content-->
+<div class="coming-soon">
 
-            <!--Left Column-->
-            <div class="col-lg-12 col-md-12">
-                <h2 class="title"><?= isset($this->context->subTitle) ? $this->context->subTitle : "sub Title" ?></h2>
-                <div class="row space-top">
-                    <div class="clo-lg-3 col-md-3 col-sm-3 space-bottom">
-                        Test  
+    <section class="container">
+        <div class="row space-top">
+            <div class="col-lg-3 col-md-3 col-sm-3">&nbsp;</div>
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                <h3 class="cs-heading"><?= isset($this->context->subSubTitle) ? $this->context->subSubTitle : "sub sub Title" ?></h3>
+                <form class="space-bottom" role="form" method="post">
+
+                    <div class="form-group">
+                        <label for="cs-password">Password</label>
+                        <input type="password" class="form-control" id="cs-password" placeholder="Password">
                     </div>
-                </div>
+
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
             </div>
+            <div class="col-lg-3 col-md-3 col-sm-3">&nbsp;</div>
         </div>
-    </div>
-</section><!--Support Close-->
+    </section>
+
+</div><!--Content Close-->
