2bf428f9a172d59705d44fee829e87d4a20b30cd
Pew CodesLoves <sodapew17@gmail.com>
2016-07-06 10:38:27 +0700

    update layouts and sub menu
    1. Controller Profile
    2. Controller Payment
    3. Nav sub menu

---
+++ frontend/controllers/ProfileController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace frontend\controllers;
+
+use Yii;
+use yii\base\InvalidParamException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use frontend\models\PasswordResetRequestForm;
+use frontend\models\ResetPasswordForm;
+use frontend\models\SignupForm;
+use frontend\models\ContactForm;
+
+/**
+ * Checkout controller
+ */
+class ProfileController extends MasterController {
+
+    public $enableCsrfValidation = false;
+
+    /**
+     * Displays homepage.
+     *
+     * @return mixed
+     */
+    public function actionIndex() {
+
+        $this->title = 'Cost.fit | My Profile';
+        $this->subTitle = 'Home';
+        $this->subSubTitle = "My Profile";
+        return $this->render('profile');
+    }
+
+}
