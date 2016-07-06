dccffbb917098b4929bbe5e86f491e62f025f64b
Pew CodesLoves <sodapew17@gmail.com>
2016-07-06 09:43:25 +0700

    update view checkouts

--- frontend/views/checkout/checkout.php
+++ frontend/views/checkout/checkout.php
@@ -17,36 +17,7 @@ $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
                 <a class="panel-toggle" href="#login"><i></i>Returning customer? Click here to login</a>
                 <div class="row">
                     <div class="hidden-panel" id="login">
-                        <div class="col-lg-12">
-                            <div class="row">
-                                <div class="col-lg-7 col-md-7 col-sm-7">
-                                    <form class="login-form" method="post">
-                                        <div class="form-group group">
-                                            <label for="log-email2">Email</label>
-                                            <input type="email" class="form-control" name="log-email2" id="log-email2" placeholder="Enter your email" required>
-                                            <a class="help-link" href="#">Forgot email?</a>
-                                        </div>
-                                        <div class="form-group group">
-                                            <label for="log-password2">Password</label>
-                                            <input type="text" class="form-control" name="log-password2" id="log-password2" placeholder="Enter your password" required>
-                                            <a class="help-link" href="#">Forgot password?</a>
-                                        </div>
-                                        <div class="checkbox">
-                                            <label><input type="checkbox" name="remember"> Remember me</label>
-                                        </div>
-                                        <input class="btn btn-primary" type="submit" value="Login">
-                                    </form>
-                                </div>
-                                <div class="col-lg-5 col-md-5 col-sm-5">
-                                    <label>Use social accounts</label>
-                                    <div class="social-login">
-                                        <a class="facebook" href="#"><i class="fa fa-facebook-square"></i></a>
-                                        <a class="google" href="#"><i class="fa fa-google-plus-square"></i></a>
-                                        <a class="twitter" href="#"><i class="fa fa-twitter-square"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <?php echo $this->render('@app/views/register/form_login'); ?>
                     </div>
                 </div>
                 <a class="panel-toggle" href="#coupon"><i></i>Have a coupon? Click here to enter your code</a>
---
+++ frontend/views/register/form_login.php
@@ -0,0 +1,30 @@
+<div class="col-lg-12">
+    <div class="row">
+        <div class="col-lg-7 col-md-7 col-sm-7">
+            <form class="login-form" method="post">
+                <div class="form-group group">
+                    <label for="log-email2">Email</label>
+                    <input type="email" class="form-control" name="log-email2" id="log-email2" placeholder="Enter your email" required>
+                    <a class="help-link" href="#">Forgot email?</a>
+                </div>
+                <div class="form-group group">
+                    <label for="log-password2">Password</label>
+                    <input type="text" class="form-control" name="log-password2" id="log-password2" placeholder="Enter your password" required>
+                    <a class="help-link" href="#">Forgot password?</a>
+                </div>
+                <div class="checkbox">
+                    <label><input type="checkbox" name="remember"> Remember me</label>
+                </div>
+                <input class="btn btn-primary" type="submit" value="Login">
+            </form>
+        </div>
+        <div class="col-lg-5 col-md-5 col-sm-5">
+            <label>Use social accounts</label>
+            <div class="social-login">
+                <a class="facebook" href="#"><i class="fa fa-facebook-square"></i></a>
+                <a class="google" href="#"><i class="fa fa-google-plus-square"></i></a>
+                <a class="twitter" href="#"><i class="fa fa-twitter-square"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
