245a420704f42dde16ed3408fb074db89d2aec89
Pew CodesLoves <sodapew17@gmail.com>
2016-07-07 16:28:50 +0700

    update form emil

--- common/mail/register_confirm.php
+++ common/mail/register_confirm.php
@@ -1,13 +1,72 @@
-<html>
-    <head></head>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <!--
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+
+        <link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        -->
+    </head>
     <body>
-        <b>COST.FIT</b>
+        <style type="text/css">
+            .title{
+                color: #fff;
+            }
+            .main{
+                text-align: center;
+                width:100%;
+                background-color: #ffffff;
 
-        <p>ขอยคุณที่ลงทะเบียนกับ COST.FIT</p>
+            }
+            .main .main-leyouts{
+                /*padding: 50px;*/
+                background-color: #f5f5f5;
 
-        <p><b>กรุณากด</b> <a href="<?php echo $url ?>"><?php echo $url ?></a> เพื่อดู ยืนยันการสมัคร </p>
+            }
+            .head{
+                background-color: #03a9f4;
+                padding: 20px;
+                text-align: left;
+            }
+            .content{
+                background-color: #ffffff;
+                padding: 20px;
+                text-align: left;
+                background-image: url("https://scontent.fbkk10-1.fna.fbcdn.net/v/t1.0-9/13310617_1276754799019296_4526760882475392005_n.jpg?oh=8f19d7cd558266d1673bdf347ab2d63c&oe=58027A2B") ;
+                background-repeat: repeat;
+                height: 300px;
+            }
+            .foorter{
+                background-color: #03a9f4;
+                padding: 20px;
+                text-align: left;
+                line-height: 25px;
+            }
+        </style>
 
-        <p>อีเมลฉบับนี้เป็นการแจ้งข้อมูลโดยอัตโนมัติ กรุณาอย่าตอบกลับ</p>
+        <div class="main">
+            <div class="main-leyouts">
+                <div class="head title">
+                    COST.FIT
+                </div>
+                <div class="content">
+                    <p>ขอบคุณที่ลงทะเบียนกับ COST.FIT</p>
+
+                    <p><b>กรุณากด</b> <a href="<?php echo $url ?>"><?php echo $url ?></a> เพื่อดู ยืนยันการสมัคร </p>
+
+                    <p>อีเมลฉบับนี้เป็นการแจ้งข้อมูลโดยอัตโนมัติ กรุณาอย่าตอบกลับ</p>
+                </div>
+                <div class="foorter title">
+                    Daii Group Public Company Limited
+                    <br>
+                    T : 02-938-3464 <br>
+                    F : 02-938-3463
+                </div>
+            </div>
+        </div>
+
     </body>
 </html>
 
