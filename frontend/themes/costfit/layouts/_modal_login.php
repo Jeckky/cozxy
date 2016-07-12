f441393312b9440d2e73b59d0f8e1349a7da0910
kamyjapkung <kamyjap@gmail.com>
2016-07-12 15:59:25 +0700

    Show Email in cart comfirm modal

--- frontend/themes/costfit/layouts/_modal_login.php
+++ frontend/themes/costfit/layouts/_modal_login.php
@@ -55,7 +55,8 @@ $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
             </div>
             <div class="modal-footer" style="border-top: 1px solid #03a9f4;">
                 <div id="div-login-msg ">
-                    <h4 class="text-center">คุณใช่ <span class="email">test@gmail.com</span> หรือไม่ </h4>
+                    <?php $order = common\models\costfit\Order::getOrder(); ?>
+                    <h4 class="text-center">คุณใช่ <span class="email"><?= isset($order) ? $order->user->email : "-" ?></span> หรือไม่ </h4>
                 </div>
                 <div class="text-center">
                     <button id="login_lost_btn" type="button" class="yes btn btn-primary">Yes</button>
