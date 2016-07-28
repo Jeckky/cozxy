91248dc0ee776e0cf4ad190e027aadef176b08fe
Pew CodesLoves <sodapew17@gmail.com>
2016-07-28 09:51:34 +0700

    menu

--- frontend/themes/costfit/layouts/_nav_main_menu.php
+++ frontend/themes/costfit/layouts/_nav_main_menu.php
@@ -8,7 +8,7 @@ $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@app/themes/costfit/
 $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
 ?>
 <ul class="main">
-    <li class="has-submenu"><a href="<?php echo $baseUrl; ?>"><i class="fa fa-chevron-down"></i></a>
+    <li class="has-submenu"><a href="<?php echo $baseUrl; ?>"><span>H</span>ome<i class="fa fa-chevron-down"></i></a>
         <!--Class "has-submenu" for proper highlighting and dropdown <span>H</span>ome-->
     </li>
 </ul>
