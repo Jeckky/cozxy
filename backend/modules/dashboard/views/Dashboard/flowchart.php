cd1f9e58b45f696f7e8146d6c4b11b556b4843a9
Pew CodesLoves <sodapew17@gmail.com>
2016-08-02 09:58:54 +0700

    update flowchart frontend

--- backend/modules/dashboard/views/Dashboard/flowchart.php
+++ backend/modules/dashboard/views/Dashboard/flowchart.php
@@ -4,10 +4,13 @@
         if ($id == 'order') {
             ?>
             <iframe frameborder="0" style="width:100%;height:1635px" src="https://www.draw.io/?chrome=0&lightbox=1&edit=https%3A%2F%2Fwww.draw.io%2F%23G0B9JTDqsqskF6cWowdUo3aWtzX00&nav=1#G0B9JTDqsqskF6cWowdUo3aWtzX00"></iframe>
+            <?php
+        } else if ($id == 'frontOrder') {
+            ?>
+
+            //Place Frontend Order Embed Iframe
+            <iframe frameborder = "0" style = "width:100%;height:1635px" src = "https://www.draw.io/?chrome=0&lightbox=1&edit=https%3A%2F%2Fwww.draw.io%2F%23G0B2rFICQsYdtIbVdVQnBaVWIzaTQ&nav=1#G0B2rFICQsYdtIbVdVQnBaVWIzaTQ"></iframe>
                 <?php
-            } else if ($id == 'frontOrder') {
-                //Place Frontend Order Embed Iframe
-                //Place Frontend Order Embed Iframe
             }
             ?>
     </div>
