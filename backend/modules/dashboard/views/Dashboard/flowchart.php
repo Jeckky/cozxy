8e0fb98827be2d3d97f2254248eb68c27122229d
kamyjapkung <kamyjap@gmail.com>
2016-08-03 10:50:02 +0700

    qc & sortproduct chart

--- backend/modules/dashboard/views/Dashboard/flowchart.php
+++ backend/modules/dashboard/views/Dashboard/flowchart.php
@@ -16,6 +16,10 @@
         } else if ($id == 'qc') { //Place Frontend Order Embed Iframe
             ?>
             <iframe frameborder="0" style="width:100%;height:1943px" src="https://www.draw.io/?chrome=0&lightbox=1&edit=https%3A%2F%2Fwww.draw.io%2F%23G0B9JTDqsqskF6RFBOT0FNNlE3NGc&nav=1#G0B9JTDqsqskF6RFBOT0FNNlE3NGc"></iframe>
+            <?php
+        } else if ($id == 'sortProduct') { //Place Frontend Order Embed Iframe
+            ?>
+            <iframe frameborder="0" style="width:100%;height:1943px" src="https://www.draw.io/?chrome=0&lightbox=1&edit=https%3A%2F%2Fwww.draw.io%2F%23G0B9JTDqsqskF6MFlQMkFsbXQ0VDQ&nav=1#G0B9JTDqsqskF6MFlQMkFsbXQ0VDQ"></iframe>
                 <?php
             }
             ?>
