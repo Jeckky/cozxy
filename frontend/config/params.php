f5167199bb240620eafaf825fc2595e5c6eaa812
kamyjapkung <kamyjap@gmail.com>
2016-08-15 14:29:39 +0700

    e payment server type

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -3,5 +3,6 @@
 return [
     'adminEmail' => 'admin@example.com',
     'secureKey' => 'S-UWwuv9Y9Ec_U-r7XVJHh3oC0BhdN0T',
-    'secureVi' => 'cost.fit'
+    'secureVi' => 'cost.fit',
+    'ePaymentServerType' => 1
 ];
