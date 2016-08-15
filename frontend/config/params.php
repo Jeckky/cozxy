d0405d49af35ba992028939573db851e5afa76b6
Kurtumm <npr@NPR.local>
2016-08-15 11:57:20 +0700

    change encrype and decrypt url

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -1,5 +1,6 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
-    'secureKey' => 'S-UWwuv9Y9Ec_U-r7XVJHh3oC0BhdN0T'
+    'secureKey' => 'S-UWwuv9Y9Ec_U-r7XVJHh3oC0BhdN0T',
+    'secureVi' => 'cost.fit'
 ];
