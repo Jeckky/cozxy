37826a61f1f85626d76775a849e78fc0d57d266b
Pew CodesLoves <sodapew17@gmail.com>
2016-08-18 15:30:36 +0700

    Hidden Form Term & Condition
    - View Product

--- frontend/views/products/product_tabs_widget.php
+++ frontend/views/products/product_tabs_widget.php
@@ -119,10 +119,10 @@ $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
     <div class="container">
         <div class="row">
             <!--<div class="col-lg-4 col-md-5 col-sm-5">
-                <img class="center-block" src="<?php //echo $directoryAsset;           ?>/img/posts-widget/1.jpg" alt="Description"/>
+                <img class="center-block" src="<?php //echo $directoryAsset;              ?>/img/posts-widget/1.jpg" alt="Description"/>
             </div>-->
             <div class="col-lg-12 col-md-12 col-sm-12">
-<!--                <p class="p-style2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.</p>
+                <!--                <p class="p-style2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.</p>
                 <div class="row">
                     <div class="col-lg-3 col-md-4 col-sm-5 col-xs-6">
                         <h4>Unordered list</h4>
@@ -152,24 +152,24 @@ $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
     <div class="container">
         <div class="row">
             <!--Disqus Comments Plugin-->
-            <div class="col-lg-10 col-lg-offset-1">
+            <!--<div class="col-lg-10 col-lg-offset-1">
                 <div id="disqus_thread"></div>
                 <script type="text/javascript">
                     /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-                    var disqus_shortname = '8guild'; // required: replace example with your forum shortname
+                    // var disqus_shortname = '8guild'; // required: replace example with your forum shortname
 
                     /* * * DON'T EDIT BELOW THIS LINE * * */
-                    (function () {
-                        var dsq = document.createElement('script');
-                        dsq.type = 'text/javascript';
-                        dsq.async = true;
-                        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-                        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-                    })();
+                    //(function () {
+                    //var dsq = document.createElement('script');
+                    // dsq.type = 'text/javascript';
+                    // dsq.async = true;
+                    // dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                    // (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                    //})();
                 </script>
                 <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
                 <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
-            </div>
+            </div>-->
         </div>
     </div>
 </div>
