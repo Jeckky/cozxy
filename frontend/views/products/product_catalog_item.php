a5e38cfad77d19b6a410c216ba786146caf83d96
Pew CodesLoves <sodapew17@gmail.com>
2016-08-19 10:51:36 +0700

    edit form product
    - update form

--- frontend/views/products/product_catalog_item.php
+++ frontend/views/products/product_catalog_item.php
@@ -23,6 +23,14 @@ $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
         background-color: rgba(51,204,204,.15);
         color: purple;
     }
+    .checkbox label {
+        min-height: 1em;
+        padding-left: 10px;
+        margin-bottom: 0;
+        font-weight: normal;
+        cursor: pointer;
+    }
+
 </style>
 <div class="col-lg-6 col-md-6">
     <h1><?= $model->title; ?></h1>
@@ -56,26 +64,17 @@ $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
                 ");
                     ?>
                 <?php endif; ?>
-                                                                    <!--            <select name="size" id="changeOption">
-                                                                    <option>Size:28 Inches</option>
-                                                                    <option>Size:32 Inches</option>
-                                                                    <option>Size:40 Inches</option>
-                                                                    <option>Size:48 Inches</option>
-                                                                    <option>Size:50 Inches</option>
-                                                                    </select>-->
+                                                                                                                                                                                                                                                                <!--            <select name="size" id="changeOption">
+                                                                                                                                                                                                                                                                <option>Size:28 Inches</option>
+                                                                                                                                                                                                                                                                <option>Size:32 Inches</option>
+                                                                                                                                                                                                                                                                <option>Size:40 Inches</option>
+                                                                                                                                                                                                                                                                <option>Size:48 Inches</option>
+                                                                                                                                                                                                                                                                <option>Size:50 Inches</option>
+                                                                                                                                                                                                                                                                </select>-->
             </div>
         <?php endif; ?>
     </div>
-    <div class="buttons group products-buttons-group">
-        <div class="form-group">
-            <label for="shopping-cart" class="col-sm-1" style="padding-right: 0px;  padding-left: 0px;  margin-bottom: 0px;">
-                <img  src="<?php echo Yii::$app->homeUrl; ?>images/icon/1.png" alt="thumb" class="img-responsive"/>
-            </label>
-            <div class="col-sm-11 text-left" style="padding: 0px; margin-left: 0px; margin-top: 15px;">
-                &nbsp;FREE Shipping
-            </div>
-        </div>
-    </div>
+
     <div class="buttons group products-buttons-group">
         <div class="old-price"><?= (isset($model->price) && !empty($model->price)) ? number_format($model->price, 2) . " ฿" : "815,00 $" ?></div>
         <div class="price"><?= number_format($model->calProductPrice($model->productId, 1), 2) . " ฿" ?></div>
@@ -130,8 +129,32 @@ $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
         <a class="btn btn-primary btn-sm" id="addItemToCart" href="#" <?= ($model->findMaxQuantity($model->productId) <= 0) ? " disabled" : " " ?>><i class="icon-shopping-cart"></i>Add to cart</a>
         <a class="btn btn-black btn-sm" <?php if (\Yii::$app->user->isGuest == 1) { ?> id="GuestaddItemToWishlist" <?php } else { ?> id="addItemToWishlist" <?php } ?> href="#" <?= (\common\models\costfit\Wishlist::isExistingList($model->productId)) ? " disabled" : " " ?>><i class="icon-heart"></i>Add to wishlist</a>
     </div>
-    <p class="p-style2"><?//= strip_tags($model->shortDescription); ?></p>
+   <!-- <p class="p-style2"><?//= strip_tags($model->shortDescription); ?></p>-->
+    <div class="panel panel-warning">
+        <div class="panel-heading">
+            <h3 class="panel-title">ส่งสินค้า</h3>
+        </div>
+        <div class="panel-body">
+            <div class="form-group col-lg-12" style="margin-bottom:5px;">
+                <label for="shopping-cart" class="col-sm-1" style="padding-right: 0px;  padding-left: 0px;  margin-bottom: 0px;">
+                    <img  src="<?php echo Yii::$app->homeUrl; ?>images/icon/1.png" alt="thumb" class="img-responsive"/>
+                </label>
+                <div class="col-sm-11 text-left" style="padding: 0px; margin-left: 0px; margin-top: 15px;">
+                    &nbsp;ส่งสินค้าภานใน 7 วัน
+                </div>
+            </div>
+            <div class="form-group  col-lg-12">
+                <div class="checkbox form-group">
+                    <label>
+                        <input type="checkbox" name="sizes" value="blue" id="size_3">
+                        ต้องการส่งสินค้าราคาประหยัดอีก xxx  บาท (ส่งภายใน 15 วัน)
+                    </label>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="row">
+        <!--
         <div class="col-lg-4 col-md-4 col-sm-5">
             <h3>Tell friends</h3>
             <div class="social-links">
@@ -139,7 +162,7 @@ $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
                 <a href="#"><i class="fa fa-pinterest-square"></i></a>
                 <a href="#"><i class="fa fa-facebook-square"></i></a>
             </div>
-        </div>
+        </div>-->
         <div class="col-lg-8 col-md-8 col-sm-7">
             <h3>Tags</h3>
             <div class="tags">
@@ -149,9 +172,10 @@ $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
             </div>
         </div>
     </div>
+    <!--
     <div class="promo-labels">
+        <div data-content="This is a place for the unique commercial offer. Make it known."><i class="fa fa-truck"></i>Save for </div>
         <div data-content="This is a place for the unique commercial offer. Make it known."><i class="fa fa-truck"></i>Free delivery</div>
-        <div data-content="This is a place for the unique commercial offer. Make it known."><img src="<?php echo Yii::$app->homeUrl; ?>images/icon/Fast-Deliver-1.png" alt="cost.fit" style="height: 44px;">Fast Deliver</div>
         <div data-content="This is a place for the unique commercial offer. Make it known."><i class="fa fa-shield"></i>Safe Buy</div>
     </div>
 
@@ -162,10 +186,8 @@ $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
             <input id="sent-s" class="form-control" type="text" value="<?= ($model->findMaxQuantity($model->productId) == 0) ? 0 : 1 ?>">
             <a class="incr-btn" href="#" data-toggle="popover" data-content="Max Quantity For this Item" data-placement="bottom">+</a>
         </div>
-        <!-- <a class="btn btn-primary btn-sm" id="addItemToCart" href="#" <?= ($model->findMaxQuantity($model->productId) <= 0) ? " disabled" : " " ?>><i class="icon-shopping-cart"></i>Add to cart</a>
-        <a class="btn btn-black btn-sm" href="<?php echo Yii::$app->homeUrl; ?>wishlist"><i class="icon-heart"></i>Add to wishlist</a>-->
-    </div>
-
+     </div>
+    -->
 </div>
 
 
