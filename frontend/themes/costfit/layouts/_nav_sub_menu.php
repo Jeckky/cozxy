15baa9b888fb839d801a8fd234129b36fc77a2f5
Pew CodesLoves <sodapew17@gmail.com>
2016-08-19 11:14:08 +0700

    update

--- frontend/themes/costfit/layouts/_nav_sub_menu.php
+++ frontend/themes/costfit/layouts/_nav_sub_menu.php
@@ -46,23 +46,22 @@ $MenuCategory = $this->context->actionMenuCategory();
                 <?php
             }
             ?>
+        </li> 
+        <?php
+    }
+    ?>
+    <?php if (!Yii::$app->user->isGuest): ?>
+        <li class="has-submenu pill-right"><a href="#"><?= (Yii::$app->user->identity->email); ?><i class="fa fa-chevron-down"></i></a>
+            <ul class="submenu">
+                <li class="pill-right"><a href="<?php echo Yii::$app->homeUrl; ?>profile" style="font-size: 12px;">My Profile</a></li>
+                <li class="pill-right"><a href="<?php echo Yii::$app->homeUrl; ?>profile/order" style="font-size: 12px;">Order History</a></li>
+                <!--<li class="pill-right"><a href="<?php echo Yii::$app->homeUrl; ?>profile/payment">Payment Methods</a></li>
+                <li class="pill-right"><a href="<?php echo Yii::$app->homeUrl; ?>history">Easy Re-Order</a></li>-->
+            </ul>
         </li>
-    </li>
-    <?php
-}
-?>
-<?php if (!Yii::$app->user->isGuest): ?>
-    <li class="has-submenu pill-right"><a href="#"><?= (Yii::$app->user->identity->email); ?><i class="fa fa-chevron-down"></i></a>
-        <ul class="submenu">
-            <li class="pill-right"><a href="<?php echo Yii::$app->homeUrl; ?>profile" style="font-size: 12px;">My Profile</a></li>
-            <li class="pill-right"><a href="<?php echo Yii::$app->homeUrl; ?>profile/order" style="font-size: 12px;">Order History</a></li>
-            <!--<li class="pill-right"><a href="<?php echo Yii::$app->homeUrl; ?>profile/payment">Payment Methods</a></li>
-            <li class="pill-right"><a href="<?php echo Yii::$app->homeUrl; ?>history">Easy Re-Order</a></li>-->
-        </ul>
-    </li>
-<?php endif; ?>
-<li class="has-submenu pill-right"><a href="<?php echo Yii::$app->homeUrl; ?>coupon">Super special offers</a></li><!--ข้อเสนอพิเศษจากพาร์ทเนอร์-->
-<li class="has-submenu pill-right"><a href="<?php echo Yii::$app->homeUrl; ?>how-cost-fit-works">How Works</a></li>
+    <?php endif; ?>
+    <li class="has-submenu pill-right"><a href="<?php echo Yii::$app->homeUrl; ?>coupon">Super special offers</a></li><!--ข้อเสนอพิเศษจากพาร์ทเนอร์-->
+    <li class="has-submenu pill-right"><a href="<?php echo Yii::$app->homeUrl; ?>how-cost-fit-works">How Works</a></li>
 </ul>
 <!-- Show For Desktop -->
 <ul class="catalog hidden-xs" id="catalog_new" style="width: 100%;">
