4c9a91661c229dc4738ed45f2b495d10dcb4d2a5
kamyjapkung <kamyjap@gmail.com>
2016-09-02 16:28:53 +0700

    xxx

--- common/models/costfit/CategoryToProduct.php
+++ common/models/costfit/CategoryToProduct.php
@@ -16,31 +16,46 @@ use \common\models\costfit\master\CategoryToProductMaster;
  * @property string $createDateTime
  * @property string $updateDateTime
  */
-class CategoryToProduct extends \common\models\costfit\master\CategoryToProductMaster {
+class CategoryToProduct extends \common\models\costfit\master\CategoryToProductMaster
+{
 
     /**
      * @inheritdoc
      */
-    public function rules() {
+    public function rules()
+    {
         return array_merge(parent::rules(), []);
     }
 
     /**
      * @inheritdoc
      */
-    public function attributeLabels() {
+    public function attributeLabels()
+    {
         return array_merge(parent::attributeLabels(), []);
     }
 
-	/**
-	 * relation
-	 */
-	public function getProduct()
-	{
-		return $this->hasOne(Product::className(), ['productId'=>'productId']);
-	}
+    /**
+     * @inheritdoc
+     */
+    public function attributes()
+    {
+        return array_merge(parent::attributes(), [
+            'minPrice',
+            'maxPrice',
+        ]);
+    }
 
-    public static function saveCategoryToProduct($categoryId, $productId) {
+    /**
+     * relation
+     */
+    public function getProduct()
+    {
+        return $this->hasOne(Product::className(), ['productId' => 'productId']);
+    }
+
+    public static function saveCategoryToProduct($categoryId, $productId)
+    {
         $model = new CategoryToProduct();
         CategoryToProduct::deleteAll("productId = " . $productId);
 
