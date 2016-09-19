ef7b8aae420fe76bb2e5892f42dfc00cfa31a5e0
Pew CodesLoves <sodapew17@gmail.com>
2016-09-19 16:28:26 +0700

    Update  profile Picking Point is DEMO
    - Picking Point -> ADD
    - Picking Point -> Edit
    - Picking Point -> DELETE

--- frontend/views/profile/list_picking_point.php
+++ frontend/views/profile/list_picking_point.php
@@ -27,7 +27,7 @@ if ($model->isDefault == '1') {
     echo ($model->states['stateName']) ? $model->states['stateName'] : '' . ' ,';
     //echo '<br>' . ($model->countries['localName']) ? $model->countries['localName'] : '' . ' ,';
     //echo '<br>Zipcode ' . $model->zipcode;
-    echo '&nbsp;<a href="' . Yii::$app->homeUrl . 'profile/shipping-address/' . \common\models\ModelMaster::encodeParams(['addressId' => $model->addressId]) . '"><span style ="color:#b11010; cursor: hand;" >( <i class="fa fa-pencil-square-o" aria-hidden="true"></i> ,</span></a>';
+    echo '&nbsp;<a href="' . Yii::$app->homeUrl . 'profile/picking-point/' . \common\models\ModelMaster::encodeParams(['addressId' => $model->addressId]) . '"><span style ="color:#b11010; cursor: hand;" >( <i class="fa fa-pencil-square-o" aria-hidden="true"></i> ,</span></a>';
     echo '<span class= "obutton feature2"  data-id="' . $model->addressId . '" data-toggle="modal" data-target="#modal-delete-item">'
     . '<span style ="color:#b11010; cursor: hand;" class="get-shipping-address"> <i class="fa fa-trash" aria-hidden="true"></i> )</span></span>';
     echo $this->render('@frontend/views/modal/modal_delete_item');
