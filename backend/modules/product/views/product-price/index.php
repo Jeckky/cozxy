54c6d584a5540f733ac626ee458b1abcdbfe00bc
kamyjapkung <kamyjap@gmail.com>
2016-09-27 15:28:56 +0700

    xxx

--- backend/modules/product/views/product-price/index.php
+++ backend/modules/product/views/product-price/index.php
@@ -21,7 +21,7 @@ $this->params['pageHeader'] = Html::encode($this->title);
                 <div class="col-md-6"><?= $this->title ?></div>
                 <div class="col-md-6">
                     <div class="btn-group pull-right">
-                        <?= Html::a('<i class=\'glyphicon glyphicon-plus\'></i> Create Product Price', ['create'], ['class' => 'btn btn-success btn-xs']) ?>
+                        <?= Html::a('<i class=\'glyphicon glyphicon-plus\'></i> Create Product Price', ['create?productId=' . $_GET["productId"]], ['class' => 'btn btn-success btn-xs']) ?>
                     </div>
                 </div>
             </div>
@@ -122,80 +122,80 @@ $this->params['pageHeader'] = Html::encode($this->title);
                         'buttons' => [
                             'update' => function($url, $model, $id) {
                                 return Html::a("<span class='glyphicon glyphicon-pencil'></span>", ['/product/product-price-other-web/update', 'id' => $id], [
-                                            'title' => Yii::t('app', 'Edit'),]);
+                                    'title' => Yii::t('app', 'Edit'),]);
                             },
-                                    'delete' => function($url, $model, $id) {
+                            'delete' => function($url, $model, $id) {
                                 return Html::a("<span class='glyphicon glyphicon-trash'></span>", ['/product/product-price-other-web/delete', 'id' => $id], [
-                                            'title' => Yii::t('app', 'Delete'), 'data-confirm' => 'Are you sure to delete']);
+                                    'title' => Yii::t('app', 'Delete'), 'data-confirm' => 'Are you sure to delete']);
                             },]
-                            ],
-                        ],
-                    ]);
-                    ?>
+                    ],
+                ],
+            ]);
+            ?>
+        </div>
+    </div>
+    <?php Pjax::end(); ?>
+    <?php Pjax::begin(['id' => 'g3']); ?>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <div class="row">
+                <div class="col-md-6">Product Shipping Price</div>
+                <div class="col-md-6">
+                    <div class="btn-group pull-right">
+                        <?=
+                        Html::a('<i class=\'glyphicon glyphicon-plus\'></i> Create Product Shipping Price', ['product-shipping-price/create?id=' . $id], ['class' => 'btn btn-primary btn-xs'])
+                        ?>
+                    </div>
                 </div>
             </div>
-            <?php Pjax::end(); ?>
-            <?php Pjax::begin(['id' => 'g3']); ?>
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <div class="row">
-                        <div class="col-md-6">Product Shipping Price</div>
-                        <div class="col-md-6">
-                            <div class="btn-group pull-right">
-                                <?=
-                                Html::a('<i class=\'glyphicon glyphicon-plus\'></i> Create Product Shipping Price', ['product-shipping-price/create?id=' . $id], ['class' => 'btn btn-primary btn-xs'])
-                                ?>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="panel-body">
-                    <?=
-                    GridView::widget([
-                        'layout' => "{summary}\n{pager}\n{items}\n{pager}\n",
-                        'dataProvider' => $dataProviderPrice,
-                        'pager' => [
-                            'options' => ['class' => 'pagination pagination-xs']
-                        ],
-                        'options' => [
-                            'class' => 'table-light'
-                        ],
-                        'columns' => [
-                            ['class' => 'yii\grid\SerialColumn'],
-                            [
-                                'attribute' => 'productId',
-                                'value' => function($model) {
-                                    return isset($model->product) ? $model->product->title : NULL;
-                                }
-                            ],
-                            ['attribute' => 'shippingTypeId',
-                                'value' => function($model) {
-                                    return $model->shippingType->title;
-                                }
-                            ],
-                            'discount',
-                            ['attribute' => 'type',
-                                'value' => function($model) {
-                                    return $model->types;
-                                }
-                            ],
-                            ['class' => 'yii\grid\ActionColumn',
-                                'header' => 'Actions',
-                                'template' => '{update} {delete}',
-                                'buttons' => [
-                                    'update' => function($url, $model, $id) {
-                                        return Html::a("<span class='glyphicon glyphicon-pencil'></span>", ['/product/product-shipping-price/update', 'id' => $id], [
-                                                    'title' => Yii::t('app', 'Edit'),]);
-                                    },
-                                            'delete' => function($url, $model, $id) {
-                                        return Html::a("<span class='glyphicon glyphicon-trash'></span>", ['/product/product-shipping-price/delete', 'id' => $id], [
-                                                    'title' => Yii::t('app', 'Delete'), 'data-confirm' => 'Are you sure to delete']);
-                                    },]
-                                    ],
-                                ],
-                            ]);
-                            ?>
-                        </div>
-                    </div>
-                    <?php Pjax::end(); ?>
+        </div>
+        <div class="panel-body">
+            <?=
+            GridView::widget([
+                'layout' => "{summary}\n{pager}\n{items}\n{pager}\n",
+                'dataProvider' => $dataProviderPrice,
+                'pager' => [
+                    'options' => ['class' => 'pagination pagination-xs']
+                ],
+                'options' => [
+                    'class' => 'table-light'
+                ],
+                'columns' => [
+                    ['class' => 'yii\grid\SerialColumn'],
+                    [
+                        'attribute' => 'productId',
+                        'value' => function($model) {
+                            return isset($model->product) ? $model->product->title : NULL;
+                        }
+                    ],
+                    ['attribute' => 'shippingTypeId',
+                        'value' => function($model) {
+                            return $model->shippingType->title;
+                        }
+                    ],
+                    'discount',
+                    ['attribute' => 'type',
+                        'value' => function($model) {
+                            return $model->types;
+                        }
+                    ],
+                    ['class' => 'yii\grid\ActionColumn',
+                        'header' => 'Actions',
+                        'template' => '{update} {delete}',
+                        'buttons' => [
+                            'update' => function($url, $model, $id) {
+                                return Html::a("<span class='glyphicon glyphicon-pencil'></span>", ['/product/product-shipping-price/update', 'id' => $id], [
+                                    'title' => Yii::t('app', 'Edit'),]);
+                            },
+                            'delete' => function($url, $model, $id) {
+                                return Html::a("<span class='glyphicon glyphicon-trash'></span>", ['/product/product-shipping-price/delete', 'id' => $id], [
+                                    'title' => Yii::t('app', 'Delete'), 'data-confirm' => 'Are you sure to delete']);
+                            },]
+                    ],
+                ],
+            ]);
+            ?>
+        </div>
+    </div>
+    <?php Pjax::end(); ?>
 </div>
