995475f94aa6c85dfbb9a63b2d8d1a9bf0101fe8
kamyjapkung <kamyjap@gmail.com>
2016-09-28 07:52:55 +0700

    show product image in product detail

--- frontend/views/products/_product_image.php
+++ frontend/views/products/_product_image.php
@@ -1,10 +1,10 @@
 <?php
 $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@app/themes/costfit/assets');
-$baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
+//$baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
 ?>
 <div class="prod-gal master-slider" id="prod-gal">
     <?php
-//    throw new \yii\base\Exception($model->productImages[0]->imageThumbnail1);
+//    throw new \yii\base\Exception(count($model->productImages));
     foreach ($model->productImages as $image) {
         // รูปภาพ default : /cost.fit/assets/img/catalog/product-gallery/th_1.jpg
         ?>
@@ -12,16 +12,18 @@ $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
             <img src="<?php echo Yii::$app->homeUrl . $image->image; ?>" data-src="<?php echo Yii::$app->homeUrl . $image->image; ?>" alt="<?= $image->title ?>" class="img-responsive img-thumbnail"/>
             <?php
             //echo 'imageThumbnail2 : ' . count($image->imageThumbnail2);
+//            throw new \yii\base\Exception(Yii::$app->basePath . "/web/" . $image->imageThumbnail1);
+//            throw new \yii\base\Exception(Yii::$app->homeUrl);
             if (isset($image->imageThumbnail1) && !empty($image->imageThumbnail1)) {
-                $filename = $image->imageThumbnail1;
-                if (file_exists(Yii::$app->basePath . $filename)) {
-                    echo "<img class=\"ms-thumb\" src=\" " . $baseUrl . $image->imageThumbnail1 . "  \" alt=\"1\" class=\"img-responsive img-thumbnail\"/>";
+                if (file_exists(Yii::$app->basePath . "/web/" . $image->imageThumbnail1)) {
+                    echo "<img class=\"ms-thumb\" src=\" " . Yii::$app->homeUrl . $image->imageThumbnail1 . "  \" alt=\"1\" class=\"img-responsive img-thumbnail\"/>";
                 } else {
-                    echo "<img  class=\"ms-thumb\"  src=\"" . $baseUrl . "/images/ContentGroup/DUHWYsdXVc.png\" alt=\"1\" width=\"137\" height=\"130\" class=\"img-responsive img-thumbnail\"/>";
+                    echo "<img  class=\"ms-thumb\"  src=\"" . Yii::$app->homeUrl . "images/ContentGroup/DUHWYsdXVc.png\" alt=\"1\" width=\"137\" height=\"130\" class=\"img-responsive img-thumbnail\"/>";
                 }
             } else {
+                throw new \yii\base\Exception(111);
                 ?>
-                <img class="ms-thumb" src="<?php echo $baseUrl . "/images/ContentGroup/DUHWYsdXVc.png"; ?>" alt="1" width="137" height="130" class="img-responsive img-thumbnail"/>
+                <img class="ms-thumb" src="<?php echo Yii::$app->homeUrl . "/images/ContentGroup/DUHWYsdXVc.png"; ?>" alt="1" width="137" height="130" class="img-responsive img-thumbnail"/>
             <?php } ?>
         </div>
         <?php
