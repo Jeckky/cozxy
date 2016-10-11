49f0814bbcc5f0b6edd40489032e5cf0cc43aba2
sakqqboy <saknakhngam@hotmail.com>
2016-10-11 15:00:43 +0700

    update

--- backend/modules/report/controllers/ReportController.php
+++ backend/modules/report/controllers/ReportController.php
@@ -11,14 +11,6 @@ use common\models\costfit\Order;
 class ReportController extends ReportMasterController {
 
     public function actionIndex() {
-//        $a = substr('2016-10-11 17:24:16', 0, -9);
-//        $b = substr('2016-08-23 17:24:16', 0, -9);
-//        if ($a > $b) {
-//            throw new \yii\base\Exception('a>b');
-//        } else {
-//            throw new \yii\base\Exception('b>a');
-//        }
-        //throw new \yii\base\Exception($a);
         if (isset($_GET['fromDate']) && !empty($_GET['fromDate'])) {
             if (isset($_GET['toDate']) && !empty($_GET['toDate'])) {
                 $model = Order::find()->where("paymentDateTime BETWEEN '" . $_GET['fromDate'] . "' and '" . $_GET['toDate'] . "' and status>=5 order by paymentDateTime DESC")->all();
