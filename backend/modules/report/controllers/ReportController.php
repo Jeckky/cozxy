1f6a453b3ab3002b65fb00db95b408f816584de6
sakqqboy <saknakhngam@hotmail.com>
2016-10-11 14:54:24 +0700

    update

--- backend/modules/report/controllers/ReportController.php
+++ backend/modules/report/controllers/ReportController.php
@@ -11,16 +11,24 @@ use common\models\costfit\Order;
 class ReportController extends ReportMasterController {
 
     public function actionIndex() {
-
-        if (isset($_GET['fromDate'])) {
-            if (isset($_GET['toDate'])) {
-                $model = Order::find()->where("substr(paymentDateTime,0,-9)>='" . $_GET['fromDate'] . "' or substr(paymentDateTime,0,-9)<='" . $_GET['toDate'] . "' and status>=5 order by paymentDateTime DESC")->all();
+//        $a = substr('2016-10-11 17:24:16', 0, -9);
+//        $b = substr('2016-08-23 17:24:16', 0, -9);
+//        if ($a > $b) {
+//            throw new \yii\base\Exception('a>b');
+//        } else {
+//            throw new \yii\base\Exception('b>a');
+//        }
+        //throw new \yii\base\Exception($a);
+        if (isset($_GET['fromDate']) && !empty($_GET['fromDate'])) {
+            if (isset($_GET['toDate']) && !empty($_GET['toDate'])) {
+                $model = Order::find()->where("paymentDateTime BETWEEN '" . $_GET['fromDate'] . "' and '" . $_GET['toDate'] . "' and status>=5 order by paymentDateTime DESC")->all();
             } else {
-                $model = Order::find()->where("substr(paymentDateTime,0,-9)>='" . $_GET['fromDate'] . "' or status>=5 order by paymentDateTime DESC")->all();
+                ;
+                $model = Order::find()->where("paymentDateTime>='" . $_GET['fromDate'] . "' and status>=5 order by paymentDateTime DESC")->all();
             }
         } else {
-            if (isset($_GET['toDate'])) {
-                $model = Order::find()->where("substr(paymentDateTime,0,-9)>='" . $_GET['fromDate'] . "' or substr(paymentDateTime,0,-9)<='" . $_GET['toDate'] . "' and status>=5 order by paymentDateTime DESC")->all();
+            if (isset($_GET['toDate']) && !empty($_GET['toDate'])) {
+                $model = Order::find()->where("paymentDateTime<='" . $_GET['toDate'] . "' and status>=5 order by paymentDateTime DESC")->all();
             } else {
                 $model = Order::find()->where("status>=5 order by paymentDateTime DESC")->all();
             }
