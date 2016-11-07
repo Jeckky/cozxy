08fbd6b05bda4c38080e348a49e3b75e7351760f
sakqqboy <saknakhngam@hotmail.com>
2016-11-07 16:30:14 +0700

    update picking

--- common/models/costfit/OrderItem.php
+++ common/models/costfit/OrderItem.php
@@ -21,8 +21,7 @@ use \common\models\costfit\master\OrderItemMaster;
  * @property Order $order
  * @property Product $product
  */
-class OrderItem extends \common\models\costfit\master\OrderItemMaster
-{
+class OrderItem extends \common\models\costfit\master\OrderItemMaster {
 
     const DATE_GAP_TO_PICKING = 2;
     const ORDERITEM_PICKING = 4;
@@ -38,16 +37,14 @@ class OrderItem extends \common\models\costfit\master\OrderItemMaster
     /**
      * @inheritdoc
      */
-    public function rules()
-    {
+    public function rules() {
         return array_merge(parent::rules(), []);
     }
 
     /**
      * @inheritdoc
      */
-    public function attributes()
-    {
+    public function attributes() {
         return array_merge(parent::attributes(), [
             'maxDate',
             'sumQuantity',
@@ -60,23 +57,19 @@ class OrderItem extends \common\models\costfit\master\OrderItemMaster
     /**
      * @inheritdoc
      */
-    public function attributeLabels()
-    {
+    public function attributeLabels() {
         return array_merge(parent::attributeLabels(), []);
     }
 
-    public function getProduct()
-    {
+    public function getProduct() {
         return $this->hasOne(Product::className(), ['productId' => 'productId']);
     }
 
-    public function getShippingType()
-    {
+    public function getShippingType() {
         return $this->hasOne(ShippingType::className(), ['shippingTypeId' => 'sendDate']);
     }
 
-    public static function findSlowestDate($orderId)
-    {
+    public static function findSlowestDate($orderId) {
         $model = OrderItem::find()
         ->select("MAX(st.date) as maxDate")
         ->join("LEFT JOIN", 'shipping_type st', 'st.shippingTypeId = order_item.sendDate')
@@ -86,8 +79,7 @@ class OrderItem extends \common\models\costfit\master\OrderItemMaster
         return isset($model->maxDate) ? $model->maxDate : NULL;
     }
 
-    public static function countPickingItemsArray($orderId)
-    {
+    public static function countPickingItemsArray($orderId) {
         $res = [];
         $query = \common\models\costfit\OrderItem::find()
         ->where("DATE(DATE_SUB(sendDateTime,INTERVAL " . \common\models\costfit\OrderItem::DATE_GAP_TO_PICKING . " DAY)) <= CURDATE() and orderId=" . $orderId)
@@ -103,14 +95,34 @@ class OrderItem extends \common\models\costfit\master\OrderItemMaster
         return $res;
     }
 
-    public static function findOrderItems($orderId, $productId)
-    {
+    public static function findOrderItems($orderId, $productId) {
         $items = OrderItem::find()->where("orderId=" . $orderId . " and productId=" . $productId)->one();
         if (isset($items)) {
             return $items;
         } else {
             return '';
         }
+    }
+
+    public static function creteStatus($orderId) {
+        $totalPicked = 0;
+        $total2 = 0;
+        $items = OrderItem::find()->where("orderId=" . $orderId . " and status!=1")->all();
+        $picked = count($items);
+        if (isset($items) && !empty($items)) {
+            foreach ($items as $item):
+                $totalPicked+=$item->quantity;
+            endforeach;
+        }
+        $items2 = OrderItem::find()->where("orderId=" . $orderId . " and status=1")->all();
+        $ready = count($items2);
+        if (isset($items2) && !empty($items2)) {
+            foreach ($items2 as $item2):
+                $total2+=$item2->quantity;
+            endforeach;
+        }
+        $text = 'หยิบแล้ว ' . $picked . ' รายการ ' . $totalPicked . ' ชิ้น<br> ยังไม่หยิบ ' . $ready . ' รายการ ' . $total2 . ' ชิ้น';
+        return $text;
     }
 
 }
