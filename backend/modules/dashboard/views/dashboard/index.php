c7618656f883b114c2b5b70b5e122df2d79c7330
Pew CodesLoves <sodapew17@gmail.com>
2016-11-11 08:30:49 +0700

    update

--- backend/modules/dashboard/views/dashboard/index.php
+++ backend/modules/dashboard/views/dashboard/index.php
@@ -565,7 +565,7 @@ $directoryAsset = Yii::$app->assetManager->getPublishedUrl('@app/themes/costfit/
         <!-- 18. $FOLLOWERS ========  Followers=========== -->
         <div class="panel widget-followers">
             <div class="panel-heading">
-                <span class="panel-title"><i class="panel-title-icon fa fa-check"></i>สมาชิก Login Top 5 ในวันนี้</span>
+                <span class="panel-title"><i class="fa fa-group"></i> สมาชิก Login Top 5 ในวันนี้</span>
             </div> <!-- / .panel-heading -->
             <div class="panel-body">
                 <?php
