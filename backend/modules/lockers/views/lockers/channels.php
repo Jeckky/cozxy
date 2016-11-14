c02b0d0e80701c0182e37933fef5558f78ee7fcd
Pew CodesLoves <sodapew17@gmail.com>
2016-11-14 11:59:49 +0700

    update

--- backend/modules/lockers/views/lockers/channels.php
+++ backend/modules/lockers/views/lockers/channels.php
@@ -10,7 +10,7 @@ $this->title = 'ช่องของ Lockers';
 $this->params['breadcrumbs'][] = $this->title;
 $this->params['pageHeader'] = Html::encode($this->title);
 ?>
-<h1>Lockers /  ตรวจสอบช่องที่ลูกค้ารับสินค้าแล้ว </h1>
+<h1>Lockers /  ตรวจสอบช่องที่ลูกค้ารับสินค้าแล้ว /</h1>
 <!--<div class="note note-success ">
     <h3>สถานที่ตั้ง Lockers </h3>
     <h4 style="color: #003147">
@@ -36,8 +36,8 @@ $this->params['pageHeader'] = Html::encode($this->title);
         <div class="col-xs-3"><span><?php echo $countrie->localName; ?></span></div>
     </div>
     <input type="text" placeholder="Code Lockers  : <?php echo ($listPoint->code != '') ? $listPoint->code : ''; ?>" class="form-control input-lg widget-profile-input">
-    <div class="widget-profile-text">
-        &nbsp;
+    <div class="widget-profile-text text-center" style="font-size: 16px;">
+        <?php echo $this->context->dateThai(date("Y-m-d"), 1, TRUE); ?> เวลา <div id="clockDisplay" class="clockStyle"></div>
     </div>
 </div>
 <div class="order-index col-md-12">
@@ -223,8 +223,6 @@ $this->params['pageHeader'] = Html::encode($this->title);
         </div>
     </div>
 </div>
-
-
 <?php $this->registerJs("
 
 ", \yii\web\View::POS_END); ?>
@@ -246,4 +244,46 @@ $this->params['pageHeader'] = Html::encode($this->title);
     </div> <!-- / .modal-dialog -->
 </div> <!-- / .modal -->
 
+<style>
+    .clockStyle {
+        background-color:#000;
+        border:#999 2px inset;
+        padding:6px;
+        color:#0FF;
+        font-family:"Arial Black", Gadget, sans-serif;
+        font-size:16px;
+        font-weight:bold;
+        letter-spacing: 2px;
+        display:inline;
+    }
+</style>
 
+<script>
+    function renderTime() {
+        var currentTime = new Date();
+        var diem = "AM";
+        var h = currentTime.getHours();
+        var m = currentTime.getMinutes();
+        var s = currentTime.getSeconds();
+        setTimeout('renderTime()', 1000);
+        if (h == 0) {
+            h = 12;
+        } else if (h > 12) {
+            h = h - 12;
+            diem = "PM";
+        }
+        if (h < 10) {
+            h = "0" + h;
+        }
+        if (m < 10) {
+            m = "0" + m;
+        }
+        if (s < 10) {
+            s = "0" + s;
+        }
+        var myClock = document.getElementById('clockDisplay');
+        myClock.textContent = h + ":" + m + ":" + s + " " + diem;
+        myClock.innerText = h + ":" + m + ":" + s + " " + diem;
+    }
+    renderTime();
+</script>
