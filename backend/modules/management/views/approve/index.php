3400615ea3de126019b6a34e8eb7ea0b8cfa2a55
Pew CodesLoves <sodapew17@gmail.com>
2016-11-30 15:24:25 +0700

    update

--- backend/modules/management/views/approve/index.php
+++ backend/modules/management/views/approve/index.php
@@ -207,7 +207,7 @@ $this->params['pageHeader'] = Html::encode($this->title);
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                <h4 class="modal-title" id="myModalLabel">Modal Heading</h4>
+                <h4 class="modal-title" id="myModalLabel">Modal View Product</h4>
             </div>
             <div class="modal-body">
                 <!--<h4>Text in a modal</h4>
