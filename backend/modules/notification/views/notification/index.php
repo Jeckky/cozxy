3cb4c2e82a7de47607f741827b5429247749b97b
Pew CodesLoves <sodapew17@gmail.com>
2017-01-05 16:21:41 +0700

    update

--- backend/modules/notification/views/notification/index.php
+++ backend/modules/notification/views/notification/index.php
@@ -11,7 +11,6 @@
             </div> <!-- / .panel-heading -->
             <div class="panel-body padding-sm">
                 <div class="notifications-list">
-
                     <div class="notification">
                         <div class="notification-title text-danger">SYSTEM COZXY</div>
 
@@ -22,7 +21,7 @@
                                 ?>
                                 <div class="notification-description"><strong>หัวข้ออนุมัติสินค้า</strong>: <?php echo $value->title; ?> (<strong class="notification-systme">ดู</strong>)</div>
                                 <div class="notification-ago"><?php echo common\helpers\CozxyUnity::TimeElapsedString($value->createDateTime); ?></div>
-                                <!--<div class="notification-icon fa fa-hdd-o bg-danger"></div>-->
+                                <div class="notification-icon fa fa-hdd-o bg-danger"></div>
                                 <?php
                             }
                         }
@@ -31,7 +30,33 @@
                     </div> <!-- / .notification -->
 
 
+                    <div class="notification">
+                        <div class="notification-title text-info">STORE</div>
+                        <div class="notification-description">&nbsp;<strong>&nbsp;</strong> &nbsp;</div>
+                        <div class="notification-ago">&nbsp;</div>
+                        <div class="notification-icon fa fa-truck bg-info"></div>
+                    </div> <!-- / .notification -->
 
+                    <div class="notification">
+                        <div class="notification-title text-default">CRON DAEMON</div>
+                        <div class="notification-description">&nbsp;<strong>&nbsp;</strong> &nbsp;</div>
+                        <div class="notification-ago">&nbsp;</div>
+                        <div class="notification-icon fa fa-clock-o bg-default"></div>
+                    </div> <!-- / .notification -->
+
+                    <div class="notification">
+                        <div class="notification-title text-success">SYSTEM</div>
+                        <div class="notification-description">&nbsp;<strong>&nbsp;</strong> &nbsp;</div>
+                        <div class="notification-ago">&nbsp;</div>
+                        <div class="notification-icon fa fa-hdd-o bg-success"></div>
+                    </div> <!-- / .notification -->
+
+                    <div class="notification">
+                        <div class="notification-title text-warning">SYSTEM</div>
+                        <div class="notification-description">&nbsp;<strong>&nbsp;</strong> &nbsp;</div>
+                        <div class="notification-ago">&nbsp;</div>
+                        <div class="notification-icon fa fa-hdd-o bg-warning"></div>
+                    </div> <!-- / .notification -->
 
                 </div>
                 <a href="#" class="notifications-link">MORE NOTIFICATIONS</a>
