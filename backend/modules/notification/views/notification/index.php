fabe10d344a0b33163b5fc5036c91981106f41ac
Pew CodesLoves <sodapew17@gmail.com>
2017-01-05 16:09:21 +0700

    update

--- backend/modules/notification/views/notification/index.php
+++ backend/modules/notification/views/notification/index.php
@@ -1,9 +1,58 @@
 <?php
 /* @var $this yii\web\View */
 ?>
-<h1>settings/index</h1>
+<div class="row">
+    <div class="col-md-12">
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+        <!-- 13. $NOTIFICATIONS ======    Notifications -->
+        <div class="panel widget-notifications">
+            <div class="panel-heading">
+                <span class="panel-title"><i class="panel-title-icon fa fa-fire"></i>Notifications</span>
+            </div> <!-- / .panel-heading -->
+            <div class="panel-body padding-sm">
+                <div class="notifications-list">
+
+                    <div class="notification">
+                        <div class="notification-title text-danger">SYSTEM</div>
+                        <div class="notification-description"><strong>Error 500</strong>: Syntax error in index.php at line <strong>461</strong>.</div>
+                        <div class="notification-ago">12h ago</div>
+                        <div class="notification-icon fa fa-hdd-o bg-danger"></div>
+                    </div> <!-- / .notification -->
+
+                    <div class="notification">
+                        <div class="notification-title text-info">STORE</div>
+                        <div class="notification-description">You have <strong>9</strong> new orders.</div>
+                        <div class="notification-ago">12h ago</div>
+                        <div class="notification-icon fa fa-truck bg-info"></div>
+                    </div> <!-- / .notification -->
+
+                    <div class="notification">
+                        <div class="notification-title text-default">CRON DAEMON</div>
+                        <div class="notification-description">Job <strong>"Clean DB"</strong> has been completed.</div>
+                        <div class="notification-ago">12h ago</div>
+                        <div class="notification-icon fa fa-clock-o bg-default"></div>
+                    </div> <!-- / .notification -->
+
+                    <div class="notification">
+                        <div class="notification-title text-success">SYSTEM</div>
+                        <div class="notification-description">Server <strong>up</strong>.</div>
+                        <div class="notification-ago">12h ago</div>
+                        <div class="notification-icon fa fa-hdd-o bg-success"></div>
+                    </div> <!-- / .notification -->
+
+                    <div class="notification">
+                        <div class="notification-title text-warning">SYSTEM</div>
+                        <div class="notification-description"><strong>Warning</strong>: Processor load <strong>92%</strong>.</div>
+                        <div class="notification-ago">12h ago</div>
+                        <div class="notification-icon fa fa-hdd-o bg-warning"></div>
+                    </div> <!-- / .notification -->
+
+                </div>
+                <a href="#" class="notifications-link">MORE NOTIFICATIONS</a>
+            </div> <!-- / .panel-body -->
+        </div> <!-- / .panel -->
+        <!-- /13. $NOTIFICATIONS -->
+
+
+    </div>
+</div>
