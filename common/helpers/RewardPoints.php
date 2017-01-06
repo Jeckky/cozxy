55d1c3a55da05b457e87f72149f1628bba2a5645
Pew CodesLoves <sodapew17@gmail.com>
2017-01-06 15:10:41 +0700

    increate common\helpers\RewardPoints
    - เรืองเก็บคะแนนสะสม

---
+++ common/helpers/RewardPoints.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace common\helpers;
+
+use Yii;
+use yii\base\Model;
+use yii\db\ActiveRecord;
+use yii\data\ActiveDataProvider;
+
+/**
+ * Description of RewardPoints
+ *
+ * @author it
+ * 6/1/2017 Taninut.Bm
+ *
+ */
+class RewardPoints {
+    //put your code here
+}
