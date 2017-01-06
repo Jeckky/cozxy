6dda22b0e57fb3a4d098962d063c88aacdc4c5cc
Pew CodesLoves <sodapew17@gmail.com>
2017-01-06 09:54:54 +0700

    update

--- backend/modules/suppliers/controllers/ProductAllController.php
+++ backend/modules/suppliers/controllers/ProductAllController.php
@@ -13,7 +13,7 @@ class ProductAllController extends SuppliersMasterController {
         $userId = Yii::$app->request->get('userId');
         $dataProvider = new ActiveDataProvider([
             'query' => ProductSuppliers::find()
-            ->select('`product_suppliers`.* ,  (SELECT product_price_suppliers.price  FROM costfit_test.product_price_suppliers
+            ->select('`product_suppliers`.* ,  (SELECT product_price_suppliers.price  FROM product_price_suppliers
             where product_price_suppliers.productSuppId = product_suppliers.productSuppId and product_price_suppliers.status = 1  limit 1)
             AS `priceSuppliers`')
             ->where('userId=' . $userId)->orderBy('product_suppliers.productSuppId desc'),
