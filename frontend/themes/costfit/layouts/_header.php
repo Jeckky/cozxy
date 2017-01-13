8efc13f53d2c1cd027a581d622989f17f1dab86a
Pew CodesLoves <sodapew17@gmail.com>
2017-01-13 15:49:33 +0700

    update

--- frontend/themes/costfit/layouts/_header.php
+++ frontend/themes/costfit/layouts/_header.php
@@ -25,11 +25,11 @@ $logo = common\models\costfit\ContentGroup::find()->where("lower(title)='logoima
     <div class="menu-toggle"><i class="fa fa-list"></i></div>
     <!-- Show For Desktop -->
     <div class="container hidden-xs hidden-sm">
-        <a class="logo" href="<?= Yii::$app->homeUrl ?>"><img src="<?php echo $logo->image; ?>" alt="Cost Fit" class="img-responsive"/></a>
+        <a class="logo" href="<?= Yii::$app->homeUrl ?>"><img src="<?php echo Yii::$app->homeUrl . $logo->image; ?>" alt="Cost Fit" class="img-responsive"/></a>
     </div>
     <!-- Show For Mobile -->
     <div class="container hidden-md  hidden-lg">
-        <a class="logo" href="<?= Yii::$app->homeUrl ?>"><img src="<?php echo $logo->image; ?>" alt="Cost Fit" class="img-responsive" style="width: 70px; height: 32px;"/></a>
+        <a class="logo" href="<?= Yii::$app->homeUrl ?>"><img src="<?php echo Yii::$app->homeUrl . $logo->image; ?>" alt="Cost Fit" class="img-responsive" style="width: 70px; height: 32px;"/></a>
     </div>
 
     <!--Main Menu-->
