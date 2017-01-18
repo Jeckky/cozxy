76ed387a9bdddecf576690e8ce07643203a79217
Pew CodesLoves <sodapew17@gmail.com>
2017-01-18 11:23:07 +0700

    update slash หน้า search

--- frontend/views/site/_popular_cat.php
+++ frontend/views/site/_popular_cat.php
@@ -14,11 +14,11 @@ $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
         <span class="tile-overlay"></span>
 
         <div class="footer" id="products-popular-footer">
-            <a href="<?php echo $baseUrl; ?>search/<?= $model->createTitle() ?>/<?= $model->encodeParams(['categoryId' => $model->categoryId]) ?>"><?= $model->title; ?></a>
+            <a href="<?php echo $baseUrl; ?>/search/<?= $model->createTitle() ?>/<?= $model->encodeParams(['categoryId' => $model->categoryId]) ?>"><?= $model->title; ?></a>
             <span>
                 <small><?= $model->title; ?></small>
             </span>
-            <a href="<?php echo $baseUrl; //Yii::$app->homeUrl;   ?>/search/<?= $model->createTitle() ?>/<?= $model->encodeParams(['categoryId' => $model->categoryId]) ?>"><button class="btn btn-primary btn-sm">view</button></a>
+            <a href="<?php echo $baseUrl; //Yii::$app->homeUrl;    ?>/search/<?= $model->createTitle() ?>/<?= $model->encodeParams(['categoryId' => $model->categoryId]) ?>"><button class="btn btn-primary btn-sm">view</button></a>
         </div>
     </div>
 </div>
