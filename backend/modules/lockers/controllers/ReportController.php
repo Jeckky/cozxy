7ff530853185146cfee1c6ec13dbe663a403a70a
Pew CodesLoves <sodapew17@gmail.com>
2017-02-08 11:41:27 +0700

    increate report data use lockkers
    - สร้าง รายงาน report ข้อมูลการใช้งานของ lockkers

---
+++ backend/modules/lockers/controllers/ReportController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace backend\modules\lockers\controllers;
+
+class ReportController extends \yii\web\Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
