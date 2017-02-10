0a3b142bdef6aab5c60e941e6ae7c21654b81d1e
Pew CodesLoves <sodapew17@gmail.com>
2017-02-10 08:05:21 +0700

    update .js approve product cozxy

--- backend/themes/costfit/assets/javascripts/cozxy/cozxy-unity.js
+++ backend/themes/costfit/assets/javascripts/cozxy/cozxy-unity.js
@@ -415,9 +415,7 @@ function switchers(id, type, status) {
     //alert(approveReceiveType);
     ///var ReceiveType = $('#approveReceiveType-' + id).checked;
     //
-    if (approveReceiveType == undefined) {
-        alert('กรุณาเลือกรูปแบบการรับสินค้า !!');
-    } else {
+    if (type == 2) {
         $.ajax({
             url: 'approve/approve-items',
             data: {"productSuppId": id, "type": type, 'status': status, "receiveType": approveReceiveType},
@@ -446,7 +444,41 @@ function switchers(id, type, status) {
                 //console.log(on);
             });
         });
+    } else {
+        if (approveReceiveType == undefined) {
+            alert('กรุณาเลือกรูปแบบการรับสินค้า !!');
+        } else {
+            $.ajax({
+                url: 'approve/approve-items',
+                data: {"productSuppId": id, "type": type, 'status': status, "receiveType": approveReceiveType},
+                type: 'post',
+                success: function (result) {
+                    //alert(result);
+                    //var JSONObject2 = JSON.parse(result);
+                    //$(".tes-test").html('' + JSONObject2 + '');
+                    //$("tbody tr:first-child").css({backgroundColor: 'yellow', fontWeight: 'bolder'});
+                    if (result == 1) {
+                        $(".suppliers tbody #productSuppId-" + id).remove();
+                    } else if (result == 2) {
+                        //alert($(".suppliers tbody tr:first-child").attribute('data-key'));
+                        $(".system tbody #productId-" + id).remove();
+                    }
+                },
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    //debugger;
+                    //alert(errorThrown);
+                }
+            });
+
+            init.push(function () {
+                $('#switchers-colors-square .switcher > input').on('change.switcher', function (on, off, toggle) {
+                    //alert('Test Yes !!');
+                    //console.log(on);
+                });
+            });
+        }
     }
+
     //
     ///alert($('#switchers-colors-square').find('.switcher').find(".checked"));
 }
