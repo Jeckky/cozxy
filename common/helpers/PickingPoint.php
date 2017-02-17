ec85c318a2f6182ebdd112971d2f7019a7b76a93
Pew CodesLoves <sodapew17@gmail.com>
2017-02-17 07:47:55 +0700

    increate PickingPoint in helpers

---
+++ common/helpers/PickingPoint.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace common\helpers;
+
+use Yii;
+use yii\base\Model;
+use yii\db\ActiveRecord;
+use yii\data\ActiveDataProvider;
+
+/**
+ * Description of PickingPoint
+ *
+ * @author it
+ */
+class PickingPoint {
+
+    //put your code here
+
+    /*
+     * แจ้งเตือนสถานที่ ที่ Customer เคยเลือกมาแล้วแสดงให้ดูด้วย
+     */
+    public static function LocationHistoryReceiveTypeInCustomer() {
+
+    }
+
+}
