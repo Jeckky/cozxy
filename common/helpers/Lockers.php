11f3186838140394f8380b9cb3b06cc9d1430dc0
Pew CodesLoves <sodapew17@gmail.com>
2017-02-27 07:40:12 +0700

    update

--- common/helpers/Lockers.php
+++ common/helpers/Lockers.php
@@ -77,7 +77,7 @@ class Lockers {
         ->where("order_item_packing.status = '" . \common\models\costfit\OrderItemPacking::PACKING_SENDING_PACKING_SHIPPING . "' "
         . " and order_item_packing.bagNo ='" . $bagNo . "' and order.pickingId = '" . $boxcode . "' "
         . " or order_item_packing.status = '" . \common\models\costfit\OrderItemPacking::PACKING_STATUS_EXPORT_TO_LOCKERS . "'  "
-        . " and order_item_packing.bagNo ='" . $bagNo . "' and order.pickingId = '" . $boxcode . "' ")
+        . " and order_item_packing.bagNo ='" . $bagNo . "' and order.pickingId = '" . $boxcode . "' and order_item.receiveType = '1'")
         ->groupBy(['order_item_packing.bagNo'])->one();
         return $queryOrderItemPackingId;
     }
@@ -91,7 +91,7 @@ class Lockers {
         ->joinWith(['orderItems'])
         ->join(' LEFT JOIN', 'order', 'order_item.orderId = order.orderId')
         ->where(" order_item_packing.status = '" . \common\models\costfit\OrderItemPacking::PACKING_SENDING_PACKING_SHIPPING . "' "
-        . " and order_item_packing.bagNo ='" . $bagNo . "' ")
+        . " and order_item_packing.bagNo ='" . $bagNo . "' and order_item.receiveType = '1'")
         ->groupBy(['order_item_packing.bagNo']);
         return $query;
     }
@@ -105,7 +105,7 @@ class Lockers {
         ->joinWith(['orderItems'])
         ->join('LEFT JOIN', 'order', 'order_item.orderId = order.orderId')
         ->where("order_item_packing.status = '" . \common\models\costfit\OrderItemPacking::PACKING_SENDING_PACKING_SHIPPING . "'"
-        . "  and order_item_packing.bagNo ='" . $bagNo . "' ")
+        . "  and order_item_packing.bagNo ='" . $bagNo . "' and order_item.receiveType = '1'")
         ->groupBy(['order_item_packing.bagNo'])->one();
         return $queryCountBag;
     }
@@ -114,7 +114,7 @@ class Lockers {
         $countBag = \common\models\costfit\OrderItemPacking::find()
         ->select('order_item_packing.orderItemPackingId, order_item_packing.orderItemId, order_item_packing.bagNo, order_item_packing.status')
         ->joinWith(['orderItems'])
-        ->where("order_item.orderId = '" . $orderId . "' and order_item_packing.status = '" . \common\models\costfit\OrderItemPacking::PACKING_SENDING_PACKING_SHIPPING . "' ")
+        ->where("order_item.orderId = '" . $orderId . "' and order_item_packing.status = '" . \common\models\costfit\OrderItemPacking::PACKING_SENDING_PACKING_SHIPPING . "' and order_item.receiveType = '1'")
         ->groupBy(['order_item_packing.bagNo'])
         ->count();
         return $countBag;
@@ -144,7 +144,7 @@ class Lockers {
         ->joinWith(['orderItems'])
         ->join('LEFT JOIN', 'order', 'order_item.orderId = order.orderId')
         ->where("order_item_packing.status in (7,8) and  order_item.orderItemId ='" . $orderItemId . "' and order_item_packing.pickingItemsId = '" . $pickingItemsId . "'or order_item_packing.status in (7,8) "
-        . "and  order_item_packing.bagNo ='" . $bagNo . "' and order_item_packing.pickingItemsId = '" . $pickingItemsId . "' ")
+        . "and  order_item_packing.bagNo ='" . $bagNo . "' and order_item_packing.pickingItemsId = '" . $pickingItemsId . "'  and order_item.receiveType = '1'")
         ->groupBy(['order_item_packing.bagNo']);
         return $query1;
     }
@@ -159,7 +159,7 @@ class Lockers {
         . 'order.orderId,order_item_packing.quantity')
         ->joinWith(['orderItems'])
         ->join('LEFT JOIN', 'order', 'order_item.orderId = order.orderId')
-        ->where("order_item_packing.status = 5 and order.orderId ='" . $orderId . "' ")
+        ->where("order_item_packing.status = 5 and order.orderId ='" . $orderId . "'  and order_item.receiveType = '1'")
         ->groupBy(['order_item_packing.bagNo']);
         return $queryAllOrder;
     }
@@ -174,7 +174,7 @@ class Lockers {
         . 'order.orderId ,order_item_packing.quantity')
         ->joinWith(['orderItems'])
         ->join('LEFT JOIN', 'order', 'order_item.orderId = order.orderId')
-        ->where("order_item_packing.status in (7,8) and  order_item_packing.orderItemId ='" . $orderItemId . "'")
+        ->where("order_item_packing.status in (7,8) and  order_item_packing.orderItemId ='" . $orderItemId . "'  and order_item.receiveType = '1'")
         //->where("order_item_packing.status = 5 and order_item_packing.bagNo ='" . $bagNo . "' ")
         ->groupBy(['order_item_packing.bagNo']);
         return $query1;
@@ -184,7 +184,7 @@ class Lockers {
         $query = \common\models\costfit\OrderItemPacking::find()
         ->select('order_item_packing.orderItemPackingId, order_item_packing.orderItemId, order_item_packing.bagNo, order_item_packing.status')
         ->joinWith(['orderItems'])
-        ->where("order_item.orderId = '" . $orderId . "' and order_item_packing.status = 5");
+        ->where("order_item.orderId = '" . $orderId . "' and order_item_packing.status = 5  and order_item.receiveType = '1'");
         return $query;
     }
 
@@ -196,7 +196,7 @@ class Lockers {
         . 'order.orderId,order_item_packing.quantity')
         ->joinWith(['orderItems'])
         ->join('LEFT JOIN', 'order', 'order_item.orderId = order.orderId')
-        ->where("order_item_packing.status = 5 and order.orderId ='" . $orderId . "' ")
+        ->where("order_item_packing.status = 5 and order.orderId ='" . $orderId . "'  and order_item.receiveType = '1'")
         ->groupBy(['order_item_packing.bagNo']);
         return $queryAllOrder;
     }
