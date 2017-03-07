3746f2ba65104d45897f8f2791483537cf3d3449
Pew CodesLoves <sodapew17@gmail.com>
2017-03-07 08:41:24 +0700

    increate helpers Booth

---
+++ common/helpers/Booth.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace common\helpers;
+
+use Yii;
+use yii\base\Model;
+use yii\db\ActiveRecord;
+use yii\data\ActiveDataProvider;
+
+/**
+ * Description of Booth
+ * Create By Taninut.bm
+ * Create Date : 7/03/2017
+ * @author it
+ */
+class Booth {
+    //put your code here
+}
