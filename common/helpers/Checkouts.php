18a28f2e4a690ed4379cc55758a2c2a8e9cfd566
Pew CodesLoves <sodapew17@gmail.com>
2017-03-27 08:45:16 +0700

    increate helpers Class Checkouts

---
+++ common/helpers/Checkouts.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace common\helpers;
+
+use Yii;
+use yii\base\Model;
+use yii\db\ActiveRecord;
+use yii\data\ActiveDataProvider;
+
+/**
+ * Description of Checkouts
+ *
+ * @author it , Taninut.Bm
+ * Create Date : 27/03/2017
+ */
+class Checkouts {
+    //put your code here
+}
