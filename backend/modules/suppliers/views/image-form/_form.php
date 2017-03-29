36451d0c8550025a0c47e574b954cd9782630812
Pew CodesLoves <sodapew17@gmail.com>
2017-03-29 08:43:44 +0700

    update

--- backend/modules/suppliers/views/image-form/_form.php
+++ backend/modules/suppliers/views/image-form/_form.php
@@ -183,7 +183,6 @@ use yii\widgets\Pjax;
             </h4>
         </div>
         <?php
-        //echo Yii::$app->user->identity->type;
         if (Yii::$app->user->identity->type == 4 || Yii::$app->user->identity->type == 3) {
             ?>
             <!-- 49.1. $DROPZONEJS_EXAMPLE ====   Example ==== -->
