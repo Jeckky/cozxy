918a4e79cf0c1e69e07acd5930ab723cf2dc78ce
Pew CodesLoves <sodapew17@gmail.com>
2017-04-05 08:03:24 +0700

    xxx

--- common/mail/orderToMember.php
+++ common/mail/orderToMember.php
@@ -62,6 +62,9 @@
                 text-align: left;
                 line-height: 25px;
             }
+            #order > table, td, th {
+                border: 1px solid black;
+            }
         </style>
 
         <div class="main">
@@ -138,7 +141,7 @@
                             </tr>
                             <tr>
                                 <td>
-                                    <table class="table table-list-order"  border="0"  style="padding: 10px;" >
+                                    <table id="order" class="table table-list-order"  border="0"  style="padding: 10px;" >
                                         <thead>
                                             <tr style="background-color: #f5f5f5;">
                                                 <th style="font-size: 13px;">ลำดับ</th>
