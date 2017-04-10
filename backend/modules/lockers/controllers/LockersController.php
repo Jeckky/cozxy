b644be117e0144e4e4e04de78f018520507ca567
sakqqboy <saknakhngam@hotmail.com>
2017-04-10 13:30:03 +0700

    Zz

--- backend/modules/lockers/controllers/LockersController.php
+++ backend/modules/lockers/controllers/LockersController.php
@@ -193,7 +193,6 @@ class LockersController extends LockersMasterController {
          */
 
         if (isset($bagNo) && !empty($bagNo)) {
-            throw new \yii\base\Exception($bagNo);
             /*
              * Check ว่า BagNo. นี้ มีอยู่ใน Lockers และช่องนี้ยัง
              * End Check ว่า BagNo. นี้ มีอยู่ใน Lockers และช่องนี้ยั
@@ -208,7 +207,7 @@ class LockersController extends LockersMasterController {
 
             if (count($queryOrderItemPackingId) == 0) {
 //                throw new \yii\base\Exception("Step 1");
-                return $this->redirect(Yii::$app->homeUrl . 'lockers/lockers/scan-bag?pickingItemsId=' . $pickingItemsId . '&boxcode=' . $boxcode . '&model=' . $model . '&code=' . $channel . '&orderId=' . $orderId . '&c=e');
+                return $this->redirect(Yii::$app->homeUrl . 'lockers/lockers/scan-bag?pickingItemsId=' . $pickingItemsId . '&boxcode=' . $boxcode . '&model=' . $model . '&code=' . $channel . '&orderId=' . $orderId . '&c=e&bagNo=' . $bagNo);
             }
 //            throw new \yii\base\Exception(222);
             $orderId = $queryOrderItemPackingId->orderId; // ได้ OrderId มาเพื่อหา ????
