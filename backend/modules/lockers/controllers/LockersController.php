21799bc619eede51b5a8f138b90b19722aabe0f7
sakqqboy <saknakhngam@hotmail.com>
2017-04-10 12:19:44 +0700

    Zzz

--- backend/modules/lockers/controllers/LockersController.php
+++ backend/modules/lockers/controllers/LockersController.php
@@ -193,7 +193,6 @@ class LockersController extends LockersMasterController {
          */
 
         if (isset($bagNo) && !empty($bagNo)) {
-            throw new \yii\base\Exception($bagNo);
             /*
              * Check ว่า BagNo. นี้ มีอยู่ใน Lockers และช่องนี้ยัง
              * End Check ว่า BagNo. นี้ มีอยู่ใน Lockers และช่องนี้ยั
