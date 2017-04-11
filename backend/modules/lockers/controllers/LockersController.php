5cac9e3475aaef17a46190a78e16b675c4b72e40
sakqqboy <saknakhngam@hotmail.com>
2017-04-11 08:53:48 +0700

    Sss

--- backend/modules/lockers/controllers/LockersController.php
+++ backend/modules/lockers/controllers/LockersController.php
@@ -211,6 +211,7 @@ class LockersController extends LockersMasterController {
               } by sak */
 //            throw new \yii\base\Exception(222);
             $queryOrderItemPackingId = Lockers::GetOrderItemPackingCheckLockersBagNo($bagNo, $boxcode);
+            throw new \yii\base\Exception($queryOrderItemPackingId->orderId);
             $orderId = $queryOrderItemPackingId->orderId; // ได้ OrderId มาเพื่อหา ????
             $orderItemId = $queryOrderItemPackingId->orderItemId; // ได้ OrderId มาเพื่อหา ????
             $orderItemPackingId = $queryOrderItemPackingId->orderItemPackingId;
