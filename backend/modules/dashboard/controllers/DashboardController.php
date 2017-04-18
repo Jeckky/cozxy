d4735e1f65ecfcede390e15d43f610f54ffb77f2
Pew CodesLoves <sodapew17@gmail.com>
2017-04-18 10:19:55 +0700

    update

--- backend/modules/dashboard/controllers/DashboardController.php
+++ backend/modules/dashboard/controllers/DashboardController.php
@@ -9,7 +9,7 @@ class DashboardController extends DashboardMasterController {
     public function behaviors() {
         return [
             'access' => [
-                //'class' => \yii\filters\AccessControl::className(),
+                'class' => \yii\filters\AccessControl::className(),
                 'only' => ['index', 'create', 'update', 'view'],
                 'rules' => [
                     // allow authenticated users
