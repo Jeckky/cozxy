378fb14465735c532ac95572cadce16ddddb8068
kamyjapkung <kamyjap@gmail.com>
2017-04-21 16:58:21 +0700

    xx

--- frontend/views/cart/cart_totals_right.php
+++ frontend/views/cart/cart_totals_right.php
@@ -8,10 +8,10 @@
         <td>Starting Subtotal</td>
         <td class="subtotal align-r"><?= number_format($this->params['cart']['totalWithoutDiscount'], 2) ?> ฿</td>
     </tr>
-    <tr style="color: red;">
+<!--    <tr style="color: red;">
         <td>Extra Savings</td>
         <td class="savings align-r">-<?= number_format($this->params['cart']['totalItemDiscount'], 2) ?> ฿</td>
-    </tr>
+    </tr>-->
     <tr>
         <td style="color: #000;">Subtotal</td>
         <td class="total align-r"><?= number_format($this->params['cart']['total'], 2) ?> ฿</td>
