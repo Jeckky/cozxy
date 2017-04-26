9a7091881f450199c71a76b034783b6d6c453962
kamyjapkung <kamyjap@gmail.com>
2017-04-26 16:55:18 +0700

    xxx

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -205,7 +205,7 @@ $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
                                             //echo "<div class=\"col-sm-3\"><img id=\"myImg-" . $valueImages['productImageId'] . "\" onClick=\"reviews_click(" . $valueImages['productImageId'] . ',' . "xx" . ")\"   src=\"/" . $valueImages['imageThumbnail2'] . "\" alt=\"1\" class=\"img-responsive img-thumbnail myImg\"/></div>";
                                             ?>
                                             <div class="col-sm-12 col-lg-12 col-md-12" style="padding-left: 0px;  padding-right: 0px;">
-                                                <!--<img id="myImg-<?php // echo $valueImages['productImageId'];      ?>" onclick="reviews_click(<?php // echo $valuex->productSuppId;      ?>,<?php // echo $valueImages['productImageId'];      ?>, '<?php // echo $valueImages['image'];      ?>', '<?php // echo $valuex->title;      ?>')" src="<?php // echo $valueImages['imageThumbnail1'];      ?>" alt="1" class="img-responsive  myImg">-->
+                                                <!--<img id="myImg-<?php // echo $valueImages['productImageId'];         ?>" onclick="reviews_click(<?php // echo $valuex->productSuppId;         ?>,<?php // echo $valueImages['productImageId'];         ?>, '<?php // echo $valueImages['image'];         ?>', '<?php // echo $valuex->title;         ?>')" src="<?php // echo $valueImages['imageThumbnail1'];         ?>" alt="1" class="img-responsive  myImg">-->
                                                 <a href="<?php echo Yii::$app->homeUrl; ?>reviews/see-review?productPostId=<?php echo $value->productPostId; ?>&productSupplierId=<?php echo $valuex->productSuppId; ?>&productId=<?php echo $valuex->productId; ?>" style="font-size: 14px; margin-top: 5px;">
                                                     <img  src="<?php echo $valueImages['imageThumbnail1']; ?>" alt="1" class="img-responsive  myImg">
                                                 </a>
@@ -249,7 +249,7 @@ $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
                             <div class="col-md-12 text-center" style=" border-bottom: 0px #e6e6e6 dotted; border-top: 1px #bbb dotted;padding-left: 0px;  padding-right: 0px; ">
                                 <?php
                                 //echo '<span style="font-size: 12px; color:#0066c0;">' . number_format($results_rating, 3) . ' จาก 5 คะแนน | ' . $rating_count . ' post  </span>';
-                                echo '<span style="font-size: 12px; color:#0066c0;">' . $rating_count . ' Post  </span>';
+                                echo ($rating_count == 1) ? '<span style="font-size: 12px; color:#0066c0;">' . $rating_count . ' Story  </span>' : '<span style="font-size: 12px; color:#0066c0;">' . $rating_count . ' Stories  </span>';
                                 ?>
                             </div>
                             <?php
@@ -288,9 +288,9 @@ $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
         </div>
         <div class="row" id="save-main-limit"></div>
 <?php if ($saveCat->getTotalCount() > 6): ?>
-                                                                                                                                                                                                                                                                                                        <div class="row see-more-x col-md-12 text-right" style="margin-bottom: 15px">
-                                                                                                                                                                                                                                                                                                            <span id="btn-see-more" class="btn btn-primary btn-xs ">See more</span>
-                                                                                                                                                                                                                                                                                                        </div>
+                                                                                                                                                                                                                                                                                                                    <div class="row see-more-x col-md-12 text-right" style="margin-bottom: 15px">
+                                                                                                                                                                                                                                                                                                                        <span id="btn-see-more" class="btn btn-primary btn-xs ">See more</span>
+                                                                                                                                                                                                                                                                                                                    </div>
 <?php endif; ?>
     </div>
 </section> Categories Close-->
