c592de84e3b7091faa2cb960cf03e631a497a8bc
Pew CodesLoves <sodapew17@gmail.com>
2017-04-26 12:49:34 +0700

    update

--- frontend/views/products/product_catalog_item.php
+++ frontend/views/products/product_catalog_item.php
@@ -118,7 +118,7 @@ $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
     <!--    <div class="buttons group products-buttons-group" style="margin-top: -18px;">
             <div class="form-group" style="word-wrap: break-word;">
                 <label for="shopping-dollar" class="col-sm-1 " style="float: left; padding-right: 0px; padding-left: 0px; margin-bottom: 0px;">
-                    <img  src="<?php // echo Yii::$app->homeUrl;                                                                                                                                                                                                                                                              ?>images/icon/Untitled-2-50-48.png" alt="thumb" class="img-responsive img-circle-thumbnail" width="38" height="38" style="background-color: #eee;"/>
+                    <img  src="<?php // echo Yii::$app->homeUrl;                                                                                                                                                                                                                                                                  ?>images/icon/Untitled-2-50-48.png" alt="thumb" class="img-responsive img-circle-thumbnail" width="38" height="38" style="background-color: #eee;"/>
                 </label>
                 <div class="col-sm-11 text-left discountPrice " style="float: left; padding: 0px; margin-left: 0px; margin-top: 15px;">
                     &nbsp;Add more than 1 item to your order
@@ -383,6 +383,13 @@ $this->registerJsFile($directoryAsset . "/js/plugins/icheck.min.js", ['depends' 
                 height: auto;
 
             }
+            .post{
+                overflow-y: scroll;
+                overflow-x: hidden;
+                height:350px;
+                max-height:100%;
+                color: #635d5d;
+            }
         </style>
         <?php
         if (Yii::$app->controller->action->id != 'see-review') {
@@ -485,7 +492,6 @@ if (Yii::$app->controller->action->id != 'see-review') {
                             color: #000 !important;
                         }
                         .test{
-
                             overflow-y: scroll;
                             overflow-x: hidden;
 
