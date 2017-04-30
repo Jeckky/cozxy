f882ae55c038ecc7cce7c23384b18c39e1ac56d9
Pew CodesLoves <sodapew17@gmail.com>
2017-04-30 10:27:04 +0700

    update

--- frontend/views/search/_product.php
+++ frontend/views/search/_product.php
@@ -47,10 +47,10 @@ if (isset($model->productId)) {
             <div class="footer search-category-footer">
                 <div class="" style="max-height: 50px; min-height: 50px;">
                     <a href="<?php echo Yii::$app->homeUrl; ?>products/<?= $model->encodeParams(['productId' => $model->productId, 'productSupplierId' => $model->productSuppId]) ?>">
-                        <?= substr($model->title, 0, 40);
+                        <?= substr($model->title, 0, 35);
                         ?></a>
                 </div>
-                <span><?php //= $model->shortDescription;                ?></span>
+                <span><?php //= $model->shortDescription; ?></span>
                 <a href="<?php echo Yii::$app->homeUrl; ?>products/<?= $model->encodeParams(['productId' => $model->productId, 'productSupplierId' => $model->productSuppId]) ?>"><button class="btn btn-primary" id="addItemToCart"><i class="fa fa-search"></i>View</button></a>
             </div>
         </div>
