b6af9c9d697c1d5b2c331d63301c7c321e9a58df
Pew CodesLoves <sodapew17@gmail.com>
2017-05-02 16:30:56 +0700

    update

--- backend/modules/warrantyperiod/views/warranty-period/_form.php
+++ backend/modules/warrantyperiod/views/warranty-period/_form.php
@@ -4,9 +4,9 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
 use yii\widgets\MaskedInput;
-use common\models\areawow;
+use common\models\costfit;
 use yii\jui\DatePicker;
-use common\models\areawow\WarrantyPeriod; 
+use common\models\costfit\WarrantyPeriod;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\costfit\WarrantyPeriod */
@@ -15,33 +15,35 @@ use common\models\areawow\WarrantyPeriod;
 
 <div class="warranty-period-form">
 
-    <?php $form = ActiveForm::begin([
-    'options' => ['class' => 'panel panel-default form-horizontal', 'enctype' => 'multipart/form-data'],
-    'fieldConfig' => [
-    'template' => '{label}<div class="col-sm-9">{input}</div>',
-    'labelOptions'=>[
-    'class'=>'col-sm-3 control-label'
-    ]
-    ]
-    ]); ?>
+    <?php
+    $form = ActiveForm::begin([
+        'options' => ['class' => 'panel panel-default form-horizontal', 'enctype' => 'multipart/form-data'],
+        'fieldConfig' => [
+            'template' => '{label}<div class="col-sm-9">{input}</div>',
+            'labelOptions' => [
+                'class' => 'col-sm-3 control-label'
+            ]
+        ]
+    ]);
+    ?>
 
     <div class="panel-heading">
-        <span class="panel-title"><?=$title?></span>
+        <span class="panel-title"><?= $title ?></span>
     </div>
 
     <div class="panel-body">
-        		<?= $form->errorSummary($model)?>
+        <?= $form->errorSummary($model) ?>
 
-		<?= $form->field($model, 'warrantyPeriodId',['options'=>['class'=>'row form-group']])->dropDownList(ArrayHelper::map(WarrantyPeriod::find()->all(), 'warrantyPeriodId', 'title'), ['prompt' => '-- Select WarrantyPeriod --']) ?>
+        <?= $form->field($model, 'warrantyPeriodId', ['options' => ['class' => 'row form-group']])->dropDownList(ArrayHelper::map(WarrantyPeriod::find()->all(), 'warrantyPeriodId', 'title'), ['prompt' => '-- Select WarrantyPeriod --']) ?>
 
-		<?= $form->field($model, 'title',['options'=>['class'=>'row form-group']])->textInput(['maxlength' => 45]) ?>
+<?= $form->field($model, 'title', ['options' => ['class' => 'row form-group']])->textInput(['maxlength' => 45]) ?>
 
-                <div class="form-group">
+        <div class="form-group">
             <div class="col-sm-9 col-sm-offset-3">
-                <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+<?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
             </div>
         </div>
     </div>
-    <?php ActiveForm::end(); ?>
+<?php ActiveForm::end(); ?>
 
 </div>
--- backend/modules/warrantytype/views/warranty-type/_form.php
+++ backend/modules/warrantytype/views/warranty-type/_form.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
 use yii\widgets\MaskedInput;
-use common\models\areawow;
+use common\models\costfit;
 use yii\jui\DatePicker;
 
 /* @var $this yii\web\View */
@@ -14,33 +14,35 @@ use yii\jui\DatePicker;
 
 <div class="warranty-type-form">
 
-    <?php $form = ActiveForm::begin([
-    'options' => ['class' => 'panel panel-default form-horizontal', 'enctype' => 'multipart/form-data'],
-    'fieldConfig' => [
-    'template' => '{label}<div class="col-sm-9">{input}</div>',
-    'labelOptions'=>[
-    'class'=>'col-sm-3 control-label'
-    ]
-    ]
-    ]); ?>
+    <?php
+    $form = ActiveForm::begin([
+        'options' => ['class' => 'panel panel-default form-horizontal', 'enctype' => 'multipart/form-data'],
+        'fieldConfig' => [
+            'template' => '{label}<div class="col-sm-9">{input}</div>',
+            'labelOptions' => [
+                'class' => 'col-sm-3 control-label'
+            ]
+        ]
+    ]);
+    ?>
 
     <div class="panel-heading">
-        <span class="panel-title"><?=$title?></span>
+        <span class="panel-title"><?= $title ?></span>
     </div>
 
     <div class="panel-body">
-        		<?= $form->errorSummary($model)?>
+        <?= $form->errorSummary($model) ?>
 
-		<?= $form->field($model, 'title',['options'=>['class'=>'row form-group']])->textInput(['maxlength' => 255]) ?>
+        <?= $form->field($model, 'title', ['options' => ['class' => 'row form-group']])->textInput(['maxlength' => 255]) ?>
 
-		<?= $form->field($model, 'defimition',['options'=>['class'=>'row form-group']])->widget(\yii\redactor\widgets\Redactor::className()) ?>
+<?= $form->field($model, 'defimition', ['options' => ['class' => 'row form-group']])->widget(\yii\redactor\widgets\Redactor::className()) ?>
 
-                <div class="form-group">
+        <div class="form-group">
             <div class="col-sm-9 col-sm-offset-3">
-                <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+<?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
             </div>
         </div>
     </div>
-    <?php ActiveForm::end(); ?>
+<?php ActiveForm::end(); ?>
 
 </div>
