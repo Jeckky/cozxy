ebadbcb4518dcef8d518ade7adf551626aab674a
kamyjapkung <kamyjap@gmail.com>
2017-05-03 10:57:58 +0700

    xxx

--- frontend/views/search/_product_search.php
+++ frontend/views/search/_product_search.php
@@ -48,7 +48,7 @@ if (isset($model->productId)) {
                 <span>
                     <small>in <?php
                         $brand = common\models\costfit\Brand::find()->where('brandId=' . $model->brandId)->one();
-                        echo $brand->title;
+                        echo isset($brand) ? $brand->title : "";
                         ?></small>
                 </span>
                 <div class="" style="height: 60px;">
@@ -56,7 +56,7 @@ if (isset($model->productId)) {
                         <?= substr($model->title, 0, 35);
                         ?></a>
                 </div>
-                <span><?php //= $model->shortDescription;         ?></span>
+                <span><?php //= $model->shortDescription;          ?></span>
                 <a href="<?php echo Yii::$app->homeUrl; ?>products/<?= $model->encodeParams(['productId' => $model->productId, 'productSupplierId' => $model->productSuppId]) ?>"><button class="btn btn-primary" id="addItemToCart"><i class="fa fa-search"></i>View</button></a>
             </div>
         </div>
