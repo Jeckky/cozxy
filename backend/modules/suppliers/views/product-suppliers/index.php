3e889032a8ba15f312d976690a9a4b1f47aeb733
Pew CodesLoves <sodapew17@gmail.com>
2017-05-04 09:10:57 +0700

    update

--- backend/modules/suppliers/views/product-suppliers/index.php
+++ backend/modules/suppliers/views/product-suppliers/index.php
@@ -98,7 +98,7 @@ if (Yii::$app->user->identity->type != 4 && Yii::$app->user->identity->type != 5
         <div class="panel-body">
             <?php
             //Pjax::begin(['id' => 'employee-grid-view']);
-            Pjax::begin(['id' => 'employee-grid-view', 'enablePushState' => FALSE, 'clientOptions' => ['method' => 'POST']], ['data-pjax' => '']);
+            Pjax::begin(['id' => 'employee-grid-view', 'enablePushState' => FALSE, 'clientOptions' => ['method' => 'POST']]);
             ?>
             <?=
             GridView::widget([
