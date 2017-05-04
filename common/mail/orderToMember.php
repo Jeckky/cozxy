620f43f23dd9e4216c7b2c7dab0762c7ed531fda
Pew CodesLoves <sodapew17@gmail.com>
2017-05-04 14:44:19 +0700

    update

--- common/mail/orderToMember.php
+++ common/mail/orderToMember.php
@@ -192,7 +192,59 @@
                                                         //if ($value1->receiveType == 1) {
                                                         ?>
                                                         <tr style="background-color:#f1f1f1 ; border-bottom: 1px #000000 solid; height: 25px; text-align: left; color: #166db9;">
-                                                            <td style="font-size: 12px;" colspan="7"><?php echo ($value1->receiveType == 1) ? ' สถานที่รับของ : ปลายทางที่ <strong>Lockers</strong>' : ' สถานที่รับของ : ปลายทางที่ <strong>Booth</strong>'; ?></td>
+                                                            <td style="font-size: 12px;" colspan="7">
+                                                                <?php
+                                                                $GetOrder = common\models\costfit\OrderItem::find()->where('orderId=' . $value1['orderId'] . ' and supplierId=' . $value1['supplierId'] . ' and receiveType=' . $value1->receiveType)->groupBy('orderId')->one();
+                                                                if (isset($GetOrder->pickingId)) {
+                                                                    $picking_point = common\models\costfit\PickingPoint::find()->where('pickingId=' . $GetOrder->pickingId)->one();
+                                                                    if (count($picking_point) > 0) {
+                                                                        $picking_pointName = $picking_point->title;
+                                                                    } else {
+                                                                        $picking_pointName = '';
+                                                                    }
+                                                                    $Countries = common\models\dbworld\Countries::find()->where("countryId= '" . $picking_point->countryId . "' ")->one();
+                                                                    if (count($Countries) > 0) {
+                                                                        $CountriesName = $Countries->localName;
+                                                                    } else {
+                                                                        $CountriesName = '';
+                                                                    }
+                                                                    $States = common\models\dbworld\States::find()->where("stateId='" . $picking_point->provinceId . "'")->one();
+                                                                    if (count($States) > 0) {
+                                                                        $StateslocalName = $States->localName;
+                                                                    } else {
+                                                                        $StateslocalName = '';
+                                                                    }
+                                                                    $Cities = common\models\dbworld\Cities::find()->where("cityId='" . $picking_point->amphurId . "'")->one();
+                                                                    if (count($Cities) > 0) {
+                                                                        $CitieslocalName = $Cities->localName;
+                                                                    } else {
+                                                                        $CitieslocalName = '';
+                                                                    }
+                                                                    if ($value1->receiveType == 1) {
+                                                                        //echo 'Pickup location: ปลายทางที่ <strong><span style="color: #0286c2;">Lockers เย็น</span></strong>';
+                                                                        echo 'Pickup location : ' . $picking_pointName;
+                                                                        echo ', ' . $CountriesName;
+                                                                        echo ', ' . $StateslocalName;
+                                                                        echo ', ' . $CitieslocalName;
+                                                                    } elseif ($value1->receiveType == 2) {
+                                                                        echo 'Pickup location : ปลายทางที่ <strong><span style="color: #0286c2;">Lockers ร้อน</span></strong>';
+                                                                    } elseif ($value1->receiveType == 3) {
+                                                                        echo 'Pickup location : ปลายทางที่ <strong><span style="color: #0286c2;">Booth</span></strong>';
+                                                                    }
+                                                                }
+
+                                                                /*
+                                                                  $picking_point = common\models\costfit\PickingPoint::find()->where('pickingId=' . $GetOrder->pickingId)->one();
+                                                                  $Countries = common\models\dbworld\Countries::find()->where("countryId= '" . $picking_point->countryId . "' ")->one();
+                                                                  $States = common\models\dbworld\States::find()->where("stateId='" . $picking_point->provinceId . "'")->one();
+                                                                  $Cities = common\models\dbworld\Cities::find()->where("cityId='" . $picking_point->amphurId . "'")->one();
+                                                                  echo '<b>จุดรับสินค้าที่ :</b>' . $picking_point->title;
+                                                                  echo ', <b>ประเทศ :</b>' . $Countries->localName;
+                                                                  echo ', ' . $States->localName;
+                                                                  echo ', ' . $Cities->localName;
+                                                                  echo ($value1->receiveType == 1) ? ' สถานที่รับของ : ปลายทางที่ <strong>Lockers</strong>' : ' สถานที่รับของ : ปลายทางที่ <strong>Booth</strong>'; */
+                                                                ?>
+                                                            </td>
                                                         </tr>
                                                         <?php
                                                         $GetOrder = common\models\costfit\OrderItem::find()->where('orderId=' . $value1['orderId'] . ' and supplierId=' . $value1['supplierId'] . ' and receiveType=' . $value1->receiveType)->all();
@@ -215,23 +267,23 @@
                                                             </tr>
                                                             <?php
                                                         }
-                                                        $GetOrder = common\models\costfit\OrderItem::find()->where('orderId=' . $value1['orderId'] . ' and supplierId=' . $value1['supplierId'] . ' and receiveType=' . $value1->receiveType)->groupBy('orderId')->one();
+                                                        //$GetOrder = common\models\costfit\OrderItem::find()->where('orderId=' . $value1['orderId'] . ' and supplierId=' . $value1['supplierId'] . ' and receiveType=' . $value1->receiveType)->groupBy('orderId')->one();
                                                         ?>
-                                                        <tr>
-                                                            <td style="font-size: 12px; text-align: left;" colspan="7">
-                                                                <strong>สถานที่รับของ :</strong><br>
-                                                                <?php
-                                                                $picking_point = common\models\costfit\PickingPoint::find()->where('pickingId=' . $GetOrder->pickingId)->one();
-                                                                $Countries = common\models\dbworld\Countries::find()->where("countryId= '" . $picking_point->countryId . "' ")->one();
-                                                                $States = common\models\dbworld\States::find()->where("stateId='" . $picking_point->provinceId . "'")->one();
-                                                                $Cities = common\models\dbworld\Cities::find()->where("cityId='" . $picking_point->amphurId . "'")->one();
-                                                                echo '<b>จุดรับสินค้าที่ :</b>' . $picking_point->title;
-                                                                echo ', <b>ประเทศ :</b>' . $Countries->localName;
-                                                                echo ', ' . $States->localName;
-                                                                echo ', ' . $Cities->localName;
-                                                                ?>
-                                                            </td>
-                                                        </tr>
+                                        <!--<tr>
+                                            <td style="font-size: 12px; text-align: left;" colspan="7">
+                                                <strong>สถานที่รับของ :</strong><br>
+                                                        <?php
+                                                        /* $picking_point = common\models\costfit\PickingPoint::find()->where('pickingId=' . $GetOrder->pickingId)->one();
+                                                          $Countries = common\models\dbworld\Countries::find()->where("countryId= '" . $picking_point->countryId . "' ")->one();
+                                                          $States = common\models\dbworld\States::find()->where("stateId='" . $picking_point->provinceId . "'")->one();
+                                                          $Cities = common\models\dbworld\Cities::find()->where("cityId='" . $picking_point->amphurId . "'")->one();
+                                                          echo '<b>จุดรับสินค้าที่ :</b>' . $picking_point->title;
+                                                          echo ', <b>ประเทศ :</b>' . $Countries->localName;
+                                                          echo ', ' . $States->localName;
+                                                          echo ', ' . $Cities->localName; */
+                                                        ?>
+                                            </td>
+                                        </tr>-->
                                                         <?php
                                                         //} /* $value1->receiveType == 1 : Lockers */
                                                     }
