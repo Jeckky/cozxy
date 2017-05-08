c1ae1418888c118e542458d5cf8a0f2002b776d2
Pew CodesLoves <sodapew17@gmail.com>
2017-05-08 15:04:42 +0700

    update

--- frontend/views/site/_popular_cat.php
+++ frontend/views/site/_popular_cat.php
@@ -14,9 +14,9 @@ $baseUrl = Yii::$app->getUrlManager()->getBaseUrl();
         <div class="footer" id="products-popular-footer" >
             <a href="<?php echo $baseUrl; ?>/search/<?= $model->createTitle() ?>/<?= $model->encodeParams(['categoryId' => $model->categoryId]) ?>"><?= $model->title; ?></a>
             <span>
-                <small><?= $model->title; ?></small>
+                <small><?//= $model->title; ?></small>
             </span>
-            <a href="<?php echo $baseUrl; //Yii::$app->homeUrl;  ?>/search/<?= $model->createTitle() ?>/<?= $model->encodeParams(['categoryId' => $model->categoryId]) ?>"><button class="btn btn-primary btn-sm">view</button></a>
+            <a href="<?php echo $baseUrl; //Yii::$app->homeUrl;   ?>/search/<?= $model->createTitle() ?>/<?= $model->encodeParams(['categoryId' => $model->categoryId]) ?>"><button class="btn btn-primary btn-sm">view</button></a>
         </div>
     </div>
 </div>
