e5523dc24eedbd22e2696d1f728a797be0056065
sakqqboy <saknakhngam@hotmail.com>
2017-05-23 11:28:49 +0700

    Update

--- frontend/themes/cozxy/layouts/main.php
+++ frontend/themes/cozxy/layouts/main.php
@@ -14,22 +14,31 @@ AppAsset::register($this);
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
-<head>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
-</head>
-<body>
-<?php $this->beginBody() ?>
-<?=$this->render('_menu')?>
+    <head>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?= Html::csrfMetaTags() ?>
+        <title><?= Html::encode($this->title) ?></title>
+        <?php $this->head() ?>
+        <?php
+        $this->registerJs("(function(i,s,o,g,r,a,m){i[‘GoogleAnalyticsObject’]=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,‘script’,‘https://www.google-analytics.com/analytics.js','ga');
 
-<?= $content ?>
+ ga(‘create’, ‘UA-99594678-1’, ‘auto’);
+  ga(‘send’, ‘pageview’);");
+        ?>
+    </head>
+    <body>
+        <?php $this->beginBody() ?>
+        <?= $this->render('_menu') ?>
 
-<?=$this->render('_footer')?>
+        <?= $content ?>
 
-<?php $this->endBody() ?>
-</body>
+        <?= $this->render('_footer') ?>
+
+        <?php $this->endBody() ?>
+    </body>
 </html>
 <?php $this->endPage() ?>
