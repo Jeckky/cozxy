c16918ac3996224447753223c2feb1c39f85ef1f
Pew CodesLoves <sodapew17@gmail.com>
2017-05-23 07:24:06 +0700

    update

--- frontend/themes/cozxy/layouts/story/_panel_recent_stories.php
+++ frontend/themes/cozxy/layouts/story/_panel_recent_stories.php
@@ -1,18 +1,26 @@
-<div class="media">
-    <div class="media-left">
-        <a href="#">
-            <img alt="64x64" class="media-object img-circle" data-src="holder.js/64x64" src="" data-holder-rendered="true" style="width: 64px; height: 64px;">
-        </a>
-    </div>
-    <div class="media-body">
-        <h4 class="media-heading size14" style="margin:0px;">Media heading</h4>
-        <p class="size12" style="margin:0px;color:#989898;">Cras sit amet nibh libero, in
-            gravida nulla. Nulla... more.</p>
-        <div class="size12">
-            <i class="fa fa-eye" style="color:#989898;"></i>
-            <span style="color:rgb(254, 230, 10)">1,234</span>&nbsp;&nbsp;<i class="fa fa-star" style="color:#989898;"></i>
-            <span style="color:rgb(254, 230, 10)">4.32</span>
+<div class="panel panel-defailt">
+    <div class="size14" style="background-color:rgb(254, 230, 10);">&nbsp;</div>
+    <h3 class="page-header" style="margin:10px 20px;">Recent Stories</h3>
+    <div class="panel-body">
+        <div class="media">
+            <div class="media-left">
+                <a href="#">
+                    <img alt="64x64" class="media-object img-circle" data-src="holder.js/64x64" src="" data-holder-rendered="true" style="width: 64px; height: 64px;">
+                </a>
+            </div>
+            <div class="media-body">
+                <h4 class="media-heading size14" style="margin:0px;">Media heading</h4>
+                <p class="size12" style="margin:0px;color:#989898;">Cras sit amet nibh libero, in
+                    gravida nulla. Nulla... more.</p>
+                <div class="size12">
+                    <i class="fa fa-eye" style="color:#989898;"></i>
+                    <span style="color:rgb(254, 230, 10)">1,234</span>&nbsp;&nbsp;<i class="fa fa-star" style="color:#989898;"></i>
+                    <span style="color:rgb(254, 230, 10)">4.32</span>
+                </div>
+            </div>
         </div>
+        <hr>
+        <div class="text-center"><a href="#" class="b btn-g999" style="margin:24px auto 12px">See
+                All</a></div>
     </div>
 </div>
-<hr>
