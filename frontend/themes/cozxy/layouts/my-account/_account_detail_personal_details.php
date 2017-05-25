9195c7a09e79a3e07b77568a9f85e5a7bb2e5e4e
Pew CodesLoves <sodapew17@gmail.com>
2017-05-25 13:47:01 +0700

    update formatter asDate >birthDate

--- frontend/themes/cozxy/layouts/my-account/_account_detail_personal_details.php
+++ frontend/themes/cozxy/layouts/my-account/_account_detail_personal_details.php
@@ -3,7 +3,7 @@
 <div class="col-sm-12 col-md-2">E-mail:</div>
 <div class="col-sm-12 col-md-10"><?= $model['email'] ?></div>
 <div class="col-sm-12 col-md-2">Birthday:</div>
-<div class="col-sm-12 col-md-10"><?= $model['birthDate'] ?></div>
+<div class="col-sm-12 col-md-10"><?= Yii::$app->formatter->asDate($model['birthDate'], 'yyyy-MM-dd'); // 2014-10-06  ?></div>
 <div class="col-sm-12 col-md-2">Password:</div>
 <div class="col-sm-12 col-md-10"><?= \yii\bootstrap\Html::a('Change Password', \yii\helpers\Url::to(['my-account/change-password']), ['class' => 'fc-g999']) ?> </div>
 <div class="size12">&nbsp;</div>
