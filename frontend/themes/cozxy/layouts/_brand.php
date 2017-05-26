07290f63d0c5198f588d14505e12bf277c87e992
Pew CodesLoves <sodapew17@gmail.com>
2017-05-26 10:22:04 +0700

    update image brand

---
+++ frontend/themes/cozxy/layouts/_brand.php
@@ -0,0 +1,3 @@
+<a class="" href="<?php echo $model['url']; ?>">
+    <img src="<?php echo $model['image']; ?>" alt="" class="img-responsive"/>
+</a>
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -98,21 +98,22 @@ $this->title = 'My Yii Application';
                         <a href="#"><img src="imgs/banner07.png" alt=""></a>
                         <a href="#"><img src="imgs/banner08.png" alt=""></a>-->
                         <?php
-                        $brands = common\models\costfit\Brand::find()->all();
-                        foreach ($brands as $brand) {
-                            if (isset($brand->image) && !empty($brand->image)) {
-                                if (file_exists(Yii::$app->basePath . "/web/" . $brand->image)) {
-                                    $brandImages = $brand->image;
-                                } else {
-                                    $brandImages = Yii::$app->homeUrl . 'images/no-image.jpg';
-                                }
-                            }
-                            ?>
-                            <a class="" href="<?php echo Yii::$app->homeUrl; ?>brand/<?= $brand->createTitle() ?>/<?php echo \common\models\ModelMaster::encodeParamsBrand(['brandId' => $brand->brandId]); ?>">
-                                <img src="<?php echo $brandImages; ?>" alt="" class="img-responsive"/></a>
-                            <?php
-                        }
-                        ?>
+                        echo \yii\widgets\ListView::widget([
+                            'dataProvider' => $productBrand,
+                            'options' => [
+                                'tag' => false,
+                            ],
+                            'itemView' => function ($model, $key, $index, $widget) {
+                                return $this->render('@app/themes/cozxy/layouts/_brand', ['model' => $model, 'index' => $index]);
+                            },
+                            // 'summaryOptions' => ['class' => 'sort-by-section clearfix'],
+                            //'layout'=>"{summary}{pager}{items}"
+                            'layout' => "{items}",
+                            'itemOptions' => [
+                                'tag' => false,
+                            ],
+                        ]);
+                        ?> 
                     </div>
                     <!--<a class="align-middle fc-g999 size24 scrollingHotSpotRight" href="#" style="padding-top:8px;right:0"><span class="glyphicon glyphicon-menu-right"></span></a>-->
                 </div>
