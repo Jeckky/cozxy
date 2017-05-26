484417e3a34c8d57cafa66d97563cb426f60e121
Pew CodesLoves <sodapew17@gmail.com>
2017-05-26 09:00:11 +0700

    update

--- frontend/themes/cozxy/layouts/product/_product_item.php
+++ frontend/themes/cozxy/layouts/product/_product_item.php
@@ -3,7 +3,7 @@
     <div class="product-box">
         <div class="product-img text-center">
 
-            <img alt="262x262" class="media-object fullwidth" data-src="holder.js/262x262" src="<?= $model['image'] ?>" data-holder-rendered="true" style="width: 262px; height: 262px;">
+            <img alt="262x262" class="media-object fullwidth" data-src="holder.js/262x262" src="<?= $model['image'] ?>" data-holder-rendered="true">
 
             <div class="v-hover">
                 <a href="<?= $model['url'] ?>">
