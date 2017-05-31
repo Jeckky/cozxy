de88a0990252596eff67d890a38b6b7c5e34c4a4
kamyjapkung <kamyjap@gmail.com>
2017-05-31 10:35:42 +0700

    produc group new version

--- backend/modules/product/views/product-group/101/step.php
+++ backend/modules/product/views/product-group/101/step.php
@@ -6,6 +6,7 @@ use yii\helpers\ArrayHelper;
 use yii\widgets\MaskedInput;
 use common\models\areawow;
 use yii\jui\DatePicker;
+use kartik\grid\GridView;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\costfit\ProductGroup */
@@ -85,7 +86,7 @@ $this->params['pageHeader'] = Html::encode($this->title);
         }
 
         .wizard .nav-tabs > li {
-            width: 25%;
+            width: 20%;
         }
 
         .wizard li:after {
@@ -174,9 +175,8 @@ $this->params['pageHeader'] = Html::encode($this->title);
             <div class="panel-body">
                 <?php
                 $form = ActiveForm::begin([
-                    'enableClientValidation' => ($step == 2) ? false : TRUE,
-//            'options' => ['class' => 'panel panel-default form-horizontal', 'enctype' => 'multipart/form-data'],
-                    'options' => ['class' => 'panel panel-default form-horizontal'],
+                    'enableClientValidation' => ($step == 2 || $step == 3) ? false : TRUE,
+                    'options' => ['class' => 'panel panel-default form-horizontal', 'enctype' => 'multipart/form-data'],
                     'fieldConfig' => [
                         'template' => '{label}<div class="col-sm-9">{input}</div>',
                         'labelOptions' => [
@@ -204,7 +204,7 @@ $this->params['pageHeader'] = Html::encode($this->title);
                                         <li role="presentation" class="<?= ($step == 2) ? "active" : "disabled not-active" ?>">
                                             <a href="#step2" data-toggle="tab" aria-controls="step2" role="tab" title="Step 2">
                                                 <span class="round-tab">
-                                                    <i class="fa fa-pencil"></i>
+                                                    <i class="fa fa-picture-o"></i>
                                                 </span>
                                             </a>
                                         </li>
@@ -217,7 +217,15 @@ $this->params['pageHeader'] = Html::encode($this->title);
                                         </li>
 
                                         <li role="presentation" class="<?= ($step == 4) ? "active" : "disabled not-active" ?>">
-                                            <a href="#step4" data-toggle="tab" aria-controls="complete" role="tab" title="Complete">
+                                            <a href="#step4" data-toggle="tab" aria-controls="complete" role="tab" title="Step 4">
+                                                <span class="round-tab">
+                                                    <i class="fa fa-pencil"></i>
+                                                </span>
+                                            </a>
+                                        </li>
+
+                                        <li role="presentation" class="<?= ($step == 5) ? "active" : "disabled not-active" ?>">
+                                            <a href="#step5" data-toggle="tab" aria-controls="complete" role="tab" title="Complete">
                                                 <span class="round-tab">
                                                     <i class="fa fa-check"></i>
                                                 </span>
@@ -235,9 +243,11 @@ $this->params['pageHeader'] = Html::encode($this->title);
 
                                         <?= $form->field($model, 'title', ['options' => ['class' => 'row form-group']])->textInput(['maxlength' => 200, 'value' => isset($title) ? $title : false]); ?>
 
-                                        <?= $form->field($model, 'description', ['options' => ['class' => 'row form-group']])->widget(\yii\redactor\widgets\Redactor::className()) ?>
+                                        <?= $form->field($model, 'description', ['options' => ['class' => 'row form-group']])->textArea(['rows' => '6']) ?>
 
-                                        <?= $form->field($model, 'specification', ['options' => ['class' => 'row form-group']])->widget(\yii\redactor\widgets\Redactor::className()) ?>
+                                        <?= $form->field($model, 'specification', ['options' => ['class' => 'row form-group']])->textArea(['rows' => '6']) ?>
+
+
 
                                         <ul class="list-inline pull-right">
                                             <li>
@@ -247,7 +257,20 @@ $this->params['pageHeader'] = Html::encode($this->title);
                                         </ul>
                                     </div>
                                     <div class="tab-pane <?= ($step == 2) ? " active" : " " ?>" role="tabpanel" id="step2">
-                                        <h3>Step 2 - Choose Option</h3>
+                                        <h3>Step 2 - Product Image</h3>
+                                        <?php if (isset($_GET["productGroupId"])): ?>
+                                            <?= $this->render("_image_form", ["id" => $_GET["productGroupId"]]); ?>
+                                        <?php endif; ?>
+                                        <ul class="list-inline pull-right">
+                                            <li><button type="button" class="btn btn-default prev-step">Previous</button></li>
+                                            <li>
+                                                <!-- <button type="button" class="btn btn-primary next-step">Save and continue</button>-->
+                                                <?php echo Html::submitButton('Next', ['class' => 'btn btn-primary next-step', 'name' => 'next', 'value' => 'next']); ?>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="tab-pane <?= ($step == 3) ? " active" : " " ?>" role="tabpanel" id="step3">
+                                        <h3>Step 3</h3>
                                         <?php if (isset($productGroupTemplateOptions)): ?>
                                             <table class="table table-bordered table-hover">
                                                 <thead>
@@ -276,24 +299,49 @@ $this->params['pageHeader'] = Html::encode($this->title);
                                         <?php endif; ?>
                                         <ul class="list-inline pull-right">
                                             <li><button type="button" class="btn btn-default prev-step">Previous</button></li>
-                                            <li>
-                                                <!-- <button type="button" class="btn btn-primary next-step">Save and continue</button>-->
-                                                <?php echo Html::submitButton('Next', ['class' => 'btn btn-primary next-step', 'name' => 'next', 'value' => 'next']); ?>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    <div class="tab-pane <?= ($step == 3) ? " active" : " " ?>" role="tabpanel" id="step3">
-                                        <h3>Step 3</h3>
-                                        <p>This is step 3</p>
-                                        <ul class="list-inline pull-right">
-                                            <li><button type="button" class="btn btn-default prev-step">Previous</button></li>
-                                            <li><button type="button" class="btn btn-default next-step">Skip</button></li>
-                                            <li><button type="button" class="btn btn-primary btn-info-full next-step">Save and continue</button></li>
+                                            <!--<li><button type="button" class="btn btn-default next-step">Skip</button></li>-->
+                                            <li><?php echo Html::submitButton('Next', ['class' => 'btn btn-primary next-step', 'name' => 'next', 'value' => 'next']); ?></li>
                                         </ul>
                                     </div>
                                     <div class="tab-pane <?= ($step == 4) ? " active" : " " ?>" role="tabpanel" id="complete">
                                         <h3>Complete</h3>
-                                        <p>You have successfully completed all steps.</p>
+                                        <?php
+                                        // Control your pjax options
+                                        if (isset($dataProvider)) {
+                                            echo GridView::widget([
+                                                'dataProvider' => $dataProvider,
+//                                                'filterModel' => $searchModel,
+                                                'columns' => $gridColumns,
+                                                'pjax' => true,
+                                                'pjaxSettings' => [
+                                                    'neverTimeout' => true,
+                                                    'beforeGrid' => 'My fancy content before.',
+                                                    'afterGrid' => 'My fancy content after.',
+                                                ],
+                                                'containerOptions' => ['style' => 'overflow: auto'], // only set when $responsive = false
+                                                'headerRowOptions' => ['class' => 'kartik-sheet-style'],
+                                                'filterRowOptions' => ['class' => 'kartik-sheet-style'],
+                                                'pjax' => true, // pjax is set to always true for this demo
+                                                // set your toolbar
+//                                                'toolbar' => [
+//                                                    ['content' =>
+//                                                        Html::button('<i class="glyphicon glyphicon-plus"></i>', ['type' => 'button', 'title' => Yii::t('kvgrid', 'Add Book'), 'class' => 'btn btn-success', 'onclick' => 'alert("This will launch the book creation form.\n\nDisabled for this demo!");']) . ' ' .
+//                                                        Html::a('<i class="glyphicon glyphicon-repeat"></i>', ['grid-demo'], ['data-pjax' => 0, 'class' => 'btn btn-default', 'title' => Yii::t('kvgrid', 'Reset Grid')])
+//                                                    ],
+//                                                    '{export}',
+//                                                    '{toggleData}',
+//                                                ],
+                                                // set export properties
+                                                'export' => [
+                                                    'fontAwesome' => true
+                                                ],
+                                                'panel' => [
+                                                    'type' => GridView::TYPE_PRIMARY,
+                                                    'heading' => "Product Editor",
+                                                ],
+                                            ]);
+                                        }
+                                        ?>
                                     </div>
                                     <div class="clearfix"></div>
                                 </div>
@@ -305,6 +353,27 @@ $this->params['pageHeader'] = Html::encode($this->title);
             </div>
         </div>
 
+        <?php $this->registerJs("
+           init.push(function () {
+            if (!$('html').hasClass('ie8')) {
+                $('#product-description').summernote({
+                    height: 200,
+                    tabsize: 2,
+                    codemirror: {
+                        theme: 'monokai'
+                    }
+                });
+                $('#product-specification').summernote({
+                    height: 200,
+                    tabsize: 2,
+                    codemirror: {
+                        theme: 'monokai'
+                    }
+                });
+            }
 
+        });
+
+", \yii\web\View::POS_END); ?>
 
     </div>
