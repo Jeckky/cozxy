08d09836699b566da0dbb723d10e5dcdabf9f7e6
kamyjapkung <kamyjap@gmail.com>
2017-06-03 14:31:32 +0700

    ปปป

--- backend/modules/product/views/product-group/101/_product_form.php
+++ backend/modules/product/views/product-group/101/_product_form.php
@@ -46,6 +46,35 @@ $form = ActiveForm::begin([
                 </div>
             </div>
 
+            <?php
+            //echo Html::hiddenInput('input-type-1', $model->categoryId, ['id' => 'input-type-1']);
+            //echo Html::hiddenInput('input-type-2', $model->categoryId, ['id' => 'input-type-2']);
+            echo $form->field($model, 'categoryId')->widget(kartik\select2\Select2::classname(), [
+//            'data' => yii\helpers\ArrayHelper::map(common\models\costfit\Category::find()->all(), 'categoryId', 'title'),
+                'data' => common\models\costfit\search\Category::findCategoryArrayWithMultiLevelBackend(),
+                'pluginOptions' => [
+                    'loadingText' => '-- Select Category System --',
+                //'params' => ['input-type-1', 'input-type-2']
+                ],
+                'options' => [
+                    'placeholder' => 'Select Category System ...',
+                    'id' => 'categoryId',
+                    'class' => 'required'
+                ],
+            ]); //->label('Category');
+            echo $form->field($model, 'brandId')->widget(kartik\select2\Select2::classname(), [
+                'data' => yii\helpers\ArrayHelper::map(common\models\costfit\Brand::find()->all(), 'brandId', 'title'),
+                'pluginOptions' => [
+                    'loadingText' => '-- Select Brand --',
+                ],
+                'options' => [
+                    'placeholder' => 'Select Brand ...',
+                    'id' => 'brandId',
+                    'class' => 'required'
+                ],
+            ]); //->label('Brand');
+            ?>
+
             <?= $form->field($model, 'description', ['options' => ['class' => 'row form-group']])->textArea(['rows' => '6']) ?>
 
             <?= $form->field($model, 'specification', ['options' => ['class' => 'row form-group']])->textArea(['rows' => '6']) ?>
