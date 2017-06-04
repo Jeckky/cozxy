f362aa2e3ec06a1bda0bf5d5da8f232c07b9e0b5
kamyjapkung <kamyjap@gmail.com>
2017-06-04 13:13:28 +0700

    xxx

--- backend/modules/product/views/product-group/101/_product_grid.php
+++ backend/modules/product/views/product-group/101/_product_grid.php
@@ -152,6 +152,19 @@ if (isset($dataProvider)) {
                     return $optionStr;
                 }
             ],
+            [
+                'attribute' => 'Product Supplier',
+                'format' => 'html',
+                'value' => function($model) {
+                    $ps = \common\models\costfit\ProductSuppliers::find()->where("productId = $model->productId AND userId = " . Yii::$app->user->id)->one();
+                    if (isset($ps)) {
+                        $pps = \common\models\costfit\ProductPriceSuppliers::find("productSuppId = $ps->productSuppId AND status = 1")->where()->one();
+                        return isset($pps) ? "Stock : $ps->result" . "<br> Selling Price : " . $pps->price : "Stock : $ps->result";
+                    } else {
+                        return NULL;
+                    }
+                }
+            ],
 //                    [
 //                        'class' => 'kartik\grid\BooleanColumn',
 //                        'attribute' => 'status',
