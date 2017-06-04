086343187992c853c69e0230e1c246d3df1407ab
Pew CodesLoves <sodapew17@gmail.com>
2017-06-04 14:14:06 +0700

    update

--- frontend/web/themes/cozxy/js/cozxy.reform.js
+++ frontend/web/themes/cozxy/js/cozxy.reform.js
@@ -596,7 +596,8 @@ function filterPriceCozxy() {
     $min = $('input:hidden:eq(0)', '#amount-min').val();
     $max = $('input:hidden:eq(1)', '#amount-min').val();
     $categoryId = $('input:hidden:eq(2)', '#amount-min').val();
-    $('.btn-black-s').html('<i class="fa fa-circle-o-notch fa-spin" aria-hidden="true"></i>');
+    $('.btn-black-s').html('APPLY ...');
+    $('.filter-product-cozxy').html("<div class='text-center' style='zoom: 5;'><br><br><i class='fa fa-spinner fa-spin' aria-hidden='true'></i></div>");
     var path = $baseUrl + "search/filter-price";
     $.ajax({
         url: path,
