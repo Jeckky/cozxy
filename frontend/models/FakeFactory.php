3834cc16aefedaea7b2992a92edaed5345fe96c0
Pew CodesLoves <sodapew17@gmail.com>
2017-06-06 16:26:19 +0700

    update

--- frontend/models/FakeFactory.php
+++ frontend/models/FakeFactory.php
@@ -220,7 +220,7 @@ class FakeFactory extends Model {
         foreach ($productPost as $value) {
             $productPostList = \common\models\costfit\ProductSuppliers::find()->where('productId =' . $value->productId)->all();
             foreach ($productPostList as $items) {
-                $productImages = \common\models\costfit\ProductImage::find()->where('productId=' . $items->productId)->one();
+                $productImages = \common\models\costfit\ProductImageSuppliers::find()->where('productSuppId=' . $items['productSuppId'])->one();
                 $productPrice = \common\models\costfit\ProductPriceSuppliers::find()->where('productSuppId=' . $items->productSuppId)->orderBy('productPriceId desc')->limit(1)->one();
                 $price_s = number_format($productPrice->price, 2);
                 $price = number_format($productPrice->price, 2);
