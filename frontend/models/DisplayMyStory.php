00b829d6e065f97223780434554c3beefd538828
Pew CodesLoves <sodapew17@gmail.com>
2017-06-06 14:08:05 +0700

    update

--- frontend/models/DisplayMyStory.php
+++ frontend/models/DisplayMyStory.php
@@ -303,9 +303,9 @@ class DisplayMyStory extends Model {
         ->all();
         $i = 0;
         foreach ($productPost as $value) {
-            $productPostList = \common\models\costfit\Product::find()->where('productId =' . $value->productId)->all();
+            $productPostList = \common\models\costfit\ProductSuppliers::find()->where('productId =' . $value->productId)->all();
             foreach ($productPostList as $items) {
-                $productImages = \common\models\costfit\ProductImageSuppliers::find()->where('productSuppId=' . $productSupplierId)->one();
+                $productImages = \common\models\costfit\ProductImageSuppliers::find()->where('productSuppId=' . $items['productSuppId'])->one();
                 $productPrice = \common\models\costfit\ProductPriceSuppliers::find()->where('productSuppId=' . $items['productId'])->orderBy('productPriceId desc')->limit(1)->one();
                 $price_s = number_format($productPrice->price, 2);
                 $price = number_format($productPrice->price, 2);
