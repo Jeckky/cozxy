cf9d8ec8a92f7e4c74ed66d97d8819679e1e9656
Pew CodesLoves <sodapew17@gmail.com>
2017-06-07 11:03:08 +0700

    update

--- frontend/themes/cozxy/layouts/_brand.php
+++ frontend/themes/cozxy/layouts/_brand.php
@@ -1,3 +1,3 @@
 <a class="" href="<?php echo $model['url']; ?>">
-    <img src="<?php echo $model['image']; ?>" alt="" class="img-responsive"/>
+    <img src="<?php echo $model['image']; ?>" alt="" class="img-responsive" style="width:112px; height: 64px;"/>
 </a>
