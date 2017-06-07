6d50006c7130962707b539ac1bf511a7251ab798
Pew CodesLoves <sodapew17@gmail.com>
2017-06-07 09:54:54 +0700

    Update Quantlty in product Check qSet

--- frontend/web/themes/cozxy/js/cart.js
+++ frontend/web/themes/cozxy/js/cart.js
@@ -81,6 +81,7 @@ function qSet(y, x, productSuppId, orderId, sendDate) {
                     temp = temp - 1;
                     alert("ไม่สามารถสั่งซื้อเกินจำนวนที่กำหนดได้");
 //                    $('.incr-btn').popover('show');
+                    $('.quantity-sel').find('#quantity').val(temp);
                 }
                 $button.parent().find("input").val(temp.toFixed(2));
             }
