3cb04743066a47c60e8e3c66a0060c4842d21308
Pew CodesLoves <sodapew17@gmail.com>
2017-06-07 18:38:14 +0700

    update

--- frontend/themes/cozxy/layouts/cart/_cart_item.php
+++ frontend/themes/cozxy/layouts/cart/_cart_item.php
@@ -29,11 +29,11 @@ $val = rand(1, 10);
             </div>
         </div>
         <div class="col-sm-3 fc-g666">
-            <table style="width:100%">
-                <tr>
+            <table style="width:100%" class="qty-cart">
+                <tr >
                     <td>Quatity</td>
                     <td style="width:32px">:</td>
-                    <td><?= $item["qty"] ?></td>
+                    <td><div id="qty-cart-show"><?= $item["qty"] ?></div></td>
                 </tr>
                 <tr>
                     <td>Color</td>
