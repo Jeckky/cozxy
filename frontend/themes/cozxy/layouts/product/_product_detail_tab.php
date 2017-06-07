389881b92122a87d9da0b781940418bc674debc1
Pew CodesLoves <sodapew17@gmail.com>
2017-06-07 11:21:08 +0700

    update

--- frontend/themes/cozxy/layouts/product/_product_detail_tab.php
+++ frontend/themes/cozxy/layouts/product/_product_detail_tab.php
@@ -21,8 +21,8 @@
                         <ul class="nav nav-pills size18 size14-xs b">
                             <li class="active"><a data-toggle="pill" href="#menu1">DETAIL</a></li>
                             <li><a data-toggle="pill" href="#menu2">SPEC</a></li>
-                            <li><a data-toggle="pill" href="#menu3">BRAND CONTENT</a></li>
-                            <li><a data-toggle="pill" href="#menu4">ETC.</a></li>
+                            <!--<li><a data-toggle="pill" href="#menu3">BRAND CONTENT</a></li>
+                            <li><a data-toggle="pill" href="#menu4">ETC.</a></li>-->
                         </ul>
                         <div class="size18 size14-xs">&nbsp;</div>
                         <div class="tab-content">
@@ -34,6 +34,7 @@
                                 <p><?php echo $model['shortDescriptionCozxy'] ?></p>
                                 <h3>&nbsp;</h3>
                             </div>
+                            <!--
                             <div id="menu3" class="tab-pane fade">
                                 <p>&nbsp;</p>
                                 <h3>&nbsp;</h3>
@@ -41,22 +42,22 @@
                             <div id="menu4" class="tab-pane fade">
                                 <p>&nbsp;</p>
                                 <h3>&nbsp;</h3>
-                            </div>
+                            </div>-->
                         </div>
                     </div>
                 </div>
             </div>
             <div role="tabpanel" class="tab-pane fade in" id="account-suppliers">
                 <div class="row">
-                    <div class="col-xs-12 bg-white">
+                    <div class="col-xs-12 bg-white" style="background-color: #fdfdfd;">
                         <ul class="nav nav-pills size18 size14-xs b">
                             <li class="active"><a data-toggle="pill" href="#menu5">DETAIL</a></li>
                             <li><a data-toggle="pill" href="#menu6">SPEC</a></li>
-                            <li><a data-toggle="pill" href="#menu7">BRAND CONTENT</a></li>
-                            <li><a data-toggle="pill" href="#menu8">ETC.</a></li>
+                            <!--<li><a data-toggle="pill" href="#menu7">BRAND CONTENT</a></li>
+                            <li><a data-toggle="pill" href="#menu8">ETC.</a></li>-->
                         </ul>
                         <div class="size18 size14-xs">&nbsp;</div>
-                        <div class="tab-content">
+                        <div class="tab-content" style="background-color: #fdfdfd;">
                             <div id="menu5" class="tab-pane fade in active">
                                 <p><?php echo $model['description'] ?></p>
                                 <h3>&nbsp;</h3>
@@ -65,6 +66,7 @@
                                 <p><?php echo $model['specification'] ?></p>
                                 <h3>&nbsp;</h3>
                             </div>
+                            <!--
                             <div id="menu7" class="tab-pane fade">
                                 <p>&nbsp;</p>
                                 <h3>&nbsp;</h3>
@@ -72,7 +74,7 @@
                             <div id="menu8" class="tab-pane fade">
                                 <p>&nbsp;</p>
                                 <h3>&nbsp;</h3>
-                            </div>
+                            </div>-->
                         </div>
                     </div>
                 </div>
