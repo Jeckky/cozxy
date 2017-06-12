8ae48b7a14780452eb08c1607f9e2ae3c3c67032
Pew CodesLoves <sodapew17@gmail.com>
2017-06-12 15:18:54 +0700

    my form > be our partner

---
+++ frontend/controllers/BeOurPartnerController.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace frontend\controllers;
+
+use Yii;
+use yii\base\InvalidParamException;
+use yii\data\ArrayDataProvider;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use frontend\models\DisplayMyAccount;
+
+/**
+ * Description of BeOurPartner
+ *
+ * @author it
+ */
+class BeOurPartnerController extends MasterController {
+
+    //put your code here
+    public function actionIndex() {
+        $model = new \common\models\costfit\Address(['scenario' => 'shipping_address']);
+        if (isset($_POST['Address'])) {
+            $model->attributes = $_POST['Address'];
+            if ($_POST["Address"]['isDefault']) {
+                \common\models\costfit\Address::updateAll(['isDefault' => 0], ['userId' => Yii::$app->user->id, 'type' => \common\models\costfit\Address::TYPE_BILLING]);
+                $model->isDefault = 1;
+            }
+            $model->userId = Yii::$app->user->id;
+            $model->type = \common\models\costfit\Address::TYPE_BILLING;
+            $model->createDateTime = new \yii\db\Expression("NOW()");
+            if ($model->save(FALSE)) {
+                return $this->redirect(['/my-account']);
+            }
+        }
+        if (!isset($model->isDefault)) {
+            $model->isDefault = 0;
+        }
+        $hash = 'add';
+        return $this->render('index', compact('model', 'hash'));
+    }
+
+}
