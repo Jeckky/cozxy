c4aec1a0da4f3fd09bb7f57c94b376a8c6a06d22
Pew CodesLoves <sodapew17@gmail.com>
2017-06-14 10:13:39 +0700

    check images master and images suppliers
    - check site images
    - check product views

--- common/helpers/DataImageSystems.php
+++ common/helpers/DataImageSystems.php
@@ -83,45 +83,144 @@ class DataImageSystems {
         /*
          * รูปสินค้า
          */
-        $productImagesOneTop = \common\models\costfit\ProductImageSuppliers::find()->where('productSuppId=' . $productSuppId)->orderBy('ordering asc')->one();
-        if (count($productImagesOneTop) > 0) {
-            //$productImagesAll = \common\models\costfit\ProductImageSuppliers::find()->where('productSuppId=' . $productSuppId . ' and productImageId !=' . $productImagesOneTop['productImageId'])->orderBy('ordering asc')->all();
-            $productImagesAll = \common\models\costfit\ProductImageSuppliers::find()->where('productSuppId=' . $productSuppId)->orderBy('ordering asc')->all();
 
-            foreach ($productImagesAll as $items) {
-                if (isset($items['imageThumbnail1']) && !empty($items['imageThumbnail1'])) {
-                    if (file_exists(Yii::$app->basePath . "/web/" . $items['imageThumbnail1'])) {
-                        $productimageThumbnail1 = Yii::$app->homeUrl . $items['imageThumbnail1'];
-                    } else {
-                        $masterId = $items['productId'];
-                        if (isset($masterId)) {
-                            $ImagesMaster = \common\models\costfit\ProductImage::find()->where('productId=' . $masterId)->one();
-                            if (isset($ImagesMaster->imageThumbnail1) && !empty($ImagesMaster->imageThumbnail1)) {
-                                if (file_exists(Yii::$app->basePath . "/web/" . $ImagesMaster->imageThumbnail1)) {
-                                    $productimageThumbnail1 = \Yii::$app->homeUrl . $ImagesMaster->imageThumbnail1;
+        $productImages = \common\models\costfit\ProductImage::find()->where('productId=' . $producmasterId)->orderBy('ordering asc')->one();
+
+        if (count($productImages) > 0) {// Step Product master
+            $productImagesOneTop = \common\models\costfit\ProductImage::find()->where('productId=' . $producmasterId)->orderBy('ordering asc')->one();
+            if (count($productImagesOneTop) > 0) {
+                //$productImagesAll = \common\models\costfit\ProductImageSuppliers::find()->where('productSuppId=' . $productSuppId . ' and productImageId !=' . $productImagesOneTop['productImageId'])->orderBy('ordering asc')->all();
+                $productImagesAll = \common\models\costfit\ProductImage::find()->where('productId=' . $producmasterId)->orderBy('ordering asc')->all();
+
+                foreach ($productImagesAll as $items) {
+                    if (isset($items['imageThumbnail1']) && !empty($items['imageThumbnail1'])) {
+                        if (file_exists(Yii::$app->basePath . "/web/" . $items['imageThumbnail1'])) {
+                            $productimageThumbnail1 = Yii::$app->homeUrl . $items['imageThumbnail1'];
+                        } else {
+                            $masterId = $items['productId'];
+                            if (isset($masterId)) {
+                                $ImagesMaster = \common\models\costfit\ProductImageSuppliers::find()->where('productSuppId=' . $productSuppId)->one();
+                                if (isset($ImagesMaster->imageThumbnail1) && !empty($ImagesMaster->imageThumbnail1)) {
+                                    if (file_exists(Yii::$app->basePath . "/web/" . $ImagesMaster->imageThumbnail1)) {
+                                        $productimageThumbnail1 = \Yii::$app->homeUrl . $ImagesMaster->imageThumbnail1;
+                                    } else {
+                                        $productimageThumbnail1 = \common\helpers\Base64Decode::DataImageSvg($imageSvg1);
+                                    }
                                 } else {
                                     $productimageThumbnail1 = \common\helpers\Base64Decode::DataImageSvg($imageSvg1);
                                 }
                             } else {
                                 $productimageThumbnail1 = \common\helpers\Base64Decode::DataImageSvg($imageSvg1);
                             }
-                        } else {
-                            $productimageThumbnail1 = \common\helpers\Base64Decode::DataImageSvg($imageSvg1);
+                            //$productimageThumbnail1 = \common\helpers\Base64Decode::DataImageSvg260x260(FALSE, FALSE, FALSE);
                         }
-                        //$productimageThumbnail1 = \common\helpers\Base64Decode::DataImageSvg260x260(FALSE, FALSE, FALSE);
+                    } else {
+                        $productimageThumbnail1 = \common\helpers\Base64Decode::DataImageSvg($imageSvg1);
+                    }
+                    $imagAll[$items['productImageId']] = [
+                        'productImageId' => $items->productImageId,
+                        'imageThumbnail1' => $productimageThumbnail1,
+                    ];
+                }
+
+                if (isset($productImagesOneTop['image']) && !empty($productImagesOneTop['image'])) {
+                    if (file_exists(Yii::$app->basePath . "/web/" . $productImagesOneTop['image'])) {
+                        $productImagesOneTopz = Yii::$app->homeUrl . $productImagesOneTop['image'];
+                    } else {
+                        $suppliersId = $productImagesOneTop['productSuppId'];
+                        if (isset($masterId)) {
+                            $ImagesMaster = \common\models\costfit\ProductImageSuppliers::find()->where('productSuppId=' . $suppliersId)->one();
+                            if (isset($ImagesMaster->image) && !empty($ImagesMaster->image)) {
+                                if (file_exists(Yii::$app->basePath . "/web/" . $ImagesMaster->image)) {
+                                    $productImagesOneTopz = \Yii::$app->homeUrl . $ImagesMaster->image;
+                                } else {
+                                    $productImagesOneTopz = \common\helpers\Base64Decode::DataImageSvg($imageSvg2);
+                                }
+                            } else {
+                                $productImagesOneTopz = \common\helpers\Base64Decode::DataImageSvg($imageSvg2);
+                            }
+                        } else {
+                            $productImagesOneTopz = \common\helpers\Base64Decode::DataImageSvg($imageSvg2);
+                        }
                     }
                 } else {
-                    $productimageThumbnail1 = \common\helpers\Base64Decode::DataImageSvg($imageSvg1);
+                    $suppliersId = $productImagesOneTop['productSuppId'];
+                    if (isset($suppliersId)) {
+                        $ImagesMaster = \common\models\costfit\ProductImageSuppliers::find()->where('productSuppId=' . $suppliersId)->one();
+                        if (isset($ImagesMaster->image) && !empty($ImagesMaster->image)) {
+                            if (file_exists(Yii::$app->basePath . "/web/" . $ImagesMaster->image)) {
+                                $productImagesOneTopz = \Yii::$app->homeUrl . $ImagesMaster->image;
+                            } else {
+                                $productImagesOneTopz = \common\helpers\Base64Decode::DataImageSvg($imageSvg2);
+                            }
+                        } else {
+                            $productImagesOneTopz = \common\helpers\Base64Decode::DataImageSvg($imageSvg2);
+                        }
+                    } else {
+                        $productImagesOneTopz = \common\helpers\Base64Decode::DataImageSvg($imageSvg2);
+                    }
                 }
-                $imagAll[$items['productImageId']] = [
-                    'productImageId' => $items->productImageId,
-                    'imageThumbnail1' => $productimageThumbnail1,
-                ];
+            } else {
+                $productimageThumbnail1 = \common\helpers\Base64Decode::DataImageSvg($imageSvg1);
+                $productImagesOneTopz = \common\helpers\Base64Decode::DataImageSvg($imageSvg2);
             }
+        } else { // Step Product Suppliers
+            $productImagesOneTop = \common\models\costfit\ProductImageSuppliers::find()->where('productSuppId=' . $productSuppId)->orderBy('ordering asc')->one();
+            if (count($productImagesOneTop) > 0) {
+                //$productImagesAll = \common\models\costfit\ProductImageSuppliers::find()->where('productSuppId=' . $productSuppId . ' and productImageId !=' . $productImagesOneTop['productImageId'])->orderBy('ordering asc')->all();
+                $productImagesAll = \common\models\costfit\ProductImageSuppliers::find()->where('productSuppId=' . $productSuppId)->orderBy('ordering asc')->all();
 
-            if (isset($productImagesOneTop['image']) && !empty($productImagesOneTop['image'])) {
-                if (file_exists(Yii::$app->basePath . "/web/" . $productImagesOneTop['image'])) {
-                    $productImagesOneTopz = Yii::$app->homeUrl . $productImagesOneTop['image'];
+                foreach ($productImagesAll as $items) {
+                    if (isset($items['imageThumbnail1']) && !empty($items['imageThumbnail1'])) {
+                        if (file_exists(Yii::$app->basePath . "/web/" . $items['imageThumbnail1'])) {
+                            $productimageThumbnail1 = Yii::$app->homeUrl . $items['imageThumbnail1'];
+                        } else {
+                            $masterId = $items['productId'];
+                            if (isset($masterId)) {
+                                $ImagesMaster = \common\models\costfit\ProductImage::find()->where('productId=' . $masterId)->one();
+                                if (isset($ImagesMaster->imageThumbnail1) && !empty($ImagesMaster->imageThumbnail1)) {
+                                    if (file_exists(Yii::$app->basePath . "/web/" . $ImagesMaster->imageThumbnail1)) {
+                                        $productimageThumbnail1 = \Yii::$app->homeUrl . $ImagesMaster->imageThumbnail1;
+                                    } else {
+                                        $productimageThumbnail1 = \common\helpers\Base64Decode::DataImageSvg($imageSvg1);
+                                    }
+                                } else {
+                                    $productimageThumbnail1 = \common\helpers\Base64Decode::DataImageSvg($imageSvg1);
+                                }
+                            } else {
+                                $productimageThumbnail1 = \common\helpers\Base64Decode::DataImageSvg($imageSvg1);
+                            }
+                            //$productimageThumbnail1 = \common\helpers\Base64Decode::DataImageSvg260x260(FALSE, FALSE, FALSE);
+                        }
+                    } else {
+                        $productimageThumbnail1 = \common\helpers\Base64Decode::DataImageSvg($imageSvg1);
+                    }
+                    $imagAll[$items['productImageId']] = [
+                        'productImageId' => $items->productImageId,
+                        'imageThumbnail1' => $productimageThumbnail1,
+                    ];
+                }
+
+                if (isset($productImagesOneTop['image']) && !empty($productImagesOneTop['image'])) {
+                    if (file_exists(Yii::$app->basePath . "/web/" . $productImagesOneTop['image'])) {
+                        $productImagesOneTopz = Yii::$app->homeUrl . $productImagesOneTop['image'];
+                    } else {
+                        $masterId = $productImagesOneTop['productId'];
+                        if (isset($masterId)) {
+                            $ImagesMaster = \common\models\costfit\ProductImage::find()->where('productId=' . $masterId)->one();
+                            if (isset($ImagesMaster->image) && !empty($ImagesMaster->image)) {
+                                if (file_exists(Yii::$app->basePath . "/web/" . $ImagesMaster->image)) {
+                                    $productImagesOneTopz = \Yii::$app->homeUrl . $ImagesMaster->image;
+                                } else {
+                                    $productImagesOneTopz = \common\helpers\Base64Decode::DataImageSvg($imageSvg2);
+                                }
+                            } else {
+                                $productImagesOneTopz = \common\helpers\Base64Decode::DataImageSvg($imageSvg2);
+                            }
+                        } else {
+                            $productImagesOneTopz = \common\helpers\Base64Decode::DataImageSvg($imageSvg2);
+                        }
+                    }
                 } else {
                     $masterId = $productImagesOneTop['productId'];
                     if (isset($masterId)) {
@@ -140,26 +239,12 @@ class DataImageSystems {
                     }
                 }
             } else {
-                $masterId = $productImagesOneTop['productId'];
-                if (isset($masterId)) {
-                    $ImagesMaster = \common\models\costfit\ProductImage::find()->where('productId=' . $masterId)->one();
-                    if (isset($ImagesMaster->image) && !empty($ImagesMaster->image)) {
-                        if (file_exists(Yii::$app->basePath . "/web/" . $ImagesMaster->image)) {
-                            $productImagesOneTopz = \Yii::$app->homeUrl . $ImagesMaster->image;
-                        } else {
-                            $productImagesOneTopz = \common\helpers\Base64Decode::DataImageSvg($imageSvg2);
-                        }
-                    } else {
-                        $productImagesOneTopz = \common\helpers\Base64Decode::DataImageSvg($imageSvg2);
-                    }
-                } else {
-                    $productImagesOneTopz = \common\helpers\Base64Decode::DataImageSvg($imageSvg2);
-                }
+                $productimageThumbnail1 = \common\helpers\Base64Decode::DataImageSvg($imageSvg1);
+                $productImagesOneTopz = \common\helpers\Base64Decode::DataImageSvg($imageSvg2);
             }
-        } else {
-            $productimageThumbnail1 = \common\helpers\Base64Decode::DataImageSvg($imageSvg1);
-            $productImagesOneTopz = \common\helpers\Base64Decode::DataImageSvg($imageSvg2);
         }
+
+
 
         return array('productimageThumbnail1' => $productimageThumbnail1, 'productImagesOneTopz' => $productImagesOneTopz, 'imagAll' => $imagAll);
     }
