b8c9306a67a3a92f2fff1e7517fb3d545ff8a2b2
Pew CodesLoves <sodapew17@gmail.com>
2017-06-14 15:39:19 +0700

    fixed bug product price

--- frontend/views/search/index.php
+++ frontend/views/search/index.php
@@ -124,9 +124,8 @@ $this->params['breadcrumbs'][] = $this->title;
 <?=
 $this->render('@app/themes/cozxy/layouts/search/_search_filter', [
     'categoryId' => $categoryId,
-])
+]);
 ?>
-
 <div class="product-list">
     <div class="container">
         <div class="row">
@@ -214,7 +213,7 @@ $this->render('@app/themes/cozxy/layouts/search/_search_filter', [
 
 
             <div class="col-xs-9 text-center">
-                <!--<a href="javascript:showMore('<?php //echo $categoryId;                   ?>','<?php //echo $clickNum;                  ?>','<?php //echo $countAllProduct;                  ?>','<?php //echo $limit_start;                 ?>','<?php //echo $limit_end;                ?>')" class="b btn-black showStepMore" style="margin:24px auto 32px">SHOW MORE
+                <!--<a href="javascript:showMore('<?php //echo $categoryId;                            ?>','<?php //echo $clickNum;                           ?>','<?php //echo $countAllProduct;                           ?>','<?php //echo $limit_start;                          ?>','<?php //echo $limit_end;                         ?>')" class="b btn-black showStepMore" style="margin:24px auto 32px">SHOW MORE
                     <span class="size16">&nbsp; ↓ </span></a>-->
             </div>
             <div class="col-xs-3 text-center">&nbsp;</div>
