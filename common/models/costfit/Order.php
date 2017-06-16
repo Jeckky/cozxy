96fec273402f999647bc08173ceb70b002d17d22
Pew CodesLoves <sodapew17@gmail.com>
2017-06-16 11:12:12 +0700

    update

--- common/models/costfit/Order.php
+++ common/models/costfit/Order.php
@@ -179,7 +179,7 @@ class Order extends \common\models\costfit\master\OrderMaster {
                     'priceOnePiece' => $item->priceOnePiece,
                     'priceOnePieceText' => number_format($item->priceOnePiece, 2),
 //                    'priceMarket' => ProductSuppliers::productPriceSupplier($item->productSuppId),
-                    'priceMarket' => isset(number_format($item->product->price, 2)) ? $item->product->price : '',
+                    'priceMarket' => isset($item->product->price) ? number_format($item->product->price, 2) : '',
                     'sendDate' => $item->sendDate,
                     'firstTimeSendDate' => $item->firstTimeSendDate,
                     'sendDateNoDate' => isset($item->shippingType) ? $item->shippingType->date : NULL,
