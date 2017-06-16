b18cef3ddd8af799809f287e7545c116eb35daa8
Pew CodesLoves <sodapew17@gmail.com>
2017-06-16 11:51:55 +0700

    update

--- frontend/themes/cozxy/layouts/cart/_cart_total.php
+++ frontend/themes/cozxy/layouts/cart/_cart_total.php
@@ -13,7 +13,7 @@
             <div class="pull-right"><?= (isset($this->params['cart']['shippingRate']) && $this->params['cart']['shippingRate'] == 0) ? "FREE" : number_format($this->params['cart']['shippingRate'], 2) ?></div>
         </div>
         <div class="price-detail">PROMO CODE
-            <div class="pull-right promo-coupon-codes discountFormatText"><?= number_format($this->params['cart']['discount'], 2) ?> THB</div>
+            <div class="pull-right promo-coupon-codes discountFormatText" style="color: #f65d35;">- <?= number_format($this->params['cart']['discount'], 2) ?> THB</div>
         </div>
         <div class="price-detail b size20 size18-sm size18-xs">TOTAL
             <div class="pull-right summaryFormatText"><?= number_format($this->params ['cart']['summary'], 2) ?> THB</div>
