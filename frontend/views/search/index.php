78dff5533c3681845d563eac918a86cecbe8c66a
Pew CodesLoves <sodapew17@gmail.com>
2017-06-20 07:51:11 +0700

    update icon sort

--- frontend/views/search/index.php
+++ frontend/views/search/index.php
@@ -134,7 +134,14 @@ $this->render('@app/themes/cozxy/layouts/search/_search_filter', [
                     <div class="filter-product-cozxy col-sm-12">
 
                         <?php if ($productCanSell->getTotalCount() > 0): ?>
-                            <h3 class="b"><?= strtoupper('category') ?> :: <?= strtoupper($category) ?> (RECOMMENDED)</h3>
+                            <h3 class="b"><?= strtoupper('category') ?> :: <?= strtoupper($category) ?> (RECOMMENDED)
+                                <small>
+                                    price&nbsp;<i class="fa fa-angle-down" aria-hidden="true"></i>
+                                    brand&nbsp;<i class="fa fa-angle-down" aria-hidden="true"></i>
+                                    new product&nbsp;<i class="fa fa-angle-down" aria-hidden="true"></i>
+                                </small>
+                            </h3>
+
                             <div class="row">
                                 <div class="wf-container">
 
@@ -227,7 +234,7 @@ $this->render('@app/themes/cozxy/layouts/search/_search_filter', [
 
 
             <div class="col-xs-9 text-center">
-                <!--<a href="javascript:showMore('<?php //echo $categoryId;                                                      ?>','<?php //echo $clickNum;                                                     ?>','<?php //echo $countAllProduct;                                                     ?>','<?php //echo $limit_start;                                                    ?>','<?php //echo $limit_end;                                                   ?>')" class="b btn-black showStepMore" style="margin:24px auto 32px">SHOW MORE
+                <!--<a href="javascript:showMore('<?php //echo $categoryId;                                                                   ?>','<?php //echo $clickNum;                                                                  ?>','<?php //echo $countAllProduct;                                                                  ?>','<?php //echo $limit_start;                                                                 ?>','<?php //echo $limit_end;                                                                ?>')" class="b btn-black showStepMore" style="margin:24px auto 32px">SHOW MORE
                     <span class="size16">&nbsp; ↓ </span></a>-->
             </div>
             <div class="col-xs-3 text-center">&nbsp;</div>
--- frontend/web/themes/cozxy/js/cozxy.reform.js
+++ frontend/web/themes/cozxy/js/cozxy.reform.js
@@ -561,7 +561,10 @@ function filterPriceCozxy() {
                     var yourval = jQuery.parseJSON(JSON.stringify(data));
                     //var obj = JSON.parse(data);
                     //console.log(yourval['160']);
-                    var items = '';
+                    var items = '<h3 class="b">CATEGORY ::     \n\
+                                    <small>price <i class="fa fa-angle-down" aria-hidden="true"></i>  \n\
+                                    brand <i class="fa fa-angle-down" aria-hidden="true"></i>  \n\
+                                    new product <i class="fa fa-angle-down" aria-hidden="true"></i></small></h3>';
                     $.each(yourval, function (key, val) {
                         //console.log(key);//160,162
                         //console.log(val.productSuppId);
@@ -641,7 +644,10 @@ function showMore(cat, clickNum, countAll, limit_start, limit_end) {
                 //var yourval = jQuery.parseJSON(JSON.stringify(data));
                 //$('.fa .fa-spinner .fa-spin').removeClass();
                 yourval = JSON.parse(data);
-                var items = '';
+                var items = '<h3 class="b">CATEGORY ::     \n\
+                                    <small>price <i class="fa fa-angle-down" aria-hidden="true"></i>  \n\
+                                    brand <i class="fa fa-angle-down" aria-hidden="true"></i>  \n\
+                                    new product <i class="fa fa-angle-down" aria-hidden="true"></i></small></h3>';
                 $.each(yourval, function (key, val) {
 
                     if (val.fastId == false) {
