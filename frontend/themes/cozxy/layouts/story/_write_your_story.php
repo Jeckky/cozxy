5dfea8eb9175be84a2fede64d70443ec309ff08f
Pew CodesLoves <sodapew17@gmail.com>
2017-07-07 15:04:01 +0700

    update

--- frontend/themes/cozxy/layouts/story/_write_your_story.php
+++ frontend/themes/cozxy/layouts/story/_write_your_story.php
@@ -75,7 +75,7 @@ if (Yii::$app->controller->action->id == 'update-stories') {
             <div class="form-group login-box">
                 <label for="exampleInputEmail1">Short Description</label>
                 <?php
-                echo $form->field($model, 'shortDescription')->textarea(['rows' => '6', 'maxlength' => 150])->hint('Max 150 characters.')->label(FALSE);
+                echo $form->field($model, 'shortDescription')->textarea(['rows' => '6', 'maxlength' => 140])->hint('Max 140 characters.')->label(FALSE);
                 ?>
                 <div id="productpost-shortdescription-text">
                     <blockquote id="result"  style="font-size: 14px;">
