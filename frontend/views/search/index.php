357348c0bc630759477debac2c38cfc81f354e75
Pew CodesLoves <sodapew17@gmail.com>
2017-07-12 18:40:33 +0700

    update

--- frontend/views/search/index.php
+++ frontend/views/search/index.php
@@ -117,6 +117,7 @@ $(function() {
 });
 ');
 \frontend\assets\SearchAsset::register($this);
+$title = '';
 if (isset($title) && !empty($title)) {
     $this->title = 'Search Categories ' . isset($title) ? strtoupper($title) : '';
     $this->params['breadcrumbs'][] = $this->title;
@@ -254,7 +255,7 @@ $this->render('@app/themes/cozxy/layouts/search/_search_filter', [
 
 
             <div class="col-xs-9 text-center">
-                <!--<a href="javascript:showMore('<?php //echo $categoryId;                                                                                                                             ?>','<?php //echo $clickNum;                                                                                                                            ?>','<?php //echo $countAllProduct;                                                                                                                            ?>','<?php //echo $limit_start;                                                                                                                           ?>','<?php //echo $limit_end;                                                                                                                          ?>')" class="b btn-black showStepMore" style="margin:24px auto 32px">SHOW MORE
+                <!--<a href="javascript:showMore('<?php //echo $categoryId;                                                                                                                              ?>','<?php //echo $clickNum;                                                                                                                             ?>','<?php //echo $countAllProduct;                                                                                                                             ?>','<?php //echo $limit_start;                                                                                                                            ?>','<?php //echo $limit_end;                                                                                                                           ?>')" class="b btn-black showStepMore" style="margin:24px auto 32px">SHOW MORE
                     <span class="size16">&nbsp; ↓ </span></a>-->
             </div>
             <div class="col-xs-3 text-center">&nbsp;</div>
