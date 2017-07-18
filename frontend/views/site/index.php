8e372195379318fc142d3d183ae2a09597c47104
Pew CodesLoves <sodapew17@gmail.com>
2017-07-18 15:19:26 +0700

    update

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -189,8 +189,7 @@ $this->title = 'COZXY.COM LOWEST PRICE PRODUCTS';
                     ?>
                 </div>
 
-                <div class="col-sm-12 text-center">
-                    <br><br>
+                <div class="col-sm-12 text-center" style="margin-top: 3px;">
                     <a href="<?= Yii::$app->homeUrl ?>/story/views-more/" class="subs-btn size14-xs">See All</a>
                 </div>
                 <br><br>
