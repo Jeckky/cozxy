cc9a3f61979d9691ccec1fc7e7ccfa3d3661c750
Pew CodesLoves <sodapew17@gmail.com>
2017-07-24 15:24:56 +0700

    update

--- frontend/themes/cozxy/layouts/story/_panel_recent_stories.php
+++ frontend/themes/cozxy/layouts/story/_panel_recent_stories.php
@@ -64,7 +64,11 @@ if (Yii::$app->controller->id == 'product') {
             ?>
 
             <div class="text-center">
-                <a href="<?php echo Yii::$app->homeUrl; ?>story/see-more/<?= \common\models\ModelMaster::encodeParams(['productSupplierId' => $productSupplierId, 'productId' => $productId]); ?>>" class="b btn-g999" style="margin:24px auto 12px">See All</a>
+                <?php
+                if (count($StoryRecentStories->allModels) > 0) {
+                    ?>
+                    <a href="<?php echo Yii::$app->homeUrl; ?>story/see-more/<?= \common\models\ModelMaster::encodeParams(['productSupplierId' => $productSupplierId, 'productId' => $productId]); ?>>" class="b btn-g999" style="margin:24px auto 12px">See All</a>
+                <?php } ?>
             </div>
         </div>
     </div>
