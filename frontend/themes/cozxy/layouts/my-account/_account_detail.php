a76b5c5097668e4e9c4ed64abecf84b96638c9f9
kamyjapkung <kamyjapkung@app.deveo.com>
2017-07-31 11:09:22 +0700

    xxx

--- frontend/themes/cozxy/layouts/my-account/_account_detail.php
+++ frontend/themes/cozxy/layouts/my-account/_account_detail.php
@@ -17,7 +17,7 @@ use dosamigos\fileupload\FileUpload;
         <?php
         $avatar = (isset($user->avatar)) ? $user->avatar : 'http://ssl.gstatic.com/accounts/ui/avatar_2x.png';
         ?>
-        <?= Html::img($avatar, ['width' => 120, 'class' => 'img-circle', 'id' => 'avatar']) ?><br />
+        <?= Html::img(Yii::$app->homeUrl . "images/user/avatar/" . $avatar, ['width' => 120, 'class' => 'img-circle', 'id' => 'avatar']) ?><br />
         <?=
         FileUpload::widget([
             'model' => $user,
@@ -43,9 +43,9 @@ use dosamigos\fileupload\FileUpload;
         ?>
 
         <!--        --><?//=Html::button('change', ['class'=>'btn btn-primary btn-xs', 'id'=>'profilePictureBtn'])?>
-        <!--        --><?php //ActiveForm::begin();  ?>
+        <!--        --><?php //ActiveForm::begin();    ?>
         <!--        --><?//=Html::fileInput('profile', '', ['class'=>'hide', 'id'=>'profilePicture'])?>
-        <!--        --><?php //ActiveForm::end(); ?>
+        <!--        --><?php //ActiveForm::end();   ?>
     </div>
     <div class="col-md-10">
         <?php
