3bd5ce6464d7233651e373d8c18ac5cda0cc3770
Pew CodesLoves <sodapew17@gmail.com>
2017-08-01 16:33:37 +0700

    xxx

--- frontend/controllers/StoryController.php
+++ frontend/controllers/StoryController.php
@@ -52,7 +52,7 @@ class StoryController extends MasterController {
         $product_image_suppliers = $productPost->attributes;
         $imgShowStory = '';
         if (isset($product_image_suppliers['productId'])) {
-            $product_image = \common\models\costfit\ProductImageSuppliers::find()->where('productSuppId=' . $product_image_suppliers['productId'])
+            $product_image = \common\models\costfit\ProductImageSuppliers::find()->where('productSuppId=' . $params['productSupplierId'])
             ->orderBy('ordering asc')->limit(1)->one();
             if (isset($product_image)) {
                 $imgShowStory = $product_image->image;
