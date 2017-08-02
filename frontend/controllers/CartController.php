50f868ec31d31b6f17b02a6c874aa77627b050b0
Pew CodesLoves <sodapew17@gmail.com>
2017-08-02 15:44:48 +0700

    update

--- frontend/controllers/CartController.php
+++ frontend/controllers/CartController.php
@@ -103,7 +103,7 @@ class CartController extends MasterController {
         if (isset($productId)) {
             $Qty = \common\models\costfit\ProductSuppliers::find()->where('productId=' . $productId)->one();
             $quantityMain = $Qty->result;
-            if ((int) $orderItem->quantity > (int) $quantityMain) {
+            if ((int) $orderItem->quantity >= (int) $quantityMain) {
                 echo $quantityMain . '<br>::';
                 echo $orderItem->quantity;
                 // exit();
