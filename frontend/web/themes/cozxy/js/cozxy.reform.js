fd9e34f0932ae6cbc54c4ab4e1a20d3275866a27
Pew CodesLoves <sodapew17@gmail.com>
2017-08-09 11:43:30 +0700

    fixbug .js in menu

--- frontend/web/themes/cozxy/js/cozxy.reform.js
+++ frontend/web/themes/cozxy/js/cozxy.reform.js
@@ -1532,3 +1532,11 @@ $(function () {
 })
 
 
+$(function () {
+    $('.dropdown-submenu').on('hover', function () {
+        $(this).find('.dropdown-menu').css('display', 'block');
+    })
+    $('.menuWrapper').on('hover', '.dropdown', function () {
+        $(this).find('.dropdown-menu.multi-level').css('display', 'block');
+    })
+});
