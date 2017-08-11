64dd48fa878080ccf9771250c1736918cf59b054
Pew CodesLoves <sodapew17@gmail.com>
2017-08-11 10:06:45 +0700

    update

--- frontend/themes/cozxy/layouts/product/_product_item-new-icon.php
+++ frontend/themes/cozxy/layouts/product/_product_item-new-icon.php
@@ -1,35 +1,33 @@
 <?php
 
-use common\models\costfit\ProductShelf;
+use yii\bootstrap\ActiveForm;
 use common\models\costfit\ProductSuppliers;
+use common\models\costfit\ProductShelf;
 use yii\helpers\Html;
 use yii\helpers\Url;
+
+if (Yii::$app->controller->id == 'product') {
+    $width = "width: 195px";
+    $height = "height: 195px";
+} else {
+    $width = "width: 260px";
+    $height = "height: 260px";
+}
 ?>
-<div class="col-md-4 col-sm-6 col-xs-12">
+<?php $col = isset($colSize) ? $colSize : '4'; ?>
+<div class="col-md-<?= $col ?> col-sm-6 col-xs-12">
     <div class="product-box">
         <div class="product-img text-center">
             <a href="<?= $model['url'] ?>" class="fc-black">
-                <img src="<?= $model['image'] ?>" alt="" class="fullwidth img-responsive" style="width: 260px; height: 260px;">
+                <img alt="262x262" class="media-object fullwidth img-responsive" data-src="holder.js/262x262" src="<?= $model['image'] ?>" data-holder-rendered="true" style="<?= $width ?>; <?= $height ?>;">
             </a>
             <div class="v-hover">
                 <a href="<?= $model['url'] ?>">
-                    <div class="col-xs-6">
+                    <div class="col-xs-4">
                         <?= Html::img(Url::home() . 'imgs/Asset30.png', ['width' => '50', 'height' => '30', 'style' => 'margin-top: -10px;']) ?>
                         <!--<i class="fa fa-eye" aria-hidden="true"></i>-->
                     </div>
                 </a>
-                <?php /*
-                  if ($model['wishList'] == 1) {// เคย wishList ไปแล้ว
-                  ?>
-                  <a>
-                  <div class="col-xs-4"><i class="fa fa-heart" aria-hidden="true"></i></div>
-                  </a>
-                  <?php } else { ?>
-
-                  <a href="javascript:addItemToWishlist(<?= $model['productSuppId'] ?>);" id="addItemToWishlist-<?= $model['productSuppId'] ?>" data-loading-text="<div class='col-xs-6'><i class='fa fa-heartbeat' aria-hidden='true'></i></div>">
-                  <div class="col-xs-6"><i class="fa fa-heart-o" aria-hidden="true"></i></div>
-                  </a>
-                  <?php } */ ?>
                 <?php
                 if (Yii::$app->user->id) {
                     if ($model['wishList'] == 1) { // เคย wishList ไปแล้ว
@@ -51,12 +49,49 @@ use yii\helpers\Url;
                     <a href="<?= Yii::$app->homeUrl . 'site/login' ?>" >
                         <div class="col-xs-4"><i class="fa fa-heart-o" aria-hidden="true"></i></div>
                     </a>
+                    <?php
+                }
+                //echo 'Asset28 :' . Url::home() . 'imgs/Asset28.png';
+                ?>
+                <?php
+                if ($model['maxQnty'] > 0) {
+                    ?>
+                    <a  href="javascript:addItemToCartUnitys('<?= $model['productSuppId'] ?>',1,'<?= $model['maxQnty'] ?>','<?= $model['fastId'] ?>','<?= $model['productId'] ?>','<?= $model['supplierId'] ?>','<?= $model['receiveType'] ?>')" id="addItemsToCartMulti-<?= $model['productSuppId'] ?>" data-loading-text="<div class='col-xs-4 shopping-<?= $model['productSuppId'] ?>'><img class='fa fa-cart-plus fa-spin' src='<?= Url::home() ?>imgs/Asset28.png' width='50' height='30' alt='' style='margin-top: -10px;'></div>"><!--<i class='fa fa-cart-plus fa-spin' aria-hidden='true'></i>-->
+                        <div class="col-xs-4 shopping-<?= $model['productSuppId'] ?>">
+                        <!--<i id="cart-plus-<?//= $model['productSuppId'] ?>" class="fa fa-cart-plus" aria-hidden="true"></i>-->
+                            <span id="cart-plus-<?= $model['productSuppId'] ?>">
+                                <?= Html::img(Url::home() . 'imgs/Asset28.png', ['width' => '50', 'height' => '30', 'style' => 'margin-top: -10px;', 'class' => 'fa fa-cart-plus']) ?>
+                            </span>
+                        </div>
+                    </a>
                 <?php } ?>
             </div>
         </div>
         <div class="product-txt">
-            <p class="size14 fc-g666" ><?= strtoupper($model['brand']) ?></p>
+            <?php
+            if (isset($model['brand'])) {
+                ?>
+                <p class="size14 fc-g666"><?= strtoupper($model['brand']) ?></p>
+                <?php
+            } else {
+                echo '';
+            }
+            ?>
+
             <p class="size18 b" style="height:40px;"><a href="<?= $model['url'] ?>" class="fc-black"><?= strtoupper($model['title']) ?></a></p>
+
+            <?php
+            if ($model['price'] > 0) {
+                ?><p>
+                    <span class="size18"><?= $model['price'] ?> THB</span><br>
+                    <span class="size14 onsale"><?= $model['price_s'] ?> THB</span>
+                </p>
+                <?php
+            } else {
+                echo '';
+            }
+            ?>
+
         </div>
     </div>
 </div>
@@ -64,13 +99,13 @@ use yii\helpers\Url;
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="fa fa-times"></i>
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true" id="closeWishlistModal"><i class="fa fa-times"></i>
                 </button>
-                <h3>Save to Wish List</h3>
+                <h3>Save to shelves</h3>
             </div>
             <div class="modal-body" style="padding: 40px;">
-                <a style="cursor: pointer;" id="showCreateWishList">Create New Wish List Group</a>
-                <a style="cursor: pointer;display: none;" id="hideCreateWishList" >Create New Wish List Group</a>
+                <a style="cursor: pointer;" id="showCreateWishList">Create New shelves</a>
+                <a style="cursor: pointer;display: none;" id="hideCreateWishList" >Create New shelves</a>
                 <div id="newWishList" style="display: none;">
 
                     <h4>Name</h4>
@@ -122,8 +157,8 @@ use yii\helpers\Url;
                 <div col-lg-12 col-md-12 col-sm-12 col-xs-12 pull-left><hr style=""></div>
                 <div class="row">
                     <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4"><img src="<?= Yii::$app->homeUrl . ProductSuppliers::productImageSuppliersSmall($model['productSuppId']) ?>" style="border: #cccccc solid thin;"></div>
-                    <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 text-left">
-                        <h3><?= ProductSuppliers::productSupplierName($model['productSuppId'])->title ?></h3>
+                    <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 text-left pull-right">
+                        <?= ProductSuppliers::productSupplierName($model['productSuppId'])->title ?><br>
                         <?= ProductSuppliers::productSupplierName($model['productSuppId'])->shortDescription ?>
                     </div>
                 </div>
