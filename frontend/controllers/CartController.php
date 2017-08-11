2a25dd201bc0d8c47b9dc64ac1901cb90d9638ab
sakqqboy <saknakhngam@hotmail.com>
2017-08-11 14:47:09 +0700

    update

--- frontend/controllers/CartController.php
+++ frontend/controllers/CartController.php
@@ -246,9 +246,9 @@ class CartController extends MasterController {
             foreach ($showOrder as $item):
                 $productSupp = \common\models\costfit\ProductSuppliers::productSupplierName($item->productSuppId);
                 $text = $text . '<tr class = "item" id = "item' . $item->orderItemId . '">'
-                . '<td><div class = "delete"><input type = "hidden" id = "orderItemId" value = "' . $item->orderItemId . '"></div><a href = "' . Yii::$app->homeUrl . 'products/' . \common\models\ModelMaster::encodeParams(["productId" => $item->productId, "productSupplierId" => $item->productSuppId]) . '">' . $productSupp->title . '</a></td>'
-                . '<td class = "qty"><input type = "text" id = "qty" value = "' . $item->quantity . '" readonly = "true"></td>'
-                . '<td class = "price">' . number_format(\common\models\costfit\ProductSuppliers::productPriceSupplier($item->productSuppId), 2) . '</td><input type = "hidden" id = "productSuppId" value = "' . $item->productSuppId . '"></tr>';
+                        . '<td><div class = "delete"><input type = "hidden" id = "orderItemId" value = "' . $item->orderItemId . '"></div><a href = "' . Yii::$app->homeUrl . 'products/' . \common\models\ModelMaster::encodeParams(["productId" => $item->productId, "productSupplierId" => $item->productSuppId]) . '">' . $productSupp->title . '</a></td>'
+                        . '<td class = "qty"><input type = "text" id = "qty" value = "' . $item->quantity . '" readonly = "true"></td>'
+                        . '<td class = "price">' . number_format(\common\models\costfit\ProductSuppliers::productPriceSupplier($item->productSuppId), 2) . '</td><input type = "hidden" id = "productSuppId" value = "' . $item->productSuppId . '"></tr>';
             endforeach;
             $text = $header . $text . $footer;
         }
@@ -347,7 +347,7 @@ class CartController extends MasterController {
             $ws->createDateTime = new \yii\db\Expression("NOW()");
             if ($ws->save()) {
                 $res["status"] = TRUE;
-                $res['title'] = \common\models\costfit\ProductSuppliers::productSupplierName($_POST['productId'])->title;
+                // $res['title'] = \common\models\costfit\ProductSuppliers::productSupplierName($_POST['productId'])->title;
             } else {
                 $res["status"] = FALSE;
                 $res['errorCode'] = 2;
@@ -554,15 +554,15 @@ class CartController extends MasterController {
             endforeach;
             $id = substr($id, 0, -1);
             $products = \common\models\costfit\ProductSuppliers::find()
-            ->where("productSuppId in ($id) and approve = 'approve'")
-            ->orderBy(new \yii\db\Expression('rand()'))
-            ->limit(4)
-            ->all();
+                    ->where("productSuppId in ($id) and approve = 'approve'")
+                    ->orderBy(new \yii\db\Expression('rand()'))
+                    ->limit(4)
+                    ->all();
         } else {
             $products = \common\models\costfit\ProductSuppliers::find()->where("approve = 'approve'")
-            ->orderBy(new \yii\db\Expression('rand()'))
-            ->limit(4)
-            ->all();
+                    ->orderBy(new \yii\db\Expression('rand()'))
+                    ->limit(4)
+                    ->all();
         }
         $this->subSubTitle = '';
         //echo '<pre>';
