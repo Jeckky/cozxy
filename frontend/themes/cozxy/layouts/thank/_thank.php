5d71b93e6c857e5c8b18f1c0fe39bc1699965c0b
Pew CodesLoves <sodapew17@gmail.com>
2017-08-21 09:33:39 +0700

    update

--- frontend/themes/cozxy/layouts/thank/_thank.php
+++ frontend/themes/cozxy/layouts/thank/_thank.php
@@ -1,42 +1,57 @@
 <div class="container login-box">
     <div class="size32">&nbsp;</div>
     <div class="row">
-        <div class="col-xs-12 bg-yellow1 b" style="padding:18px 18px 10px;">
+        <!--<div class="col-xs-12 bg-yellow1 b" style="padding:18px 18px 10px;">
             <p class="size20 size18-xs">Signup Thank</p>
         </div>
-        <div class="col-xs-12 bg-white size18 b" style="padding: 20px;">
-            <?php
-            //echo $_GET['status'];
-            if (isset($_GET['status'])) {
-                if ($_GET['status'] == 1) {
-                    echo '<span style="color: red">Email is already in the system </span> please again ';
-                } else if ($_GET['status'] == 2) {
-                    echo '<span style="color: red">Email is already in the system </span> please again ';
-                } else if ($_GET['status'] == 3) {
-                    echo ' Please check your email to confirm your registration.';
-                } else {
-                    echo ' Please check your email to confirm your registration.';
-                }
+        <div class="col-xs-12 bg-white size18 b" style="padding: 20px;">-->
+        <?php
+        //echo $_GET['status'];
+        if (isset($_GET['status'])) {
+            echo '<div class="col-xs-12 bg-yellow1 b" style="padding:18px 18px 10px;">
+                            <p class="size20 size18-xs">Signup Thank</p>
+                      </div>';
+            echo '<div class="col-xs-12 bg-white size18 b" style="padding: 20px;">';
+            if ($_GET['status'] == 1) {
+                echo '<span style="color: red">Email is already in the system </span> please again ';
+            } else if ($_GET['status'] == 2) {
+                echo '<span style="color: red">Email is already in the system </span> please again ';
+            } else if ($_GET['status'] == 3) {
+                echo ' Please check your email to confirm your registration.';
             } else {
-                if (isset($_GET['verification'])) {
-                    echo 'Welcome to Cozxy.<br><br>We hope you enjoy our experience with us!<br>';
-                    echo "We are fully committed in becoming the most efficient logistic network, Thai bred E-commerce startup in Thailand and throughout Asia."
-                    . " We are working hard with partners, traditional shops and SMEs to provide you with the best experience possible.";
-                    if (isset($_GET['cz']) && !empty($_GET['cz'])) {
-                        echo '<br><br>Please wait &nbsp;&nbsp;<i class="fa fa-spinner fa-spin" aria-hidden="true"></i> ';
-                        //echo $this->redirect(Yii::$app->homeUrl . 'cart');
-                        $url = "http://" . Yii::$app->request->getServerName() . Yii::$app->homeUrl . "cart";
-                        header("Location:" . $url, true, 301);
-                        exit();
-                    }
-                } else {
-                    //echo ' Thank you for registering on Cozxy!';
-                    echo 'Verify your email to complete your registration<br>';
-                    echo "Thank you for registering. Please check your email to verify your account before you can enjoy your experience at Cozxy.com.";
-                }
+                echo ' Please check your email to confirm your registration.';
             }
-            ?>
-        </div>
+            echo '</div>';
+        } else {
+            if (isset($_GET['verification'])) {
+                echo '<div class="col-xs-12 bg-yellow1 b" style="padding:18px 18px 10px;">
+                            <p class="size20 size18-xs">Signup Thank</p>
+                      </div>';
+                echo '<div class="col-xs-12 bg-white size18 b" style="padding: 20px;">';
+                echo 'Welcome to Cozxy.<br><br>We hope you enjoy our experience with us!<br>';
+                echo "We are fully committed in becoming the most efficient logistic network, Thai bred E-commerce startup in Thailand and throughout Asia."
+                . " We are working hard with partners, traditional shops and SMEs to provide you with the best experience possible.";
+                if (isset($_GET['cz']) && !empty($_GET['cz'])) {
+                    echo '<br><br>Please wait &nbsp;&nbsp;<i class="fa fa-spinner fa-spin" aria-hidden="true"></i> ';
+                    //echo $this->redirect(Yii::$app->homeUrl . 'cart');
+                    $url = "http://" . Yii::$app->request->getServerName() . Yii::$app->homeUrl . "cart";
+                    header("Location:" . $url, true, 301);
+                    exit();
+                }
+                echo '</div>';
+            } else {
+                //echo ' Thank you for registering on Cozxy!';
+                echo '<div class="col-xs-12 bg-yellow1 b" style="padding:18px 18px 10px;">
+                            <p class="size20 size18-xs">' . $modelUser['firstname'] . '&nbsp;' . $modelUser['lastname'] . '</p>
+                      </div>';
+                echo '<div class="col-xs-12 bg-white size18 b" style="padding: 20px;">';
+                echo 'Verify your email to complete your registration<br>';
+                echo "Thank you for registering. Please check your email to verify your account before you can enjoy your experience at COZXY.COM.";
+                echo '</div>';
+            }
+        }
+        ?>
+        <!--</div>-->
     </div>
 </div>
 
