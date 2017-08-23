f90c75681f7822a96685eb8b9a100d4a39b89de3
Pew CodesLoves <sodapew17@gmail.com>
2017-08-23 16:08:47 +0700

    xx

--- frontend/web/themes/cozxy/js/cozxy.js
+++ frontend/web/themes/cozxy/js/cozxy.js
@@ -1,14 +1,25 @@
 /**
  * Created by npr on 5/17/2017 AD.
  */
-$(function() {
-    $('.topbar .dismiss').click(function(){$('.topbar').slideUp();$('.topOpener').slideDown();return false;});
-    $('.topOpener').click(function(){$('.topbar').slideDown();$('.topOpener').slideUp();});
-    $('.gotoTop').click(function(){$('html,body').animate({scrollTop:0});return false;});
+$(function () {
+    $('.topbar .dismiss').click(function () {
+        $('.topbar').slideUp();
+        $('.topOpener').slideDown();
+        return false;
+    });
+    //$('.topOpener').click(function(){$('.topbar').slideDown();$('.topOpener').slideUp();});
+    $('.topOpener').click(function () {
+        $('.topbar').slideDown();
+        $('.topOpener').slideUp();
+    });
+    $('.gotoTop').click(function () {
+        $('html,body').animate({scrollTop: 0});
+        return false;
+    });
     // Scroll Script
-    $(window).scroll(function(){
+    $(window).scroll(function () {
         var $this = $(this), $top = $(".smallTop");
-        if ($this.scrollTop()>384) {
+        if ($this.scrollTop() > 384) {
             $top.fadeIn(384);
         } else {
             $top.fadeOut(384);
