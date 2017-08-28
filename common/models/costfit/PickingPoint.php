a0fa131353f6c705c8349ad8069d1695e55fb9ea
Pew CodesLoves <sodapew17@gmail.com>
2017-08-28 14:27:48 +0700

    xxx

--- common/models/costfit/PickingPoint.php
+++ common/models/costfit/PickingPoint.php
@@ -156,12 +156,12 @@ class PickingPoint extends \common\models\costfit\master\PickingPointMaster {
     public static function availableProvince() {
         preg_match("/dbname=([^;]*)/", Yii::$app->get("db")->dsn, $dbName);
 
-        $available = ArrayHelper::map(States::find()->select(['states.stateId', 'states.localName'])
+        $available = States::find()->select(['states.stateId', 'states.localName'])
         ->leftJoin($dbName[1] . '.picking_point cpp', 'states.stateId=cpp.provinceId')
         ->where('cpp.status = 1')
         ->groupBy('cpp.provinceId')
         ->orderBy('states.localName')
-        ->asArray()->all(), 'stateId', 'localName');
+        ->asArray()->all();
 
         return $available;
     }
