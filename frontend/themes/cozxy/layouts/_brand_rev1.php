934a0edf4af05951001787e51c82af5d01a8853e
Kurtumm <kurtumm@gmail.com>
2017-08-29 20:28:22 +0700

    fixed brand image

--- frontend/themes/cozxy/layouts/_brand_rev1.php
+++ frontend/themes/cozxy/layouts/_brand_rev1.php
@@ -1,3 +1,3 @@
 <a class="" href="<?php echo Yii::$app->homeUrl.'search/brand/'.$model->encodeParams(['brandId'=>$model->brandId]); ?>">
-    <img src="<?php echo Yii::$app->homeUrl.$model->image; ?>" alt="" class="img-responsive" style="width:112px; height: 64px;"/>
+    <img src="<?php echo Yii::$app->homeUrl.substr($model->image, 1); ?>" alt="" class="img-responsive" style="width:112px; height: 64px;"/>
 </a>
