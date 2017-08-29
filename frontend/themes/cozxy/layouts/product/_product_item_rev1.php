7617f02a4798c9936ced7b8b00960a39f0b7618e
sakqqboy <saknakhngam@hotmail.com>
2017-08-29 11:38:22 +0700

    red background

--- frontend/themes/cozxy/layouts/product/_product_item_rev1.php
+++ frontend/themes/cozxy/layouts/product/_product_item_rev1.php
@@ -78,11 +78,21 @@ if (Yii::$app->controller->id == 'product') {
             </p>
             <?php
             if ($model->price > 0) {
-                ?><p class="price">
-                    <span class="size16"><?= number_format($model->price, 2) . 'THB' ?> </span><br>
-                    <span class="size10 onsale"><?= isset($model->product) ? number_format($model->product->price, 1) . 'THB' : '' ?> </span>
-                </p>
-                <?php
+                if (isset($hotDeal)) {
+                    ?>
+
+                    <p class="price" style="height: 50px;">
+                        <span class="size16 col-lg-9 col-md-9 col-sm-10 col-xs-12" style="background-color: red; height: 25px;padding: 5px;color: white;"><?= number_format($model->price, 2) . 'THB' ?> </span><br>
+                        <span class="size12 onsale"><?= isset($model->product) ? number_format($model->product->price, 1) . 'THB' : '' ?> </span>
+                    </p>
+                <?php } else {
+                    ?>
+                    <p class="price">
+                        <span class="size16"><?= number_format($model->price, 2) . 'THB' ?> </span><br>
+                        <span class="size10 onsale"><?= isset($model->product) ? number_format($model->product->price, 1) . 'THB' : '' ?> </span>
+                    </p>
+                    <?php
+                }
             } else {
                 echo '';
             }
