b424e1901e230faf21e7399bc13528c94615f550
Pew CodesLoves <sodapew17@gmail.com>
2017-09-06 16:12:22 +0700

    Resize Mobile Support

--- frontend/themes/cozxy/layouts/search/_search_filter.php
+++ frontend/themes/cozxy/layouts/search/_search_filter.php
@@ -5,7 +5,7 @@
     }
     .dropdown:hover > .dropdown-menu {
         display: block;
-        margin-top: 0px; 
+        margin-top: 0px;
     }
 </style>
 <div class="product-menu">
@@ -13,7 +13,7 @@
         <div class="row">
             <div class="dropdown items">
                 <div class="dropdown-toggle size18 size16-sm size14-xs"  data-toggle="dropdown">PRICE &nbsp; <i class="fa fa-angle-down" aria-hidden="true"></i></div>
-                <div class="dropdown-menu menu-filter-price">
+                <div class="dropdown-menu menu-filter-price" style="min-width:373px; max-width: 460px;">
                     <div class="row">
                         <form method="post" action="">
                             <div class="col-xs-10 col-xs-offset-1" style="margin-top: 24px;">
@@ -37,7 +37,7 @@
             </div>
             <div class="dropdown items">
                 <div class="dropdown-toggle size18 size16-sm size14-xs"  data-toggle="dropdown">BRAND &nbsp; <i class="fa fa-angle-down" aria-hidden="true"></i></div>
-                <div class="dropdown-menu menu-filter-brand">
+                <div class="dropdown-menu menu-filter-brand" style="min-width:256px; max-width: 460px;">
                     <div class="row input-group">
                         <form method="post" action="">
                             <?php
