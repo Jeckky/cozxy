8729f60acbe73b1726cff4121a3f614e8d66d462
Pew CodesLoves <sodapew17@gmail.com>
2017-09-06 15:23:45 +0700

    Resize Mobile Support

--- frontend/themes/cozxy/layouts/content/_content_items.php
+++ frontend/themes/cozxy/layouts/content/_content_items.php
@@ -1,7 +1,7 @@
 <div class="col-md-4 col-sm-6 col-xs-6">
     <div class="product-other">
         <a href="<?php echo $model['url']; ?>">
-            <img src="<?php echo $model['image']; ?>" alt="Other Product" class="fullwidth">
+            <img src="<?php echo $model['image']; ?>" alt="Other Product" class="fullwidth  img-responsive" style="max-height: 230px;">
         </a>
     </div>
 </div>
