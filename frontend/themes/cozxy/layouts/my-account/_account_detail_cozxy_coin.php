aa101bc2fe1c2ec45bbbc29e9c3334e349eac585
Pew CodesLoves <sodapew17@gmail.com>
2017-09-07 11:42:09 +0700

    update

--- frontend/themes/cozxy/layouts/my-account/_account_detail_cozxy_coin.php
+++ frontend/themes/cozxy/layouts/my-account/_account_detail_cozxy_coin.php
@@ -1,10 +1,10 @@
-<div class="col-sm-3 col-md-3 col-xs-4">Your Balance:</div>
-<div class="col-sm-9 col-md-9 col-xs-8"><?= $model['currentPoint'] ?></div>
-<div class="col-sm-3 col-md-3 col-xs-4">Your Cozxy System Balance:</div>
-<div class="col-sm-9 col-md-9 col-xs-8"><?= $model['currentCozxySystemPoint'] ?></div>
-<div class="col-sm-3 col-md-3 col-xs-4">Last update:</div>
-<div class="col-sm-9 col-md-9 col-xs-8"><?= date('Y-m-d H:i:s') ?></div>
-<div class="col-sm-3 col-md-3 col-xs-4">Method:</div>
-<div class="col-sm-9 col-md-9 col-xs-8"><?= $model['method'] ?></div>
-<div class="size12 col-md-12">&nbsp;</div>
-<div class="size32 hr-margin  col-md-12">&nbsp;</div>
+<div class="col-sm-12 col-md-3">Your Balance:</div>
+<div class="col-sm-12 col-md-9"><?= $model['currentPoint'] ?></div>
+<div class="col-sm-12 col-md-3">Your Cozxy System Balance:</div>
+<div class="col-sm-12 col-md-9"><?= $model['currentCozxySystemPoint'] ?></div>
+<div class="col-sm-12 col-md-3">Last update:</div>
+<div class="col-sm-12 col-md-9"><?= date('Y-m-d H:i:s') ?></div>
+<div class="col-sm-12 col-md-3">Method:</div>
+<div class="col-sm-12 col-md-9"><?= $model['method'] ?></div>
+<div class="size12">&nbsp;</div>
+<div class="size32 hr-margin">&nbsp;</div>
