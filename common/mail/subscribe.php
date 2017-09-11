9c7988c21ae38bd2fff3d0e17063f4e0eff61577
Pew CodesLoves <sodapew17@gmail.com>
2017-09-11 14:33:24 +0700

    update

--- common/mail/subscribe.php
+++ common/mail/subscribe.php
@@ -6,64 +6,63 @@
     <body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" style="text-align: center;">
         <!-- Save for Web Slices (subscribe.png) -->
         <table id="Table_01" width="540" height="768" border="0" cellpadding="0" cellspacing="0">
-            <tr>
-                <td colspan="5"><img src="<?php echo $urlImg; ?>subscribe_01.png" width="539" height="114" alt=""></td>
+            <tr style="height: 114px;">
+                <td colspan="5"><img src="<?php echo $urlImg; ?>subscribe_01.png" width="539" height="114" alt="" border="0"></td>
                 <td><img src="<?php echo $urlImg; ?>spacer.gif" width="1" height="114" alt=""></td>
             </tr>
-            <tr>
+            <tr style="height: 413px;">
                 <td colspan="5"><img src="<?php echo $urlImg; ?>subscribe_02.png" width="539" height="413" alt=""></td>
                 <td><img src="<?php echo $urlImg; ?>spacer.gif" width="1" height="413" alt=""></td>
             </tr>
             <tr>
-                <td><img src="<?php echo $urlImg; ?>subscribe_03.png" width="214" height="1" alt=""></td>
-                <td colspan="3" rowspan="2"><img src="<?php echo $urlImg; ?>subscribe_04.png" width="113" height="38" alt=""></td>
-                <td><img src="<?php echo $urlImg; ?>subscribe_05.png" width="212" height="1" alt=""></td>
-                <td><img src="<?php echo $urlImg; ?>spacer.gif" width="1" height="1" alt=""></td>
+                <td style="height: 1px;"><img src="<?php echo $urlImg; ?>subscribe_03.png" width="214" height="1" alt=""></td>
+                <td colspan="3" rowspan="2" style="height: 38px;"><img src="<?php echo $urlImg; ?>subscribe_04.png" width="113" height="38" alt=""></td>
+                <td style="height: 1px;"><img src="<?php echo $urlImg; ?>subscribe_05.png" width="212" height="1" alt=""></td>
+                <td style="height: 1px;"><img src="<?php echo $urlImg; ?>spacer.gif" width="1" height="1" alt=""></td>
             </tr>
             <tr>
-                <td><img src="<?php echo $urlImg; ?>subscribe_06.png" width="214" height="37" alt=""></td>
-                <td><img src="<?php echo $urlImg; ?>subscribe_07.png" width="212" height="37" alt=""></td>
-                <td><img src="<?php echo $urlImg; ?>spacer.gif" width="1" height="37" alt=""></td>
+                <td style="height: 37px;"><img src="<?php echo $urlImg; ?>subscribe_06.png" width="214" height="37" alt=""></td>
+                <td style="height: 37px;"><img src="<?php echo $urlImg; ?>subscribe_07.png" width="212" height="37" alt=""></td>
+                <td style="height: 37px;"><img src="<?php echo $urlImg; ?>spacer.gif" width="1" height="37" alt=""></td>
             </tr>
             <tr>
-                <td><img src="<?php echo $urlImg; ?>subscribe_08.png" width="214" height="28" alt=""></td>
-                <td rowspan="2"><a href="https://www.facebook.com/cozxydotcom/"><img src="<?php echo $urlImg; ?>subscribe_09.png" width="44" height="29" alt=""></a></td>
-                <td rowspan="2"><a href="https://www.instagram.com/cozxy_thailand"><img src="<?php echo $urlImg; ?>subscribe_10.png" width="26" height="29" alt=""></a></td>
-                <td rowspan="2"><img src="<?php echo $urlImg; ?>subscribe_11.png" width="43" height="29" alt=""></td>
-                <td><img src="<?php echo $urlImg; ?>subscribe_12.png" width="212" height="28" alt=""></td>
-                <td><img src="<?php echo $urlImg; ?>spacer.gif" width="1" height="28" alt=""></td>
+                <td style="height: 28px;"><img src="<?php echo $urlImg; ?>subscribe_08.png" width="214" height="28" alt=""></td>
+                <td rowspan="2" style="height: 29px;"><a href="https://www.facebook.com/cozxydotcom/"><img src="<?php echo $urlImg; ?>subscribe_09.png" width="44" height="29" alt=""></a></td>
+                <td rowspan="2" style="height: 29px;"><a href="https://www.instagram.com/cozxy_thailand"><img src="<?php echo $urlImg; ?>subscribe_10.png" width="26" height="29" alt=""></a></td>
+                <td rowspan="2" style="height: 29px;"><img src="<?php echo $urlImg; ?>subscribe_11.png" width="43" height="29" alt=""></td>
+                <td style="height: 28px;"><img src="<?php echo $urlImg; ?>subscribe_12.png" width="212" height="28" alt=""></td>
+                <td style="height: 28px;"><img src="<?php echo $urlImg; ?>spacer.gif" width="1" height="28" alt=""></td>
             </tr>
             <tr>
-                <td rowspan="2"><img src="<?php echo $urlImg; ?>subscribe_13.png" width="214" height="17" alt=""></td>
-                <td rowspan="2"><img src="<?php echo $urlImg; ?>subscribe_14.png" width="212" height="17" alt=""></td>
-                <td><img src="<?php echo $urlImg; ?>spacer.gif" width="1" height="1" alt=""></td>
+                <td rowspan="2" style="height: 17px;"><img src="<?php echo $urlImg; ?>subscribe_13.png" width="214" height="17" alt=""></td>
+                <td rowspan="2" style="height: 17px;"><img src="<?php echo $urlImg; ?>subscribe_14.png" width="212" height="17" alt=""></td>
+                <td style="height: 1px;"><img src="<?php echo $urlImg; ?>spacer.gif" width="1" height="1" alt=""></td>
             </tr>
             <tr>
-                <td colspan="3"><img src="<?php echo $urlImg; ?>subscribe_15.png" width="113" height="16" alt=""></td>
-                <td><img src="<?php echo $urlImg; ?>spacer.gif" width="1" height="16" alt=""></td>
+                <td colspan="3" style="height: 16px;"><img src="<?php echo $urlImg; ?>subscribe_15.png" width="113" height="16" alt=""></td>
+                <td style="height: 16px;"><img src="<?php echo $urlImg; ?>spacer.gif" width="1" height="16" alt=""></td>
             </tr>
             <tr>
-                <td colspan="5"><img src="<?php echo $urlImg; ?>subscribe_16.png" width="539" height="14" alt=""></td>
-                <td><img src="<?php echo $urlImg; ?>spacer.gif" width="1" height="14" alt=""></td>
+                <td colspan="5" style="height: 14px;"><img src="<?php echo $urlImg; ?>subscribe_16.png" width="539" height="14" alt=""></td>
+                <td style="height: 14px;"><img src="<?php echo $urlImg; ?>spacer.gif" width="1" height="14" alt=""></td>
             </tr>
             <tr>
-                <td colspan="5"><img src="<?php echo $urlImg; ?>subscribe_17.png" width="539" height="55" alt=""></td>
-                <td><img src="<?php echo $urlImg; ?>spacer.gif" width="1" height="55" alt=""></td>
+                <td colspan="5" style="height: 55px;"><img src="<?php echo $urlImg; ?>subscribe_17.png" width="539" height="55" alt=""></td>
+                <td style="height: 55px;"><img src="<?php echo $urlImg; ?>spacer.gif" width="1" height="55" alt=""></td>
             </tr>
             <tr>
-                <td colspan="5"><img src="<?php echo $urlImg; ?>subscribe_18.png" width="539" height="15" alt=""></td>
-                <td><img src="<?php echo $urlImg; ?>spacer.gif" width="1" height="15" alt=""></td>
+                <td colspan="5" style="height: 15px;"><a href="http://www.cozxy.com/site/terms-and-conditions"><img src="<?php echo $urlImg; ?>subscribe_18.png" width="539" height="15" alt=""></a></td>
+                <td style="height: 15px;"><img src="<?php echo $urlImg; ?>spacer.gif" width="1" height="15" alt=""></td>
             </tr>
             <tr>
-                <td colspan="5"><img src="<?php echo $urlImg; ?>subscribe_19.png" width="539" height="12" alt=""></td>
-                <td><img src="<?php echo $urlImg; ?>spacer.gif" width="1" height="12" alt=""></td>
+                <td colspan="5" style="height: 12px;"><img src="<?php echo $urlImg; ?>subscribe_19.png" width="539" height="12" alt=""></td>
+                <td style="height: 12px;"><img src="<?php echo $urlImg; ?>spacer.gif" width="1" height="12" alt=""></td>
             </tr>
             <tr>
-                <td colspan="5"><img src="<?php echo $urlImg; ?>subscribe_20.png" width="539" height="62" alt=""></td>
-                <td><img src="<?php echo $urlImg; ?>spacer.gif" width="1" height="62" alt=""></td>
+                <td colspan="5" style="height: 62px;"><img src="<?php echo $urlImg; ?>subscribe_20.png" width="539" height="62" alt=""></td>
+                <td style="height: 62px;"><img src="<?php echo $urlImg; ?>spacer.gif" width="1" height="62" alt=""></td>
             </tr>
         </table>
-
         <!-- End Save for Web Slices -->
     </body>
 </html>
