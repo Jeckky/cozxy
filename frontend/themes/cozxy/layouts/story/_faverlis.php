ddf88811cb6bfc17b521fd72c7d312d685c57a86
Pew CodesLoves <sodapew17@gmail.com>
2017-09-15 10:00:17 +0700

    update

--- frontend/themes/cozxy/layouts/story/_faverlis.php
+++ frontend/themes/cozxy/layouts/story/_faverlis.php
@@ -9,10 +9,10 @@ use common\models\costfit\FavoriteStory;
         ?>
         <div  style="margin:10px 20px;">
             <a href="javascript:addToFavoriteStory(<?= $productPost->productPostId ?>);" class="btn btn-primary btn-radio" id="favorite" style="display:<?= $isAdd ? 'none;' : '' ?>">
-                <?= $productPost->userId != Yii::$app->user->identity->userId ? 'FAV THIS STORY' : 'Your fav this story' ?>
+                <?= $productPost->userId != Yii::$app->user->identity->userId ? 'Add to Favorite Stories' : 'Your Favorite Stories' ?>
             </a>
             <a href="javascript:unFavoriteStory(<?= $productPost->productPostId ?>);" class="btn btn-warning btn-radio" id="unfavorite" style="display: <?= $isAdd ? '' : 'none' ?>">
-                <?= $productPost->userId != Yii::$app->user->identity->userId ? 'UNFAV THIS STORY' : 'Your fav this story' ?>
+                <?= $productPost->userId != Yii::$app->user->identity->userId ? 'UNFAV THIS STORY' : 'Your Favorite Stories' ?>
             </a>
             <div style="margin-top:10px; display: none;" id="showAddSuccess">Add to favorite successful.</div>
             <div style="margin-top:10px; display: none;" id="showDelSuccess">Delete from favorite successful.</div>
@@ -20,9 +20,9 @@ use common\models\costfit\FavoriteStory;
     <?php } else { ?>
         <div style="margin:10px 20px;">
             <!--            <button type="button" class="btn btn-primary btn-radio" style="margin:10px 20px;">
-                            FAV THIS STORY
+                           Add to Favorite Stories
                         </button>-->
-            <a class="btn btn-primary btn-radio" id="bookmarkme" href="<?= Yii::$app->homeUrl ?>site/login" rel="sidebar">FAV THIS STORY
+            <a class="btn btn-primary btn-radio" id="bookmarkme" href="<?= Yii::$app->homeUrl ?>site/login" rel="sidebar">Add to Favorite Stories
             </a>
         </div>
     <?php } ?>
