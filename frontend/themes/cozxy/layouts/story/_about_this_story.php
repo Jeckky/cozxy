8f184f0f9b71528d4c3d86ce750f88b1ada0e637
Pew CodesLoves <sodapew17@gmail.com>
2017-09-20 10:21:05 +0700

    update

--- frontend/themes/cozxy/layouts/story/_about_this_story.php
+++ frontend/themes/cozxy/layouts/story/_about_this_story.php
@@ -11,7 +11,7 @@
         $txtStory = 'ABOUT';
     }
     ?>
-    <h3 class="page-header" style="margin:10px 20px;"><?= $txtStory ?> THIS STORY</h3>
+    <h3 class="page-header" style="margin:10px 20px;">ABOUT THIS STORY</h3>
     <div class="panel-body">
         <div class="row">
             <div class="col-md-9 col-sm-9 col-xs-9">
