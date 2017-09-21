8f833e1f5c82e79d919540c8ac147f507c4044b2
Pew CodesLoves <sodapew17@gmail.com>
2017-09-21 15:39:22 +0700

    Create helpers CozxyCalculatesCart

---
+++ common/helpers/CozxyCalculatesCart.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace common\helpers;
+
+/**
+ * Description of CozxyCalculatesCart
+ * Create Date 21/09/2017
+ * @author it
+ */
+class CozxyCalculatesCart {
+    /*
+     * ภาษีมูลค่าเพิ่ม
+     */
+
+    public static function FormulaVAT() {
+
+    }
+
+    /*
+     * ราคาสินค้าไม่รวมภาษี
+     */
+
+    public static function FormulaTotalExVat() {
+
+    }
+
+    /*
+     * สวนลดพิเศษ : Coupon
+     */
+
+    public static function FormulaExtraSaving() {
+
+    }
+
+    /*
+     * ราคาสินค้ารวมภาษีมูลค่าเพิ่ม
+     */
+
+    public static function FormulaSubTotal() {
+
+    }
+
+}
