e6d685cb002f8cc68e21a68892415f59ad3c6003
Pew CodesLoves <sodapew17@gmail.com>
2017-09-28 15:12:30 +0700

    Change Form Billing

--- frontend/themes/cozxy/layouts/my-account/_form_billing.php
+++ frontend/themes/cozxy/layouts/my-account/_form_billing.php
@@ -9,12 +9,26 @@ use kartik\depdrop\DepDrop;
 $this->title = 'New Billing Address';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+<style type="text/css">
+    hr{
+        margin-top: 0px;
+    }
+    .form-billing-my-account{
+        padding: 30px;
+    }
+    .form-billing-my-account-top{
+        margin-top: -35px;
+    }
+    .radioNewCozxy .radio-inline{
+        margin-left: 50px;
+    }
 
+</style>
 <div class="container login-box">
     <div class="size32">&nbsp;</div>
     <div class="row">
         <div class="col-xs-12 bg-yellow1 b" style="padding:18px 18px 10px;">
-            <p class="size20 size18-xs">MY ACCOUNT :: New Billing Address</p>
+            <p class="size20 size18-xs">ADD NEW BILLING ADDRESS</p>
         </div>
         <div class="col-xs-12 bg-white">
             <div class="size12 size10-xs">&nbsp;</div>
@@ -24,55 +38,73 @@ $this->params['breadcrumbs'][] = $this->title;
                 'options' => ['class' => 'space-bottom'],
             ]);
             ?>
-            <div class="form-group">
-                <label for="">Billing type *</label>
-                <div class="select-style">
-                    <select name="co-organization" id="co-country" class="valid col-md-12" onchange="organization(this)">
-                        <option value="personal">Individual </option>
-                        <!--<option value="company">Legal Entity (Company)</option>-->
-                    </select>
+            <div class="form-billing-my-account">
+                <div class="form-group form-billing-my-account-top">
+                    <label for=""><?php echo strtoupper('Billing type *') ?></label>
+                    <div class="select-style">
+                        <select name="co-organization" id="co-country" class="valid col-md-12" onchange="organization(this)">
+                            <option value="personal">Individual </option>
+                            <!--<option value="company">Legal Entity (Company)</option>-->
+                        </select>
+                    </div>
+                    <!--
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="">Company (option)</label>
+                            <?//php echo $form->field($model, 'company')->textInput([ 'disabled' => 'true', 'class' => 'fullwidth', 'placeholder' => 'COMPANY'])->label(FALSE); ?>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="">Tax </label>
+                            <?//php echo $form->field($model, 'tax')->textInput([ 'disabled' => 'true', 'class' => 'fullwidth', 'placeholder' => 'TAX'])->label(FALSE); ?>
+                            </div>
+                        </div>
+                    </div>-->
+                    <br><br>
                 </div>
-                <!--
+
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group">
-                            <label for="">Company (option)</label>
-                        <?//php echo $form->field($model, 'company')->textInput([ 'disabled' => 'true', 'class' => 'fullwidth', 'placeholder' => 'COMPANY'])->label(FALSE); ?>
+                            <label for=""><?php echo strtoupper('First Name') ?></label>
+                            <?= $form->field($model, 'firstname')->textInput(['class' => 'fullwidth', 'placeholder' => 'FIRST NAME'])->label(false); ?>
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group">
-                            <label for="">Tax </label>
-                        <?//php echo $form->field($model, 'tax')->textInput([ 'disabled' => 'true', 'class' => 'fullwidth', 'placeholder' => 'TAX'])->label(FALSE); ?>
+                            <label for=""><?php echo strtoupper('Last Name') ?></label>
+                            <?= $form->field($model, 'lastname')->textInput(['class' => 'fullwidth', 'placeholder' => 'LAST NAME'])->label(false); ?>
                         </div>
                     </div>
-                </div>-->
-                <br><br>
-            </div>
-
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="">First Name</label>
-                        <?= $form->field($model, 'firstname')->textInput(['class' => 'fullwidth', 'placeholder' => 'FIRSTNAME'])->label(false); ?>
-                    </div>
                 </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label for="">Last Name</label>
-                        <?= $form->field($model, 'lastname')->textInput(['class' => 'fullwidth', 'placeholder' => 'LASTNAME'])->label(false); ?>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for=""><?php echo strtoupper('Mobile Phone Number*') ?></label>
+                            <?php echo $form->field($model, 'tel')->textInput(['class' => 'fullwidth', 'placeholder' => 'Mobile Phone Number'])->label(false); ?>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for=""><?php echo strtoupper('E-mail Address') ?></label>
+                            <?php echo $form->field($model, 'email')->textInput(['class' => 'fullwidth', 'placeholder' => 'E-mail Address'])->label(false); ?>
+                        </div>
                     </div>
                 </div>
             </div>
 
             <div class="form-group">
-                <label for="">Address</label>
-                <?= $form->field($model, 'address')->textarea(['class' => 'fullwidth', 'placeholder' => 'ADDRESS'])->label(false); ?>
+                <?php echo strtoupper('Address') ?>
+                <hr>
             </div>
-            <div class="row">
-                <div class="col-md-12">
+
+            <div class="row form-billing-my-account">
+                <div class="col-md-6 form-billing-my-account-top">
                     <div class="form-group">
-                        <label for="">Countries</label>
+                        <label for=""><?php echo strtoupper('Countries') ?></label>
                         <?php
                         echo $form->field($model, 'countryId')->widget(kartik\select2\Select2::classname(), [
                             //'options' => ['id' => 'address-countryid'],
@@ -86,9 +118,9 @@ $this->params['breadcrumbs'][] = $this->title;
                         ?>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-6 form-billing-my-account-top">
                     <div class="form-group">
-                        <label for="">Province</label>
+                        <label for=""><?php echo strtoupper('Province') ?></label>
                         <?php
                         echo Html::hiddenInput('input-type-1', $model->provinceId, ['id' => 'input-type-1']);
                         echo Html::hiddenInput('input-type-2', $model->provinceId, ['id' => 'input-type-2']);
@@ -112,7 +144,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 </div>
                 <div class="col-md-4">
                     <div class="form-group">
-                        <label for="">City</label>
+                        <label for=""><?php echo strtoupper('City') ?></label>
                         <?php
                         echo Html::hiddenInput('input-type-11', $model->amphurId, ['id' => 'input-type-11']);
                         echo Html::hiddenInput('input-type-22', $model->amphurId, ['id' => 'input-type-22']);
@@ -135,7 +167,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 </div>
                 <div class="col-md-4">
                     <div class="form-group">
-                        <label for="">District</label>
+                        <label for=""><?php echo strtoupper('District') ?></label>
                         <?php
                         echo Html::hiddenInput('input-type-13', $model->districtId, ['id' => 'input-type-13']);
                         echo Html::hiddenInput('input-type-33', $model->districtId, ['id' => 'input-type-33']);
@@ -158,7 +190,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 </div>
                 <div class="col-md-4">
                     <div class="form-group">
-                        <label for="">Zipcode</label>
+                        <label for=""><?php echo strtoupper('Zipcode') ?></label>
                         <?php
                         echo Html::hiddenInput('input-type-14', $model->zipcode, ['id' => 'input-type-14']);
                         echo Html::hiddenInput('input-type-42', $model->zipcode, ['id' => 'input-type-42']);
@@ -180,35 +212,28 @@ $this->params['breadcrumbs'][] = $this->title;
                         ?>
                     </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <div class="row">
-
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <label for="">Email</label>
-                            <?php echo $form->field($model, 'email')->textInput(['class' => 'fullwidth', 'placeholder' => 'Email'])->label(false); ?>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <label for="">Mobile Number</label>
-                            <?php echo $form->field($model, 'tel')->textInput(['class' => 'fullwidth', 'placeholder' => 'Mobile Number'])->label(false); ?>
-                        </div>
-                    </div>
+                <div class="form-group col-md-12">
+                    <label for=""><?php echo strtoupper('Address') ?></label>
+                    <?= $form->field($model, 'address')->textarea(['class' => 'fullwidth', 'placeholder' => 'ADDRESS'])->label(false); ?>
                 </div>
-                <div class="form-group">
-                    <label for="">Default address</label>
-                    <?php echo $form->field($model, 'isDefault')->radioList([1 => 'Yes', 0 => 'No'], ['itemOptions' => ['class' => 'radio']])->label(false); ?>
-                </div>
-
             </div>
+
+            <div class="form-group" style="margin-top: -30px;">
+                <?php echo strtoupper('Default address') ?>
+                <hr>
+            </div>
+            <div class="form-billing-my-account">
+                <div class="form-group form-billing-my-account-top">
+                    <?php echo $form->field($model, 'isDefault')->inline(true)->radioList([1 => 'YES', 0 => 'NO'], [ 'class' => 'radioNewCozxy', ['itemOptions' => ['class' => 'radioNewCozxy']]])->label(false); ?>
+                </div>
+            </div>
+
             <div class="row">
-                <div class="col-xs-12 text-right">
-                    <a href="<?= Url::to(['/my-account', 'act' => 'account-detail']) ?>" class="b btn-black" style="padding:12px 32px; margin:24px auto 12px">BACK</a>
+                <div class="col-xs-12 text-center">
+                    <a href="<?= Url::to(['/my-account', 'act' => 'account-detail']) ?>" class="b btn-black">CANCEL</a>
                     &nbsp;
                     <!--<a href="<?//= Url::to(['/checkout/summary']) ?>" class="b btn-yellow" style="padding:12px 32px; margin:24px auto 12px">SAVE</a>-->
-                    <input type="submit" value="SAVE"  class="b btn-yellow" style="padding:12px 32px; margin:24px auto 12px">
+                    <input type="submit" value="SAVE"  class="b btn-yellow">
                 </div>
             </div>
             <?php ActiveForm::end(); ?>
