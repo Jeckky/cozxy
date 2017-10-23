2fae84fc830617af5066aa1a585122c3e375d92a
Pew CodesLoves <sodapew17@gmail.com>
2017-10-23 11:02:28 +0700

    section hot deals in top brands or cat

--- frontend/themes/cozxy/layouts/_brand_rev1.php
+++ frontend/themes/cozxy/layouts/_brand_rev1.php
@@ -1,3 +1,3 @@
-<a class="" href="<?php echo Yii::$app->homeUrl.'search/brand/'.$model->encodeParams(['brandId'=>$model->brandId]); ?>">
-    <img src="<?php echo Yii::$app->homeUrl.substr($model->image, 1); ?>" alt="" class="img-responsive" style="width:112px; height: 64px;"/>
+<a class="" href="<?php echo Yii::$app->homeUrl . 'search/brand/' . $model->encodeParams(['brandId' => $model->brandId]); ?>">
+    <img src="<?php echo Yii::$app->homeUrl . substr($model->image, 1); ?>" alt="" title="<?php echo $model->title; ?>" class="img-responsive" style="width:112px; height: 64px;"/>
 </a>
