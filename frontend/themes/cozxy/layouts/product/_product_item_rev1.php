8c1cdc38f50fd8bcaa9eb518b1b8ddb9eac5a2f2
Pew CodesLoves <sodapew17@gmail.com>
2017-10-24 15:35:55 +0700

    update

--- frontend/themes/cozxy/layouts/product/_product_item_rev1.php
+++ frontend/themes/cozxy/layouts/product/_product_item_rev1.php
@@ -105,14 +105,12 @@ if (Yii::$app->controller->id == 'product') {
                     <p class="price" style="height: 50px;">
                         <span class="size18 col-lg-9 col-md-9 col-sm-10 col-xs-12" style="background-color: red; height: 25px;padding: 5px;color: white;"><?= number_format($model->price) . ' THB' ?> </span><br><br>
                         <span class="size16 onsale"><?= isset($model->product) ? number_format($model->product->price) . ' THB' : '' ?> </span>
-
                     </p>
                 <?php } else {
                     ?>
                     <p class="price" style="height: 50px;">
                         <span class="size18 col-lg-9 col-md-9 col-sm-10 col-xs-12" style="height: 25px;padding: 5px;"><?= number_format($model->price) . ' THB' ?> </span><br><br>
                         <span class="size16 onsale"><?= isset($model->product) ? number_format($model->product->price) . ' THB' : '' ?> </span>
-
                         <?php
                     }
                 } else {
