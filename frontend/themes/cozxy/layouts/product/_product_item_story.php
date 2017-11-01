7664b75ae740bfa49cff0961658e7413a21f292e
Pew CodesLoves <sodapew17@gmail.com>
2017-11-01 16:15:56 +0700

    no images available in see all STORIES

--- frontend/themes/cozxy/layouts/product/_product_item_story.php
+++ frontend/themes/cozxy/layouts/product/_product_item_story.php
@@ -6,7 +6,7 @@ use common\helpers\Base64Decode;
     <div class="product-box" style="padding-right: 0px; padding-left: 0px;">
         <div class="product-img text-center">
             <a href="<?= Yii::$app->homeUrl . 'story/' . $model->encodeParams(['productPostId' => $model->productPostId]) ?>">
-                <img class="media-object fullwidth img-responsive" src="<?= isset($model->product) ? \Yii::$app->homeUrl . $model->product->productImageThumbnail() : Base64Decode::DataImageSvg('Svg260x260') ?>" >
+                <img class="media-object fullwidth img-responsive" src="<?= isset($model->product) ? \Yii::$app->homeUrl . $model->product->productImageThumbnail() : \Yii::$app->homeUrl . 'images/user/avatar/150952763159f9904f9095d6.52583311.jpg' ?>" >
             </a>
         </div>
         <div class="product-txt">
