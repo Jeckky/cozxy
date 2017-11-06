551517ee0af150ebf27521cd847b0d6c48a7bd68
Taninut <sodapew17@gmail.com>
2017-11-06 11:59:59 +0700

    update

--- frontend/themes/cozxy/layouts/story/_product_item_story_avatar.php
+++ frontend/themes/cozxy/layouts/story/_product_item_story_avatar.php
@@ -7,7 +7,7 @@
     <!--<div class="col-sm-3" style=" padding: 2px; ">-->
     <div class="  hovercard product-img">
         <img id="viewPost" data-src="holder.js/64x64" src="<?= isset($model->product) ? \Yii::$app->homeUrl . $model->product->productImageThumbnail() : Base64Decode::DataImageSvg('Svg260x260') ?>" class="fullwidth"  style="border-bottom: 1px #d8d8d8 solid;">
-        <div class="avatar" id="<?= $i ?>">
+        <div class="avatar" id="">
             <a href="<?= Yii::$app->homeUrl . 'story/' . $model->encodeParams(['productPostId' => $model->productPostId]) ?>">
                 <img src="<?= $model->user->avatar ?>" alt=""/>
             </a>
