3bbd787536f27f4e31af09f8febc960427a28d8b
Taninut <sodapew17@gmail.com>
2017-11-20 10:05:12 +0700

    test login

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -190,7 +190,7 @@ return [
                     'clientSecret' => 'N739bef0DKOS7VUKT4DKmAL2',
                     // 'scope' => 'https://www.googleapis.com/auth/userinfo.email',
                     'scope' => 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email',
-                    'returnUrl' => 'http://localhost/Cozxy.com-frontend/site/auth?authclient=google',
+                    'returnUrl' => 'http://localhost/cozxy/frontend/web/site/auth?authclient=google',
                 ]
             ],
         ],
