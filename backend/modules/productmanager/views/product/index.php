bed975f00bfadb4ebc6ea6f1b049443703229849
Kurtumm <kurtumm@gmail.com>
2017-11-23 22:12:29 +0700

    search Brand and Category

--- backend/modules/productmanager/views/product/index.php
+++ backend/modules/productmanager/views/product/index.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
+use kartik\select2\Select2;
 
 /* @var $this yii\web\View */
 /* @var $searchModel backend\modules\productmanager\models\search\Product */
@@ -31,7 +32,7 @@ $this->params['breadcrumbs'][] = $this->title;
             //            'parentId',
             [
 //                'header'=>Html::checkbox('deleteAll', false, ['class'=>'deleteAll']),
-                'header'=>Html::button('Delete', ['class'=>'btn btn-danger', 'disabled'=>true, 'id'=>'deleteMultipleProducts']),
+                'header'=>Html::button('X', ['class'=>'btn btn-danger', 'disabled'=>true, 'id'=>'deleteMultipleProducts']),
                 'value'=>function($model) {
                     return Html::checkbox('delete['.$model->productId.']', false, ['class'=>'delProduct', 'id'=>'del-'.$model->productId, 'data-id'=>$model->productId, 'value'=>$model->productId]);
                 },
@@ -42,23 +43,50 @@ $this->params['breadcrumbs'][] = $this->title;
                 'value' => function ($model) {
                     return mb_substr($model->title, 0, 40);
                 },
+                'format'=>'raw'
             ],
             [
                 'attribute' => 'brandId',
+                'headerOptions' => ['style' => 'width:250px;'],
                 'value' => function ($model) {
                     return isset($model->brand) ? $model->brand->title : '';
                 },
-                'filter' => $brandFilter
+//                'filter' => $brandFilter
+                'filter' => Select2::widget([
+                    'model' => $searchModel,
+                    'attribute' => 'brandId',
+                    'data' => $brandFilter,
+                    'theme' => Select2::THEME_BOOTSTRAP,
+                    'options' => [
+                        'placeholder' => 'Brand',
+                    ],
+                    'pluginOptions' => [
+                        'allowClear' => true,
+                    ]
+                ])
             ],
             [
                 'attribute' => 'categoryId',
+                'headerOptions' => ['style' => 'width:350px;'],
                 'value' => function ($model) {
                     return isset($model->category) ? $model->category->title : '';
                 },
-                'filter' => $categoryFilter
+//                'filter' => $categoryFilter
+                'filter' => Select2::widget([
+                    'model' => $searchModel,
+                    'attribute' => 'categoryId',
+                    'data' => $categoryFilter,
+                    'theme' => Select2::THEME_BOOTSTRAP,
+                    'options' => [
+                        'placeholder' => 'Category',
+                    ],
+                    'pluginOptions' => [
+                        'allowClear' => true,
+                        ]
+                ])
             ],
             'isbn:ntext',
-            'code',
+//            'code',
             // 'suppCode',
             // 'merchantCode',
             // 'optionName',
@@ -75,7 +103,13 @@ $this->params['breadcrumbs'][] = $this->title;
             // 'tags',
 //            'status',
 //            'approve',
-            'createDateTime',
+//            'createDateTime',
+            [
+                'attribute'=>'createDateTime',
+                'value'=>function($model) {
+                    return substr($model->createDateTime, 0, 10);
+                }
+            ],
 //            'updateDateTime',
             // 'productSuppId',
             // 'approveCreateBy',
