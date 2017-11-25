74906bebda27847c9942a61139d985ef036aa33e
Taninut <sodapew17@gmail.com>
2017-11-25 10:37:14 +0700

    update

--- backend/config/main.php
+++ backend/config/main.php
@@ -1,7 +1,7 @@
 <?php
 
 $params = array_merge(
-require(__DIR__ . '/../../common/config/params.php'), require(__DIR__ . '/../../common/config/params-local.php'), require(__DIR__ . '/params.php'), require(__DIR__ . '/params-local.php')
+        require(__DIR__ . '/../../common/config/params.php'), require(__DIR__ . '/../../common/config/params-local.php'), require(__DIR__ . '/params.php'), require(__DIR__ . '/params-local.php')
 );
 
 return [
@@ -211,8 +211,8 @@ return [
             // 'product/*',
             'redactor/*',
             'gii/*',
-            //'booth/*',
-            'productmanager/*'
+        //'booth/*',
+        //'productmanager/*'
         ],
     ],
     'params' => $params,
