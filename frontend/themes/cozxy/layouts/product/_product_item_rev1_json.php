cb0e81562049049be57493218810a81d530652d0
Taninut <sodapew17@gmail.com>
2018-01-08 15:19:14 +0700

    update view elastic-searc

--- frontend/themes/cozxy/layouts/product/_product_item_rev1_json.php
+++ frontend/themes/cozxy/layouts/product/_product_item_rev1_json.php
@@ -51,39 +51,44 @@ $DiscountProduct = CozxyCalculatesCart::DiscountProduct($marketPrice, $supplierP
 <?php $col = isset($colSize) ? $colSize : '4'; ?>
 <div class="col-md-4 col-sm-6 col-xs-6 box-product">
     <div class="product-box">
-        <?php if ($DiscountProduct != 'Lessthan10') { ?>
-            <div class="product-sticker">
-                <div class="rcorners4">
-                    <p>
-                        <?php
-                        //echo Yii::$app->controller->id;
-                        if (Yii::$app->controller->id == 'search') {
-                            //echo 'SALE';
-                            echo $DiscountProduct;
-                            echo '<div class="off-style">OFF</div>';
-                        } else if (Yii::$app->controller->id == 'site') {
-                            //echo 'SALE';
-                            echo $DiscountProduct;
-                            echo '<div class="off-style">OFF</div>';
-                        } else {
-                            echo 'SAVE';
-                        }
-                        ?></p>
-                    <p><?php
-                        if (Yii::$app->controller->id == 'search') {
-                            //echo '-' . $DiscountProduct;
-                        } else if (Yii::$app->controller->id == 'site') {
-                            //echo '-' . $DiscountProduct;
-                            //echo 'OFF';
-                        } else {
-                            echo $DiscountProduct;
-                        }
-                        ?>
-                    </p>
+        <?php
+        if ($cozxySellingsPrice > 0) {
+            if ($DiscountProduct != 'Lessthan10') {
+                ?>
+                <div class="product-sticker">
+                    <div class="rcorners4">
+                        <p>
+                            <?php
+                            //echo Yii::$app->controller->id;
+                            if (Yii::$app->controller->id == 'search') {
+                                //echo 'SALE';
+                                echo $DiscountProduct;
+                                echo '<div class="off-style">OFF</div>';
+                            } else if (Yii::$app->controller->id == 'site') {
+                                //echo 'SALE';
+                                echo $DiscountProduct;
+                                echo '<div class="off-style">OFF</div>';
+                            } else {
+                                echo 'SAVE';
+                            }
+                            ?></p>
+                        <p><?php
+                            if (Yii::$app->controller->id == 'search') {
+                                //echo '-' . $DiscountProduct;
+                            } else if (Yii::$app->controller->id == 'site') {
+                                //echo '-' . $DiscountProduct;
+                                //echo 'OFF';
+                            } else {
+                                echo $DiscountProduct;
+                            }
+                            ?>
+                        </p>
+                    </div>
+                    <div class="triangle"></div>
                 </div>
-                <div class="triangle"></div>
-            </div>
-        <?php } ?>
+            <?php }
+        }
+        ?>
         <div class="product-img text-center">
             <a href="<?= Url::to(Yii::$app->homeUrl . 'product/' . common\models\ModelMaster::encodeParams(['productId' => $model['productid']])) ?>" class="fc-black">
                 <img class="media-object fullwidth img-responsive" src="<?= $productImageThumbnail ?>"  >
@@ -100,7 +105,7 @@ $DiscountProduct = CozxyCalculatesCart::DiscountProduct($marketPrice, $supplierP
                             <a href="javascript:addItemToDefaultWishlist(<?= $model['productid'] ?>);">
                                 <div class="col-xs-4 heart-<?= $model['productid'] ?>"><i class="fa fa-heart" aria-hidden="true"></i></div>
                             </a>
-                        <?php } else { ?>
+        <?php } else { ?>
                             <a href="javascript:addItemToDefaultWishlist(<?= $model['productid'] ?>);" id="heartbeat-<?= $model['productid'] ?>" data-loading-text="<div class='col-xs-4'><i class='fa fa-heart' aria-hidden='true'></i></div>" style="display: none;">
                                 <div class="col-xs-4 heart-<?= $model['productid'] ?>"><i class="fa fa-heart" aria-hidden="true"></i></div>
                             </a>
@@ -127,7 +132,7 @@ $DiscountProduct = CozxyCalculatesCart::DiscountProduct($marketPrice, $supplierP
                     <a href="javascript:addItemToCartUnitys('<?= $model['productsuppid'] ?>',1,'<?= $cozxyResult ?>','FALSE','<?= $model['productid'] ?>','<?= $model['userid'] ?>','<?= $receiveType ?>')" id="addItemsToCartMulti-<?= $model['productsuppid'] ?>" data-loading-text="<div class='col-xs-4 shopping-<?= $model['productsuppid'] ?>'><i class='fa fa-cart-plus fa-spin' aria-hidden='true'></i></div>">
                         <div class="col-xs-4 shopping-<?= $model['productsuppid'] ?>"><i id="cart-plus-<?= $model['productsuppid'] ?>" class="fa fa-cart-plus" aria-hidden="true"></i></div>
                     </a>
-                <?php } ?>
+<?php } ?>
             </div>
         </div>
         <div class="product-txt">
@@ -137,8 +142,8 @@ $DiscountProduct = CozxyCalculatesCart::DiscountProduct($marketPrice, $supplierP
                 <p class="brand">
                     <span class="size14"><?= strtoupper($cozxyBrandTitle) ?></span>
                 </p>
-            <?php } else {
-                ?>
+<?php } else {
+    ?>
                 <p class="brand">
                     <span class="size16">NO BRAND</span>
                 </p>
@@ -147,7 +152,7 @@ $DiscountProduct = CozxyCalculatesCart::DiscountProduct($marketPrice, $supplierP
             ?>
             <p class="name">
                 <a href="<?= Url::to(Yii::$app->homeUrl . 'product/' . common\models\ModelMaster::encodeParams(['productId' => isset($model['productid']) ? $model['productid'] : $model['productid']])) ?>" class="size18 b" title="<?= $model['productid'] ?>">
-                    <?= strtoupper($model['title']) ?>
+            <?= strtoupper($model['title']) ?>
                 </a>
             </p>
             <?php
@@ -158,8 +163,8 @@ $DiscountProduct = CozxyCalculatesCart::DiscountProduct($marketPrice, $supplierP
                         <span class="size18 fc-red"><?= isset($cozxySellingsPrice) ? number_format($cozxySellingsPrice) : 'NONE' . ' THB' ?> </span><br>
                         <span class="size14 onsale"><?= isset($model['price']) ? number_format($model['price']) . ' THB' : '' ?> </span>
                     </p>
-                <?php } else {
-                    ?>
+    <?php } else {
+        ?>
                     <p class="price" >
                         <span class="size18 fc-red"><?= isset($cozxySellingsPrice) ? number_format($cozxySellingsPrice) : 'NONE' . ' THB' ?> </span><br>
                         <span class="size14 onsale"><?= isset($model['price']) ? number_format($model['price']) . ' THB' : '' ?> </span>
