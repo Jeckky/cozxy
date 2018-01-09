0bd401c95dbd8e29277b35f7c8760d203ffe2396
Taninut <sodapew17@gmail.com>
2018-01-09 13:00:30 +0700

    update

--- common/helpers/ApiElasticSearch.php
+++ common/helpers/ApiElasticSearch.php
@@ -17,51 +17,8 @@ use Yii;
  */
 class ApiElasticSearch {
 
-    public static function searchProductBk($search, $status, $brand_id, $category_id) {
-
-        /* Function AddtoCart : paramenter
-         * addItemToCartUnitys(productSuppId, quantity, maxQnty, fastId, productId, supplierId, receiveType)
-         * javascript:addItemToCartUnitys('1228',1,'49','FALSE','1631','19','1')
-         * $url = 'http://192.168.8.11/cozxy/frontend/web/elastic/product-for-sale.json';
-         * $url = 'http://192.168.8.11/cozxy/frontend/web/elastic/product-not-sale.json';
-         */
-
-        if ($status == 'for-sale') {
-            $json = 'product-for-sale.json';
-        } elseif ($status == 'not-sale') {
-            $json = 'product-not-sale.json';
-        }
-        //url 'http://45.76.157.59:3000/search?text=dry%20skin&brand_id=67,68&category_id=16'
-        //$url = "http://" . Yii::$app->request->getServerName() . Yii::$app->homeUrl . "elastic/" . $json;
-        $url = "http://45.76.157.59:3000/search?text=" . $search . "&brand_id=" . $brand_id . "&category_id=" . $category_id . "";
-
-
-        //echo 'url : ' . $url . '<br><br>';
-        $data = "search=" . $search . "&status=" . $status . "&test=1&brand_id=" . $brand_id . '&category_id=' . $category_id;
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        $content = curl_exec($ch);
-        curl_close($ch);
-
-        // Convert JSON string to Array
-        $someArray = json_decode($content, true);
-
-        return $someArray;
-        //print_r($someArray);        // Dump all data of the Array
-        //echo $someArray[0]["name"]; // Access Array data
-        // Convert JSON string to Object
-        //$someObject = json_decode($someJSON);
-        //print_r($someObject);      // Dump all data of the Object
-        //echo $someObject[0]->name; // Access Object data
-    }
-
     //public static function searchProduct($search, $status, $brand_id, $category_id, $mins, $maxs, $size, $pages) {
     public static function searchProduct($Eparameter) {
-
         $search = $Eparameter['search'];
         $status = $Eparameter['status'];
         $brand_id = $Eparameter['brandId'];
@@ -105,6 +62,48 @@ class ApiElasticSearch {
             //exit();
             return $someResponse;
         }
+    }
+
+    public static function searchProductBk($search, $status, $brand_id, $category_id) {
+
+        /* Function AddtoCart : paramenter
+         * addItemToCartUnitys(productSuppId, quantity, maxQnty, fastId, productId, supplierId, receiveType)
+         * javascript:addItemToCartUnitys('1228',1,'49','FALSE','1631','19','1')
+         * $url = 'http://192.168.8.11/cozxy/frontend/web/elastic/product-for-sale.json';
+         * $url = 'http://192.168.8.11/cozxy/frontend/web/elastic/product-not-sale.json';
+         */
+
+        if ($status == 'for-sale') {
+            $json = 'product-for-sale.json';
+        } elseif ($status == 'not-sale') {
+            $json = 'product-not-sale.json';
+        }
+        //url 'http://45.76.157.59:3000/search?text=dry%20skin&brand_id=67,68&category_id=16'
+        //$url = "http://" . Yii::$app->request->getServerName() . Yii::$app->homeUrl . "elastic/" . $json;
+        $url = "http://45.76.157.59:3000/search?text=" . $search . "&brand_id=" . $brand_id . "&category_id=" . $category_id . "";
+
+
+        //echo 'url : ' . $url . '<br><br>';
+        $data = "search=" . $search . "&status=" . $status . "&test=1&brand_id=" . $brand_id . '&category_id=' . $category_id;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $content = curl_exec($ch);
+        curl_close($ch);
+
+        // Convert JSON string to Array
+        $someArray = json_decode($content, true);
+
+        return $someArray;
+        //print_r($someArray);        // Dump all data of the Array
+        //echo $someArray[0]["name"]; // Access Array data
+        // Convert JSON string to Object
+        //$someObject = json_decode($someJSON);
+        //print_r($someObject);      // Dump all data of the Object
+        //echo $someObject[0]->name; // Access Object data
     }
 
 }
