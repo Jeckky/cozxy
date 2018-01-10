a6124c18079c75ae4efa398b1fdd278a3f36e61c
Taninut <sodapew17@gmail.com>
2018-01-10 10:35:28 +0700

    update

--- frontend/controllers/SearchController.php
+++ frontend/controllers/SearchController.php
@@ -587,7 +587,7 @@ class SearchController extends MasterController {
             $categoryId = Yii::$app->request->get('categoryId');
             $site = 'category';
         } else {
-            $categoryId = '';
+            $categoryId = Yii::$app->request->get('categoryId');
             $site = 'brand';
         }
         //}
