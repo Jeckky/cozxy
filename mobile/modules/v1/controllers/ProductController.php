8f8fdc84b6904c0c45ddb3ce7a041a25046d483a
Kurtumm <kurtumm@gmail.com>
2018-02-11 22:24:33 +0700

    mobile

--- mobile/modules/v1/controllers/ProductController.php
+++ mobile/modules/v1/controllers/ProductController.php
@@ -338,6 +338,11 @@ class ProductController extends \common\controllers\MasterController
             $res['isWishlist'] = self::isWishlist($product->productId, $userId);
             $res['worldPrice'] = (self::worldPrice($id) !== null) ? self::worldPrice($id) : $res['price'];
             $res['localPrice'] = (self::localPrice($id) !== null) ? self::localPrice($id) : number_format($res['price'] * 0.8, 2);
+
+            $res['comparePrice'] = [
+                'worldPrice'=>self::worldPrice($id),
+                'localPrice'=>self::localPrice($id),
+            ];
         } else {
             $res['error'] = 'Product not found';
         }
@@ -435,16 +440,16 @@ class ProductController extends \common\controllers\MasterController
 
     private static function worldPrice($productId)
     {
-        $worldPrice = ProductPostComparePrice::find()->where(['productId'=>$productId])->orderBy('RAND()')->one();
+        $worldPrice = ProductPostComparePrice::find()->where(['productId'=>$productId])->orderBy(['price'=>SORT_ASC])->one();
 
-        return (isset($worldPrice)) ? $worldPrice->price : null;
+        return (isset($worldPrice)) ? $worldPrice->attributes : null;
     }
 
     private static function localPrice($productId)
     {
-        $worldPrice = ProductPostComparePrice::find()->where(['productId'=>$productId])->orderBy('RAND()')->one();
+        $worldPrice = ProductPostComparePrice::find()->where(['productId'=>$productId, 'country'=>'THA'])->orderBy(['price'=>SORT_ASC])->one();
 
-        return (isset($worldPrice)) ? $worldPrice->price : null;
+        return (isset($worldPrice)) ? $worldPrice->attributes : null;
     }
 
 }
