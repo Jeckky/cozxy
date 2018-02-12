0a4ffe05e99926308b1ae74559e50afc7bb15481
Taninut <sodapew17@gmail.com>
2018-02-12 13:35:08 +0700

    changes layouts

--- frontend/themes/cozxy/layoutsV2/content/_items_good_reads_list.php
+++ frontend/themes/cozxy/layoutsV2/content/_items_good_reads_list.php
@@ -2,14 +2,14 @@
     <div class="product-box">
         <div class="product-img text-center">
             <a href="#" class="fc-black">
-                <img class="media-object fullwidth img-responsive" src="<?= Yii::$app->homeUrl ?><?= $model->image ?>" width="410" height="256">
+                <img class="media-object fullwidth img-responsive" src="<?= Yii::$app->homeUrl ?><?= $model['image'] ?>" width="410" height="256">
             </a>
         </div>
 
     </div>
     <div class="text-center">
         <p class="category">
-            <span class="size18"><?= strtoupper($model->title) ?></span>
+            <span class="size18"><?= strtoupper($model['title']) ?></span>
         </p>
     </div>
 </div>
--- frontend/themes/cozxy/layoutsV2/product/_product_item_story.php
+++ frontend/themes/cozxy/layoutsV2/product/_product_item_story.php
@@ -6,8 +6,8 @@ use common\helpers\Base64Decode;
     <div class="product-box box-product">
         <div class="product-img text-center">
             <a href="<?= Yii::$app->homeUrl . 'story/' . $model->encodeParams(['productPostId' => $model->productPostId]) ?>">
-                <!--<img class="media-object fullwidth img-responsive" src="<?//= isset($model->product) ? \Yii::$app->homeUrl . $model->product->productImageThumbnail() : Base64Decode::DataImageSvg('Svg260x260') ?>" >-->
-                <img class="media-object fullwidth img-responsive" src="/cozxy/frontend/web/images/ProductImageSuppliers/thumbnail1/gWYeDn5aopSZD5PWhMJdRbrVAC_6wNVa.jpg">
+                <img class="media-object fullwidth img-responsive" src="<?//= isset($model->product) ? \Yii::$app->homeUrl . $model->product->productImageThumbnail() : Base64Decode::DataImageSvg('Svg260x260') ?>" >
+                <!--<img class="media-object fullwidth img-responsive" src="/cozxy/frontend/web/images/ProductImageSuppliers/thumbnail1/gWYeDn5aopSZD5PWhMJdRbrVAC_6wNVa.jpg">-->
             </a>
         </div>
         <div class="product-txt">
