bc28065513972ee6d794d0a8e448e7c00ac466f1
Taninut <sodapew17@gmail.com>
2018-02-13 11:50:28 +0700

    update

--- frontend/themes/cozxy/layouts/elastic/_product_item_rev1_json.php
+++ frontend/themes/cozxy/layouts/elastic/_product_item_rev1_json.php
@@ -96,7 +96,7 @@ $DiscountProduct = CozxyCalculatesCart::DiscountProduct($marketPrice, $supplierP
         ?>
         <div class="product-img text-center">
             <a href="<?= Url::to(Yii::$app->homeUrl . 'product/' . common\models\ModelMaster::encodeParams(['productId' => $model['productId']])) ?>" class="fc-black">
-                <img class="media-object fullwidth img-responsive" src="<?= $model['imageThumbnail1']//$productImageThumbnail   ?>"  >
+                <img class="media-object fullwidth img-responsive" src="<?= $model['imageThumbnail1']//$productImageThumbnail     ?>"  >
             </a>
             <div class="v-hover">
                 <a href="<?= Url::to(Yii::$app->homeUrl . 'product/' . common\models\ModelMaster::encodeParams(['productId' => isset($model->product->productId) ? $model->product->productId : $model['productId']])) ?>">
@@ -180,7 +180,7 @@ $DiscountProduct = CozxyCalculatesCart::DiscountProduct($marketPrice, $supplierP
                 //echo 'EYEWEAR-(PRODUCTS)';
                 ?>
                 <p class="price" >
-                    <span class="size18 fc-red">EXPLORE PRODUCTS </span><br>
+                    <span class="size18 fc-red">&nbsp;<!--EXPLORE PRODUCTS--></span><br>
                     <span class="size14 onsale">&nbsp;</span>
                 </p>
                 <?php
