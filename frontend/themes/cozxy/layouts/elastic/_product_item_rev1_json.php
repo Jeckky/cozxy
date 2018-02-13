5a2d86b099013cc98b9452913a377791f33bc930
Taninut <sodapew17@gmail.com>
2018-02-13 13:25:16 +0700

    update

--- frontend/themes/cozxy/layouts/elastic/_product_item_rev1_json.php
+++ frontend/themes/cozxy/layouts/elastic/_product_item_rev1_json.php
@@ -54,7 +54,7 @@ $DiscountProduct = CozxyCalculatesCart::DiscountProduct($marketPrice, $supplierP
 //GetBrowser::UserAgent() == 'computer'
 ?>
 <?php $col = isset($colSize) ? $colSize : '4'; ?>
-<div class="col-md-4 col-sm-6 col-xs-6 box-product">
+<div class="col-md-4 col-sm-6 col-xs-6 box-product cozxy-items-<?= $model['productId'] ?>">
     <div class="product-box">
         <?php
         if ($cozxySellingsPrice > 0) {
@@ -96,7 +96,7 @@ $DiscountProduct = CozxyCalculatesCart::DiscountProduct($marketPrice, $supplierP
         ?>
         <div class="product-img text-center">
             <a href="<?= Url::to(Yii::$app->homeUrl . 'product/' . common\models\ModelMaster::encodeParams(['productId' => $model['productId']])) ?>" class="fc-black">
-                <img class="media-object fullwidth img-responsive" src="<?= $model['imageThumbnail1']//$productImageThumbnail          ?>"  >
+                <img class="media-object fullwidth img-responsive" src="<?= $model['imageThumbnail1']//$productImageThumbnail           ?>"  >
             </a>
             <div class="v-hover">
                 <a href="<?= Url::to(Yii::$app->homeUrl . 'product/' . common\models\ModelMaster::encodeParams(['productId' => isset($model->product->productId) ? $model->product->productId : $model['productId']])) ?>">
