b1541ad61fd6a8ff842f1acd03ae5f74380ed6be
Taninut <sodapew17@gmail.com>
2018-02-20 18:56:07 +0700

    changes hide v-hover on mobile

--- frontend/themes/cozxy/layoutsV2/product/_new_prodcut_items.php
+++ frontend/themes/cozxy/layoutsV2/product/_new_prodcut_items.php
@@ -7,6 +7,7 @@ use yii\helpers\Url;
 use common\helpers\Base64Decode;
 use common\helpers\CozxyCalculatesCart;
 
+$UserAgent = common\helpers\GetBrowser::UserAgent();
 if (Yii::$app->controller->id == 'product') {
     $width = "width: 195px";
     $height = "height: 195px";
@@ -67,47 +68,51 @@ if ($index == 0) {
                     <a href="<?= Url::to(Yii::$app->homeUrl . 'product/' . $model->encodeParams(['productId' => $model->productId])) ?>" class="fc-black" style=" min-height: 256px; max-height: 256px;">
                         <img class="media-object fullwidth img-responsive" src="<?= isset($model->product) ? \Yii::$app->homeUrl . $model->product->productImageThumbnail() : Base64Decode::DataImageSvg('Svg260x260') ?>">
                     </a>
-                    <div class="v-hover">
-                        <a href="<?= Url::to(Yii::$app->homeUrl . 'product/' . $model->encodeParams(['productId' => isset($model->product->productId) ? $model->product->productId : $model->productId])) ?>">
-                            <div class="col-xs-4"><i class="fa fa-eye" aria-hidden="true"></i></div>
-                        </a>
-                        <?php
-                        if (Yii::$app->user->id) {
-                            if (isset($model->product)) {
-                                if ($model->product->isInWishlist() == 1) { // เคย wishList ไปแล้ว
-                                    ?>
-                                    <a href="javascript:addItemToDefaultWishlist(<?= $model->productId ?>);">
-                                        <div class="col-xs-4 heart-<?= $model->productId ?>"><i class="fa fa-heart" aria-hidden="true"></i></div>
-                                    </a>
-                                <?php } else { ?>
-                                    <a href="javascript:addItemToDefaultWishlist(<?= $model->productId ?>);" id="heartbeat-<?= $model->productId ?>" data-loading-text="<div class='col-xs-4'><i class='fa fa-heart' aria-hidden='true'></i></div>" style="display: none;">
-                                        <div class="col-xs-4 heart-<?= $model->productId ?>"><i class="fa fa-heart" aria-hidden="true"></i></div>
-                                    </a>
-                                    <a href="javascript:addItemToDefaultWishlist(<?= $model->productId ?>);" id="heart-o-<?= $model->productId ?>">
-                                        <div class="col-xs-4 heart-<?= $model->productId ?>"><i class="fa fa-heart-o" aria-hidden="true"></i></div>
-                                    </a>
-                                    <?php
+                    <?php
+                    if ($UserAgent != 'mobile') {
+                        ?>
+                        <div class="v-hover">
+                            <a href="<?= Url::to(Yii::$app->homeUrl . 'product/' . $model->encodeParams(['productId' => isset($model->product->productId) ? $model->product->productId : $model->productId])) ?>">
+                                <div class="col-xs-4"><i class="fa fa-eye" aria-hidden="true"></i></div>
+                            </a>
+                            <?php
+                            if (Yii::$app->user->id) {
+                                if (isset($model->product)) {
+                                    if ($model->product->isInWishlist() == 1) { // เคย wishList ไปแล้ว
+                                        ?>
+                                        <a href="javascript:addItemToDefaultWishlist(<?= $model->productId ?>);">
+                                            <div class="col-xs-4 heart-<?= $model->productId ?>"><i class="fa fa-heart" aria-hidden="true"></i></div>
+                                        </a>
+                                    <?php } else { ?>
+                                        <a href="javascript:addItemToDefaultWishlist(<?= $model->productId ?>);" id="heartbeat-<?= $model->productId ?>" data-loading-text="<div class='col-xs-4'><i class='fa fa-heart' aria-hidden='true'></i></div>" style="display: none;">
+                                            <div class="col-xs-4 heart-<?= $model->productId ?>"><i class="fa fa-heart" aria-hidden="true"></i></div>
+                                        </a>
+                                        <a href="javascript:addItemToDefaultWishlist(<?= $model->productId ?>);" id="heart-o-<?= $model->productId ?>">
+                                            <div class="col-xs-4 heart-<?= $model->productId ?>"><i class="fa fa-heart-o" aria-hidden="true"></i></div>
+                                        </a>
+                                        <?php
+                                    }
                                 }
-                            }
-                        } else {
-                            ?>
-                            <a href="<?= Yii::$app->homeUrl . 'site/login' ?>" >
-                                <div class="col-xs-4"><i class="fa fa-heart-o" aria-hidden="true"></i></div>
-                            </a>
-                        <?php } ?>
-                        <?php
-                        if ($model->result > 0) {
-                            if ($model->receiveType != '') {
-                                $receiveType = $model->receiveType;
                             } else {
-                                $receiveType = 1;
-                            }
-                            ?>
-                            <a  href="javascript:addItemToCartUnitys('<?= $model->productSuppId ?>',1,'<?= $model->result ?>','FALSE','<?= $model->productId ?>','<?= $model->userId ?>','<?= $receiveType ?>')" id="addItemsToCartMulti-<?= $model->productSuppId ?>" data-loading-text="<div class='col-xs-4 shopping-<?= $model->productSuppId ?>'><i class='fa fa-cart-plus fa-spin' aria-hidden='true'></i></div>">
-                                <div class="col-xs-4 shopping-<?= $model->productSuppId ?>"><i id="cart-plus-<?= $model->productSuppId ?>" class="fa fa-cart-plus" aria-hidden="true"></i></div>
-                            </a>
-                        <?php } ?>
-                    </div>
+                                ?>
+                                <a href="<?= Yii::$app->homeUrl . 'site/login' ?>" >
+                                    <div class="col-xs-4"><i class="fa fa-heart-o" aria-hidden="true"></i></div>
+                                </a>
+                            <?php } ?>
+                            <?php
+                            if ($model->result > 0) {
+                                if ($model->receiveType != '') {
+                                    $receiveType = $model->receiveType;
+                                } else {
+                                    $receiveType = 1;
+                                }
+                                ?>
+                                <a  href="javascript:addItemToCartUnitys('<?= $model->productSuppId ?>',1,'<?= $model->result ?>','FALSE','<?= $model->productId ?>','<?= $model->userId ?>','<?= $receiveType ?>')" id="addItemsToCartMulti-<?= $model->productSuppId ?>" data-loading-text="<div class='col-xs-4 shopping-<?= $model->productSuppId ?>'><i class='fa fa-cart-plus fa-spin' aria-hidden='true'></i></div>">
+                                    <div class="col-xs-4 shopping-<?= $model->productSuppId ?>"><i id="cart-plus-<?= $model->productSuppId ?>" class="fa fa-cart-plus" aria-hidden="true"></i></div>
+                                </a>
+                            <?php } ?>
+                        </div>
+                    <?php } ?>
                 </div>
                 <div class="product-txt">
                     <?php
--- frontend/themes/cozxy/layoutsV2/product/_new_prodcut_items_hot_deals.php
+++ frontend/themes/cozxy/layoutsV2/product/_new_prodcut_items_hot_deals.php
@@ -7,6 +7,8 @@ use yii\helpers\Url;
 use common\helpers\Base64Decode;
 use common\helpers\CozxyCalculatesCart;
 
+$UserAgent = common\helpers\GetBrowser::UserAgent();
+
 if (Yii::$app->controller->id == 'product') {
     $width = "width: 195px";
     $height = "height: 195px";
@@ -33,7 +35,7 @@ if ($index == 0) {
             <?php if ($DiscountProduct != 'Lessthan10') { ?>
                 <div class="product-sticker" style="right: 7%;">
                     <div class="rcorners4">
-                        <p>
+                        <p> 
                             <?php
                             //echo Yii::$app->controller->id;
                             if (Yii::$app->controller->id == 'search') {
@@ -68,47 +70,51 @@ if ($index == 0) {
                     <img class="media-object fullwidth img-responsive" src="<?= isset($model->product) ? \Yii::$app->homeUrl . $model->product->productImageThumbnail() : Base64Decode::DataImageSvg('Svg260x260') ?>">
                     <!--<img class="media-object fullwidth img-responsive" src="https://www.cozxy.com/images/ProductImageSuppliers/thumbnail1/zqQkrvAcdFGAcyoiTRDrU-6KytJ8THLF.jpg">-->
                 </a>
-                <div class="v-hover">
-                    <a href="<?= Url::to(Yii::$app->homeUrl . 'product/' . $model->encodeParams(['productId' => isset($model->product->productId) ? $model->product->productId : $model->productId])) ?>">
-                        <div class="col-xs-4"><i class="fa fa-eye" aria-hidden="true"></i></div>
-                    </a>
-                    <?php
-                    if (Yii::$app->user->id) {
-                        if (isset($model->product)) {
-                            if ($model->product->isInWishlist() == 1) { // เคย wishList ไปแล้ว
-                                ?>
-                                <a href="javascript:addItemToDefaultWishlist(<?= $model->productId ?>);">
-                                    <div class="col-xs-4 heart-<?= $model->productId ?>"><i class="fa fa-heart" aria-hidden="true"></i></div>
-                                </a>
-                            <?php } else { ?>
-                                <a href="javascript:addItemToDefaultWishlist(<?= $model->productId ?>);" id="heartbeat-<?= $model->productId ?>" data-loading-text="<div class='col-xs-4'><i class='fa fa-heart' aria-hidden='true'></i></div>" style="display: none;">
-                                    <div class="col-xs-4 heart-<?= $model->productId ?>"><i class="fa fa-heart" aria-hidden="true"></i></div>
-                                </a>
-                                <a href="javascript:addItemToDefaultWishlist(<?= $model->productId ?>);" id="heart-o-<?= $model->productId ?>">
-                                    <div class="col-xs-4 heart-<?= $model->productId ?>"><i class="fa fa-heart-o" aria-hidden="true"></i></div>
-                                </a>
-                                <?php
+                <?php
+                if ($UserAgent != 'mobile') {
+                    ?>
+                    <div class="v-hover">
+                        <a href="<?= Url::to(Yii::$app->homeUrl . 'product/' . $model->encodeParams(['productId' => isset($model->product->productId) ? $model->product->productId : $model->productId])) ?>">
+                            <div class="col-xs-4"><i class="fa fa-eye" aria-hidden="true"></i></div>
+                        </a>
+                        <?php
+                        if (Yii::$app->user->id) {
+                            if (isset($model->product)) {
+                                if ($model->product->isInWishlist() == 1) { // เคย wishList ไปแล้ว
+                                    ?>
+                                    <a href="javascript:addItemToDefaultWishlist(<?= $model->productId ?>);">
+                                        <div class="col-xs-4 heart-<?= $model->productId ?>"><i class="fa fa-heart" aria-hidden="true"></i></div>
+                                    </a>
+                                <?php } else { ?>
+                                    <a href="javascript:addItemToDefaultWishlist(<?= $model->productId ?>);" id="heartbeat-<?= $model->productId ?>" data-loading-text="<div class='col-xs-4'><i class='fa fa-heart' aria-hidden='true'></i></div>" style="display: none;">
+                                        <div class="col-xs-4 heart-<?= $model->productId ?>"><i class="fa fa-heart" aria-hidden="true"></i></div>
+                                    </a>
+                                    <a href="javascript:addItemToDefaultWishlist(<?= $model->productId ?>);" id="heart-o-<?= $model->productId ?>">
+                                        <div class="col-xs-4 heart-<?= $model->productId ?>"><i class="fa fa-heart-o" aria-hidden="true"></i></div>
+                                    </a>
+                                    <?php
+                                }
                             }
-                        }
-                    } else {
-                        ?>
-                        <a href="<?= Yii::$app->homeUrl . 'site/login' ?>" >
-                            <div class="col-xs-4"><i class="fa fa-heart-o" aria-hidden="true"></i></div>
-                        </a>
-                    <?php } ?>
-                    <?php
-                    if ($model->result > 0) {
-                        if ($model->receiveType != '') {
-                            $receiveType = $model->receiveType;
                         } else {
-                            $receiveType = 1;
-                        }
-                        ?>
-                        <a  href="javascript:addItemToCartUnitys('<?= $model->productSuppId ?>',1,'<?= $model->result ?>','FALSE','<?= $model->productId ?>','<?= $model->userId ?>','<?= $receiveType ?>')" id="addItemsToCartMulti-<?= $model->productSuppId ?>" data-loading-text="<div class='col-xs-4 shopping-<?= $model->productSuppId ?>'><i class='fa fa-cart-plus fa-spin' aria-hidden='true'></i></div>">
-                            <div class="col-xs-4 shopping-<?= $model->productSuppId ?>"><i id="cart-plus-<?= $model->productSuppId ?>" class="fa fa-cart-plus" aria-hidden="true"></i></div>
-                        </a>
-                    <?php } ?>
-                </div>
+                            ?>
+                            <a href="<?= Yii::$app->homeUrl . 'site/login' ?>" >
+                                <div class="col-xs-4"><i class="fa fa-heart-o" aria-hidden="true"></i></div>
+                            </a>
+                        <?php } ?>
+                        <?php
+                        if ($model->result > 0) {
+                            if ($model->receiveType != '') {
+                                $receiveType = $model->receiveType;
+                            } else {
+                                $receiveType = 1;
+                            }
+                            ?>
+                            <a  href="javascript:addItemToCartUnitys('<?= $model->productSuppId ?>',1,'<?= $model->result ?>','FALSE','<?= $model->productId ?>','<?= $model->userId ?>','<?= $receiveType ?>')" id="addItemsToCartMulti-<?= $model->productSuppId ?>" data-loading-text="<div class='col-xs-4 shopping-<?= $model->productSuppId ?>'><i class='fa fa-cart-plus fa-spin' aria-hidden='true'></i></div>">
+                                <div class="col-xs-4 shopping-<?= $model->productSuppId ?>"><i id="cart-plus-<?= $model->productSuppId ?>" class="fa fa-cart-plus" aria-hidden="true"></i></div>
+                            </a>
+                        <?php } ?>
+                    </div>
+                <?php } ?>
             </div>
 
         </div>
