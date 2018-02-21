776ed86816f4e71f4051a4ae03dd867be852d0c2
Taninut <sodapew17@gmail.com>
2018-02-21 11:59:42 +0700

    changes layouts menu brand

--- frontend/views/brands/index.php
+++ frontend/views/brands/index.php
@@ -95,7 +95,7 @@ $this->params['breadcrumbs'][] = $this->title;
                         <?php
                         foreach ($alphabet as $a => $value) {
                             ?>
-                            <div class="brands-alphabet-box">
+                            <div class="brands-alphabet-box cell-<?= $a ?>">
                                 <div id="brands-alphabet"  style="padding-left: 0px; padding-right: 0px;  ">
                                     <h2> <?= $value ?></h2>
                                     <div class="carousel-inner-brands-alphabet" style=" padding: 0 0px 0px 0px;">
