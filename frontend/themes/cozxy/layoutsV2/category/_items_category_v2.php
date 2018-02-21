0742a99ff6929b8eccae08f0be191bdd9f4226fa
Taninut <sodapew17@gmail.com>
2018-02-21 14:15:11 +0700

    changes link cate

--- frontend/themes/cozxy/layoutsV2/category/_items_category_v2.php
+++ frontend/themes/cozxy/layoutsV2/category/_items_category_v2.php
@@ -1,7 +1,7 @@
 <div class="col-md-4 col-sm-6 col-xs-6 box-product">
     <div class="product-box">
         <div class="product-img text-center">
-            <a href="#" class="fc-black">
+            <a href="<?= Yii::$app->homeUrl . 'search/' . common\models\ModelMaster::createTitleArray($model->title) . '/' . common\models\ModelMaster::encodeParams(['categoryId' => $model->categoryId]) ?>" class="fc-black">
                 <img class="media-object fullwidth img-responsive" src="<?= $model->image ?>" width="410" height="256">
                 <!--<img class="media-object fullwidth img-responsive" src="<?= Yii::$app->homeUrl ?>images/Category/<?= $index ?>.jpg" width="410" height="256">-->
             </a>
@@ -9,7 +9,10 @@
     </div>
     <div class="text-center">
         <p class="category">
-            <span class="size18"><?= strtoupper($model->title) ?></span>
+            <span class="size18">
+                <a href="<?= Yii::$app->homeUrl . 'search/' . common\models\ModelMaster::createTitleArray($model->title) . '/' . common\models\ModelMaster::encodeParams(['categoryId' => $model->categoryId]) ?>">
+                    <?= strtoupper($model->title) ?></a>
+            </span>
         </p>
     </div>
 </div>
