ccb57b99836ec0f6cf89f6a3931bbcd92c7e0dd3
Taninut <sodapew17@gmail.com>
2018-02-21 16:20:48 +0700

    changes layouts clearance

--- frontend/views/clearance/index.php
+++ frontend/views/clearance/index.php
@@ -148,12 +148,14 @@ if ($site == 'category') {
 }
 //echo 'controller :' . Yii::$app->controller->id;
 ?>
+<div class="wrapper-cozxy">
+    <?=
+    $this->render('@app/themes/cozxy/layouts/search/_search_filter', [
+        'categoryId' => $categoryId, 'productFilterBrand' => $productFilterBrand
+    ]);
+    ?>
+</div>
 
-<?=
-$this->render('@app/themes/cozxy/layouts/search/_search_filter', [
-    'categoryId' => $categoryId, 'productFilterBrand' => $productFilterBrand
-]);
-?>
 <div class="product-list wrapper-cozxy">
     <div class="container">
 
@@ -363,7 +365,7 @@ $this->render('@app/themes/cozxy/layouts/search/_search_filter', [
             </div>
 
             <div class="col-xs-9 text-center">
-                <!--<a href="javascript:showMore('<?php //echo $categoryId;                                                                                                                                                                                                                                                                                       ?>','<?php //echo $clickNum;                                                                                                                                                                                                                                                                                      ?>','<?php //echo $countAllProduct;                                                                                                                                                                                                                                                                                      ?>','<?php //echo $limit_start;                                                                                                                                                                                                                                                                                     ?>','<?php //echo $limit_end;                                                                                                                                                                                                                                                                                    ?>')" class="b btn-black showStepMore" style="margin:24px auto 32px">SHOW MORE
+                <!--<a href="javascript:showMore('<?php //echo $categoryId;                                                                                                                                                                                                                                                                                          ?>','<?php //echo $clickNum;                                                                                                                                                                                                                                                                                         ?>','<?php //echo $countAllProduct;                                                                                                                                                                                                                                                                                         ?>','<?php //echo $limit_start;                                                                                                                                                                                                                                                                                        ?>','<?php //echo $limit_end;                                                                                                                                                                                                                                                                                       ?>')" class="b btn-black showStepMore" style="margin:24px auto 32px">SHOW MORE
                     <span class="size16">&nbsp; ↓ </span></a>-->
             </div>
             <div class="col-xs-3 text-center">&nbsp;</div>
