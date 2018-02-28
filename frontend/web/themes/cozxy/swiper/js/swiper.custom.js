cdb030310e6c028b6e96895a4f32a9f585a18cea
Taninut <sodapew17@gmail.com>
2018-02-28 09:49:50 +0700

    changes js swiper slide per 3 items

--- frontend/web/themes/cozxy/swiper/js/swiper.custom.js
+++ frontend/web/themes/cozxy/swiper/js/swiper.custom.js
@@ -4,8 +4,6 @@
  * and open the template in the editor.
  */
 
-
-
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 6,
     spaceBetween: 5,
@@ -26,19 +24,20 @@ var swiper = new Swiper('.swiper-container', {
         // when window width is <= 320px
         320: {
             slidesPerView: 1,
-            spaceBetween: 10
+            spaceBetween: 10,
+            slidesPerGroup: 1,
         },
         // when window width is <= 480px
         480: {
             slidesPerView: 3,
-            spaceBetween: 2
+            spaceBetween: 2,
+            slidesPerGroup: 3,
         },
         // when window width is <= 640px
         640: {
             slidesPerView: 3,
             spaceBetween: 2,
-
+            slidesPerGroup: 3,
         },
-
     }
 });
